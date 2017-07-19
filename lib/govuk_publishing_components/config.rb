d5ba16171e891e1fbb574964852f28d0d3fe2a1d
Paul Hayes <fofr@users.noreply.github.com>
2017-07-19 16:32:51 +0100

    Allow application stylesheet and script to be customised
    Default both to rails convention, “application”

--- lib/govuk_publishing_components/config.rb
+++ lib/govuk_publishing_components/config.rb
@@ -4,8 +4,13 @@ module GovukPublishingComponents
   end
 
   module Config
-    # Name of your component guide
     mattr_accessor :component_guide_title
-    @component_guide_title = "GOV.UK Component Guide"
+    self.component_guide_title = "GOV.UK Component Guide"
+
+    mattr_accessor :application_stylesheet
+    self.application_stylesheet = "application"
+
+    mattr_accessor :application_javascript
+    self.application_javascript = "application"
   end
 end
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -5,9 +5,13 @@ describe GovukPublishingComponents::Config do
     it "configures the application" do
       GovukPublishingComponents.configure do |config|
         config.component_guide_title = "My component guide"
+        config.application_stylesheet = "custom_stylesheet"
+        config.application_javascript = "custom_javascript"
       end
 
       expect(GovukPublishingComponents::Config.component_guide_title).to eql("My component guide")
+      expect(GovukPublishingComponents::Config.application_stylesheet).to eql("custom_stylesheet")
+      expect(GovukPublishingComponents::Config.application_javascript).to eql("custom_javascript")
     end
   end
 end
