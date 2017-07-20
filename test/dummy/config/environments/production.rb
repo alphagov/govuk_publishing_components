a2594f7093a42afdfa5f897f072ac39a6335e504
Paul Hayes <fofr@users.noreply.github.com>
2017-07-20 09:58:30 +0100

    Remove unused active_record references

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -80,7 +80,4 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
