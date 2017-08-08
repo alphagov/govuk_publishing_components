a9eea549500a3a3b21ae4add2177c5629703ae78
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-08-08 11:07:09 +0000

    Remove test file from generator
    - cannot guarantee that an app will have the component_test_helper present, so removing the test generation from this generator for now

--- lib/generators/govuk_publishing_components/component_generator.rb
+++ lib/generators/govuk_publishing_components/component_generator.rb
@@ -7,21 +7,17 @@ module GovukPublishingComponents
 
     def copy_component_files
       @public_name = file_name.dasherize
-      @test_name = file_name.camelize
 
       template_dir = "app/views/components/"
       docs_dir = "app/views/components/docs/"
-      test_dir = "test/components/"
       scss_dir = "app/assets/stylesheets/components/"
 
       create_directory_if_not_exists(template_dir)
       create_directory_if_not_exists(docs_dir)
-      create_directory_if_not_exists(test_dir)
       create_directory_if_not_exists(scss_dir)
 
       template '_component.html.erb', "#{template_dir}_#{@public_name}.html.erb"
       template 'component.yml.erb', "#{docs_dir}#{@public_name}.yml"
-      template 'component_test.rb.erb', "#{test_dir}#{file_name}_test.rb"
       template '_component.scss', "#{scss_dir}_#{@public_name}.scss"
     end
 
