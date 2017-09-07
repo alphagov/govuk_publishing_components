8c00aba7230ea83c9f23e2fb3cad4e3e997fb354
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-09-07 12:32:22 +0000

    Add shared accessibility criteria
    - common accessibility criteria defined in the gem can be referenced in yml in components to save copying and pasting and reduce duplication
    - shared_accessibility_criteria is a list
    - only one defined at the moment, 'link'
    - components can include accessibility_criteria, shared_accessibility_criteria, or both

---
+++ app/models/govuk_publishing_components/shared_accessibility_criteria.rb
@@ -0,0 +1,19 @@
+module GovukPublishingComponents
+  module SharedAccessibilityCriteria
+    def self.link
+      "
+Links in the component must:
+
+- accept focus
+- be focusable with a keyboard
+- be usable with a keyboard
+- indicate when it has focus
+- change in appearance when touched (in the touch-down state)
+- change in appearance when hovered
+- be usable with touch
+- be usable with [voice commands](https://www.w3.org/WAI/perspectives/voice.html)
+- have visible text
+      "
+    end
+  end
+end
