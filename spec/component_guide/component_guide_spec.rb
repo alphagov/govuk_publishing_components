2b607e2714d8450d76117c6e4b3676ffbfeb18cf
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-09-11 11:04:14 +0000

    Add tests for shared accessibility criteria

--- spec/component_guide/component_guide_spec.rb
+++ spec/component_guide/component_guide_spec.rb
@@ -1,6 +1,12 @@
 require "spec_helper"
 
 describe 'Component guide' do
+  before(:each) do
+    GovukPublishingComponents.configure do |config|
+      config.static = false
+    end
+  end
+
   # Load ordering test can only fail if run as the first test in suite
   # https://github.com/rails/rails/issues/12168
   it 'renders using gem layout not app layout after viewing a page on the application' do
@@ -183,6 +189,28 @@ describe 'Component guide' do
     expect(body).to include('render <span class="token string">\'govuk_component/test-static-component\'</span>')
     within '.component-guide-preview' do
       expect(page).to have_selector('.a-test-static-component')
+    end
+  end
+
+  it 'shows shared accessibility criteria' do
+    visit '/component-guide/test-component-with-shared-accessibility-criteria'
+
+    within '.component-accessibility-criteria' do
+      within(shared_component_selector("govspeak")) do
+        content = JSON.parse(page.text).fetch("content").squish
+        expect(content).to eq('<ul> <li>This is some criteria in a list</li> </ul> <p>Links in the component must:</p> <ul> <li>accept focus</li> <li>be focusable with a keyboard</li> <li>be usable with a keyboard</li> <li>indicate when it has focus</li> <li>change in appearance when touched (in the touch-down state)</li> <li>change in appearance when hovered</li> <li>be usable with touch</li> <li>be usable with <a rel="external" href="https://www.w3.org/WAI/perspectives/voice.html">voice commands</a> </li> <li>have visible text</li> </ul>')
+      end
+    end
+  end
+
+  it 'shows shared accessibility criteria when no other accessibility criteria are included' do
+    visit '/component-guide/test-component-with-shared-accessibility-criteria-only'
+
+    within '.component-accessibility-criteria' do
+      within(shared_component_selector("govspeak")) do
+        content = JSON.parse(page.text).fetch("content").squish
+        expect(content).to eq('<p>Links in the component must:</p> <ul> <li>accept focus</li> <li>be focusable with a keyboard</li> <li>be usable with a keyboard</li> <li>indicate when it has focus</li> <li>change in appearance when touched (in the touch-down state)</li> <li>change in appearance when hovered</li> <li>be usable with touch</li> <li>be usable with <a rel="external" href="https://www.w3.org/WAI/perspectives/voice.html">voice commands</a> </li> <li>have visible text</li> </ul>')
+      end
     end
   end
 end
