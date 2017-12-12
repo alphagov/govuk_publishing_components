d4f6eb764c2010a4237901cdf7a251da85e4a400
Tijmen Brommet <tijmen@gmail.com>
2017-12-12 18:01:35 +0000

    Remove Rails cruft from dummy application
    The dummy app is being used by the specs, but it's hard to find out
    what the relevant bits in the application are. This removes anything
    that isn't needed to run the tests.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,8 +3,6 @@ require_relative 'boot'
 require 'action_controller/railtie'
 require 'action_view/railtie'
 require 'action_mailer/railtie'
-require 'active_job/railtie'
-require 'action_cable/engine'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 # Require jasmine at runtime allow the app:jasmine:ci task to build correctly
