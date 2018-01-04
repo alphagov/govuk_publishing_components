680c08ec626606b1eafe2f332a925390c59a4a4d
Chris Patuzzo <chris@patuzzo.co.uk>
2018-01-04 14:19:12 +0000

    Make html_for mandatory for labels and document it

--- spec/components/label_test_spec.rb
+++ spec/components/label_test_spec.rb
@@ -12,27 +12,44 @@ describe "Label", type: :view do
   end
 
   it "renders label with text" do
-    render_component(text: "National Insurance number")
+    render_component(
+      text: "National Insurance number",
+      html_for: "id-that-matches-input",
+    )
 
-    assert_select ".gem-c-label__text", text: "National Insurance number"
+    assert_select(
+      ".gem-c-label__text[for='id-that-matches-input']",
+      text: "National Insurance number"
+    )
   end
 
   it "renders label with text and hint text" do
     render_component(
       text: "National Insurance number",
+      html_for: "id-that-matches-input",
       hint_id: "should-match-aria-describedby-input",
       hint_text: "It’s on your National Insurance card, benefit letter, payslip or P60. For example, ‘QQ 12 34 56 C’."
     )
 
-    assert_select ".gem-c-label__text", text: "National Insurance number"
+    assert_select(
+      ".gem-c-label__text[for='id-that-matches-input']",
+      text: "National Insurance number"
+    )
     assert_select ".gem-c-label__hint", text: "It’s on your National Insurance card, benefit letter, payslip or P60. For example, ‘QQ 12 34 56 C’."
     assert_select ".gem-c-label__hint[id=should-match-aria-describedby-input]"
   end
 
   it "renders label with bold text" do
-    render_component(text: "National Insurance number", bold: true)
+    render_component(
+      text: "National Insurance number",
+      html_for: "id-that-matches-input",
+      bold: true,
+    )
 
-    assert_select ".gem-c-label__text", text: "National Insurance number"
+    assert_select(
+      ".gem-c-label__text[for='id-that-matches-input']",
+      text: "National Insurance number"
+    )
     assert_select ".gem-c-label--bold"
   end
 end
