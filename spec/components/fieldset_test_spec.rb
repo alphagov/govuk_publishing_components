ba8ba5310084a983eca9767c53a99f4aef0c082e
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-02-16 20:45:23 +0000

    Make legend optional on a fieldset
    legend is an optional child of a fieldset element however this component
    previously required you to have one which meant if you couldn't use the
    legend for some reason you would have to abandon the component.

--- spec/components/fieldset_test_spec.rb
+++ spec/components/fieldset_test_spec.rb
@@ -5,10 +5,8 @@ describe "Fieldset", type: :view do
     render file: "govuk_publishing_components/components/_fieldset", locals: locals
   end
 
-  it "fails to render when no data is given" do
-    assert_raises do
-      render_component({})
-    end
+  it "does not render anything if no data is passed" do
+    assert_empty render_component({})
   end
 
   it "renders a fieldset correctly" do
@@ -18,6 +16,15 @@ describe "Fieldset", type: :view do
     )
 
     assert_select ".gem-c-fieldset__legend", text: 'Do you have a passport?'
+    assert_select ".gem-c-fieldset", text: /Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad neque, maxime est ea laudantium totam fuga!/
+  end
+
+  it "renders a fieldset without a legend" do
+    render_component(
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad neque, maxime est ea laudantium totam fuga!'
+    )
+
+    assert_select ".gem-c-fieldset__legend", false
     assert_select ".gem-c-fieldset", text: /Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad neque, maxime est ea laudantium totam fuga!/
   end
 end
