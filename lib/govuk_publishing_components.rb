ced9a10a97dfd06580f05476ffb3de52dfbb48de
Tijmen Brommet <tijmen@gmail.com>
2018-03-16 14:58:54 +0000

    First version of the contextual sidebar component
    This is the only sidebar you'll ever need. Its purpose is to capture
    all the logic for sidebars in a single place so frontend apps become
    really simple.
    
    This sidebar component is a refactor of what's currently in
    government-frontend.

--- lib/govuk_publishing_components.rb
+++ lib/govuk_publishing_components.rb
@@ -2,6 +2,7 @@ require "govuk_publishing_components/config"
 require "govuk_publishing_components/engine"
 require "govuk_publishing_components/presenters/step_by_step_nav_helper"
 require "govuk_publishing_components/presenters/related_navigation_helper"
+require "govuk_publishing_components/presenters/contextual_sidebar"
 
 require "govuk_publishing_components/app_helpers/step_nav"
 require "govuk_publishing_components/app_helpers/step_nav_helper"
