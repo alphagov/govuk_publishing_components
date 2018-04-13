25f8d52fef1ca07354c68b76368039ba5b77bea7
Tijmen Brommet <tijmen@gmail.com>
2018-04-13 08:33:02 +0100

    Allow rspec to run with --only-failures
    `rspec --only-failures` will now run only the tests that failed in the
    last run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   config.include Capybara::DSL
   config.include Helpers::Components, type: :view
 
+  config.example_status_persistence_file_path = "tmp/failures.txt"
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
