b33f3ff46458f9467af5db2cea867853f259e819
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-04-26 13:02:48 +0000

    Make components CSS available to static
    - applications include the styles for the components as part of the application's css, but static does not
    - this means gem components viewed in the static component guide are not styled, which can cause accessibility errors to trigger
    - adds logic and a couple of stylesheets to pull in the component styles if static is the application

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,6 @@
-Rails.application.config.assets.precompile += %w(govuk_publishing_components/component_guide.css govuk_publishing_components/search-button.png)
+Rails.application.config.assets.precompile += %w(
+  govuk_publishing_components/component_guide.css
+  component_guide/all_components.css
+  component_guide/all_components_print
+  govuk_publishing_components/search-button.png
+)
