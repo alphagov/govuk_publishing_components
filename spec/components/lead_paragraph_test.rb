8601a6212085e6255f52182ba3ee68b551ec5c60
Tijmen Brommet <tijmen@gmail.com>
2018-05-18 10:51:32 +0100

    Rename lead paragraph CSS classes

--- spec/components/lead_paragraph_test.rb
+++ spec/components/lead_paragraph_test.rb
@@ -9,7 +9,7 @@ class LeadParagraphTest < ComponentTestCase
 
   def assert_lead_paragraph_matches(text, expected_text)
     render_component(text: text)
-    assert_select ".pub-c-lead-paragraph", text: expected_text
+    assert_select ".gem-c-lead-paragraph", text: expected_text
   end
 
   test "renders nothing without a description" do
@@ -18,7 +18,7 @@ class LeadParagraphTest < ComponentTestCase
 
   test "renders a lead paragraph" do
     render_component(text: 'UK Visas and Immigration is making changes to the Immigration Rules affecting various categories.')
-    assert_select ".pub-c-lead-paragraph", text: "UK Visas and Immigration is making changes to the Immigration Rules affecting various#{NBSP}categories."
+    assert_select ".gem-c-lead-paragraph", text: "UK Visas and Immigration is making changes to the Immigration Rules affecting various#{NBSP}categories."
   end
 
   [
