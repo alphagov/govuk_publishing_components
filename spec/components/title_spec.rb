da1e4d953c2df4110b00ebb3490b7f1fac7b5a7a
Tijmen Brommet <tijmen@gmail.com>
2018-05-18 11:12:51 +0100

    Rewrite title component test to rspec

--- spec/components/title_spec.rb
+++ spec/components/title_spec.rb
@@ -1,32 +1,32 @@
-require 'govuk_component_test_helper'
+require "rails_helper"
 
-class TitleComponentTestCase < ComponentTestCase
+describe "Title", type: :view do
   def component_name
     "title"
   end
 
-  test "error if no title" do
-    assert_raise do
+  it "error if no title" do
+    assert_raises do
       render_component({})
     end
   end
 
-  test "title text appears" do
+  it "title text appears" do
     render_component(title: "Hello World")
     assert_select ".pub-c-title__text", text: "Hello World"
   end
 
-  test "title context appears" do
+  it "title context appears" do
     render_component(title: "Hello World", context: "Format")
     assert_select ".pub-c-title__context", text: "Format"
   end
 
-  test "title context link appears" do
+  it "title context link appears" do
     render_component(title: "Hello World", context: { text: "Format", href: "/format", data: { tracking: true } })
     assert_select ".pub-c-title__context-link[href='/format'][data-tracking]", text: "Format"
   end
 
-  test "applies title length if supplied" do
+  it "applies title length if supplied" do
     render_component(title: "Hello World", context: "format", average_title_length: 'long')
     assert_select ".pub-c-title .pub-c-title__text--long", text: "Hello World"
   end
