f51fbedb9547ad9b400c69d2745b0ebc78e7bfab
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-05-18 06:01:45 +0000

    Add extra links

--- spec/components/image_card_spec.rb
+++ spec/components/image_card_spec.rb
@@ -30,6 +30,18 @@ describe "ImageCard", type: :view do
     assert_select ".gem-c-image-card .gem-c-image-card__description", text: 'description'
   end
 
+  it "renders extra links" do
+    render_component(href: '#', extra_links: [{href: '/1', text: 'link1'}, {href: '/2', text: 'link2'}])
+    assert_select ".gem-c-image-card__list .gem-c-image-card__list-item a[href='/1']", text: 'link1'
+    assert_select ".gem-c-image-card__list .gem-c-image-card__list-item a[href='/2']", text: 'link2'
+  end
+
+  it "renders extra links without indent" do
+    render_component(href: '#', extra_links: [{href: '/1', text: 'link1'}], extra_links_no_indent: true)
+    assert_select ".gem-c-image-card__list"
+    assert_select ".gem-c-image-card__list.gem-c-image-card__list--indented", false
+  end
+
   it "renders a large version" do
     render_component(href: '#', large: true)
     assert_select ".gem-c-image-card.gem-c-image-card--large"
