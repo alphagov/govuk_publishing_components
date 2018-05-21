bc8a6c85b380f0bbbddc4bb3563a288346692522
Tijmen Brommet <tijmen@gmail.com>
2018-05-21 12:42:40 +0100

    Add some test pages to dummy app

---
+++ spec/dummy/app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+  layout 'admin'
+
+  before_action do
+    response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
+  end
+
+  def index
+  end
+end
