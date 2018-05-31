fe30fadfa2f3200575a6bedee63d3878379a7213
Tijmen Brommet <tijmen@gmail.com>
2018-05-31 12:25:13 +0100

    Stop inserting &nbsp; between last 2 words
    This feature was added when the component lived in Static. It replaces
    the space between the last 2 words with a non-breaking space, so we'll
    never have a dangling word in a paragraph.
    
    This PR removes the feature because:
    
    1. This component was recently moved into the gem. While converting
    applications to use it, I find that it turns out to be very hard to
    maintain this feature without jumping through a lot of hoops. For
    example, a [simple test like this][1] doesn't work, because
    `@description` doesn't have the nbsp, and the actual page does. This is
    *very* surprising, even to me -- and I ported this feature so I
    should've known.
    
    2. The Sanitisation code is pretty inscrutable, and introduces
    potential security vulnerabilities.
    
    [1]:
    https://github.com/alphagov/government-frontend/blob/23ab4380b09e1e76bfb
    ab70079530900a1f3e7dc/test/integration/specialist_document_test.rb#L34

--- spec/components/lead_paragraph_spec.rb
+++ spec/components/lead_paragraph_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe "Lead paragraph", type: :view do
-  NBSP = HTMLEntities.new.decode('&nbsp;')
-
   def component_name
     "lead_paragraph"
   end
@@ -18,22 +16,6 @@ describe "Lead paragraph", type: :view do
 
   it "renders a lead paragraph" do
     render_component(text: 'UK Visas and Immigration is making changes to the Immigration Rules affecting various categories.')
-    assert_select ".gem-c-lead-paragraph", text: "UK Visas and Immigration is making changes to the Immigration Rules affecting various#{NBSP}categories."
-  end
-
-  [
-    { text: 'this and that', expected: "this and#{NBSP}that", description: 'non breaking space between last two words' },
-    { text: 'this and that.', expected: "this and#{NBSP}that.", description: 'non breaking space between last two words with trailing space' },
-    { text: 'this and that.', expected: "this and#{NBSP}that.", description: 'non breaking space between last two words at end of sentence' },
-    { text: 'this and that. ', expected: "this and#{NBSP}that.", description: 'non breaking space between last two words with trailing space after sentence' },
-    { text: "multiline\nthis and that", expected: "multiline\nthis and#{NBSP}that", description: 'non breaking space between last two words with new lines' },
-    { text: "this and that\n\n", expected: "this and#{NBSP}that", description: 'non breaking space between last two words with multiline input' },
-    { text: "this", expected: "this", description: 'single word input gives single word output' },
-    { text: "&lt;b&gt;this&lt;b&gt; &amp; that&nbsp;thing", expected: "<b>this<b> & that#{NBSP}thing", description: 'non breaking space output is not unsafe given special characters' },
-    { text: "&lt;b&gt;this&lt;b&gt; &amp;&nbsp;that", expected: "<b>this<b> &#{NBSP}that", description: 'non breaking space output is not unsafe given concatenated special characters' }
-  ].each do |test_params|
-    it test_params[:description] do
-      assert_lead_paragraph_matches(test_params[:text], test_params[:expected])
-    end
+    assert_select ".gem-c-lead-paragraph", text: "UK Visas and Immigration is making changes to the Immigration Rules affecting various categories."
   end
 end
