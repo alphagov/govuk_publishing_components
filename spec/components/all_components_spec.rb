081fa4930cea6cbf9d3b60304ae29bbcd3222641
Tijmen Brommet <tijmen@gmail.com>
2018-06-05 16:49:49 +0100

    Allow passing in safe HTML
    When the HTML has been declared safe (either by constructing it
    properly, or using `html_safe`, or `raw) we won't mark it as safe
    again. This will prevent accidental marking as safe of unsafe content.
    
    Note that `raw foo` is the same as `foo.html_safe`.

--- spec/components/all_components_spec.rb
+++ spec/components/all_components_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "All components" do
   it "doesn't use `html_safe`" do
-    files_with_html_safe = `grep -rni "html_safe" app/views/govuk_publishing_components/components`.lines
+    files_with_html_safe = `grep -rni "html_safe " app/views/govuk_publishing_components/components`.lines
 
     expect(files_with_html_safe).to be_empty
   end
