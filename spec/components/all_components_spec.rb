8f4de097e0d4556c78797ccc77bc712807ec39bd
Tijmen Brommet <tijmen@gmail.com>
2018-06-06 13:25:03 +0100

    Test that the documentation is correct

--- spec/components/all_components_spec.rb
+++ spec/components/all_components_spec.rb
@@ -8,10 +8,19 @@ describe "All components" do
     describe component_name do
       next if template.in?(%w[_govspeak.html.erb])
 
-      it "is documented" do
-        yaml_file = "#{__dir__}/../../app/views/govuk_publishing_components/components/docs/#{component_name}.yml"
+      yaml_file = "#{__dir__}/../../app/views/govuk_publishing_components/components/docs/#{component_name}.yml"
 
+      it "is documented" do
         expect(File).to exist(yaml_file)
+      end
+
+      it "has the correct documentation" do
+        yaml = YAML.load_file(yaml_file)
+
+        expect(yaml["name"]).not_to be_nil
+        expect(yaml["description"]).not_to be_nil
+        expect(yaml["examples"]).not_to be_nil
+        expect(yaml["accessibility_criteria"]).not_to be_nil
       end
 
       it "doesn't use `html_safe`" do
