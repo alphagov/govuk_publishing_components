d8084e935c6facadc590aa0a869b201305014d81
Tijmen Brommet <tijmen@gmail.com>
2018-06-06 13:22:33 +0100

    Test that every component has a doc YAML file
    This makes it easier to verify that all components have been documented.

--- spec/components/all_components_spec.rb
+++ spec/components/all_components_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 describe "All components" do
   Dir.glob("app/views/govuk_publishing_components/components/*.erb").each do |filename|
     template = filename.split('/').last
+    component_name = template.sub('_', '').sub('.html.erb', '')
 
-    describe template do
+    describe component_name do
       next if template.in?(%w[_govspeak.html.erb])
+
+      it "is documented" do
+        yaml_file = "#{__dir__}/../../app/views/govuk_publishing_components/components/docs/#{component_name}.yml"
+
+        expect(File).to exist(yaml_file)
+      end
 
       it "doesn't use `html_safe`" do
         file = File.read(filename)
