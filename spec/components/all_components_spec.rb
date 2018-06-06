8dd3ff7a5fccd4f94b4d44b7e4348d8eb786afd7
Tijmen Brommet <tijmen@gmail.com>
2018-06-06 13:26:59 +0100

    Only skip the html_safe test for govspeak

--- spec/components/all_components_spec.rb
+++ spec/components/all_components_spec.rb
@@ -6,8 +6,6 @@ describe "All components" do
     component_name = template.sub('_', '').sub('.html.erb', '')
 
     describe component_name do
-      next if template.in?(%w[_govspeak.html.erb])
-
       yaml_file = "#{__dir__}/../../app/views/govuk_publishing_components/components/docs/#{component_name}.yml"
 
       it "is documented" do
@@ -23,7 +21,7 @@ describe "All components" do
         expect(yaml["accessibility_criteria"]).not_to be_nil
       end
 
-      it "doesn't use `html_safe`" do
+      it "doesn't use `html_safe`", skip: template.in?(%w[_govspeak.html.erb]) do
         file = File.read(filename)
 
         expect(file).not_to match 'html_safe'
