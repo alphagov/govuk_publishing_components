1255ca19a3aeaaac0bc905366df580d040afd961
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-06-07 11:12:53 +0000

    Add organisation logo component from static
    - now uses gem-c css namespace
    - modified to use a helper method
    - gem branding model replaces existing branding mechanism
    - test for no link added now included, test names rewritten
    - example for without link added to documentation
    - crest images still live in whitehall
    - css restructured but essentially the same

---
+++ lib/govuk_publishing_components/presenters/organisation_logo_helper.rb
@@ -0,0 +1,36 @@
+module GovukPublishingComponents
+  module Presenters
+    class OrganisationLogoHelper
+      include ActionView::Helpers
+      include ActionView::Context
+
+      attr_reader :name, :url, :crest, :image, :logo_image_src, :logo_image_alt
+
+      def initialize(local_assigns)
+        @name = local_assigns[:organisation][:name]
+        @url = local_assigns[:organisation][:url]
+        @crest = local_assigns[:organisation][:crest]
+        @image = local_assigns[:organisation][:image] || false
+        if @image
+          @logo_image_src = local_assigns[:organisation][:image][:url] || false
+          @logo_image_alt = local_assigns[:organisation][:image][:alt_text] || false
+        end
+      end
+
+      def logo_content
+        if image
+          image_tag(logo_image_src, alt: logo_image_alt, class: "gem-c-organisation-logo__image")
+        else
+          content_tag('span', name, class: "gem-c-organisation-logo__name")
+        end
+      end
+
+      def logo_container_class
+        logo_class = "gem-c-organisation-logo__container"
+        logo_class = "#{logo_class} gem-c-organisation-logo__link" if url
+        logo_class = "#{logo_class} gem-c-organisation-logo__crest gem-c-organisation-logo__crest--#{crest}" if crest
+        logo_class
+      end
+    end
+  end
+end
---
+++ spec/components/organisation_logo_spec.rb
@@ -0,0 +1,73 @@
+require "rails_helper"
+
+describe "Organisation logo", type: :view do
+  def component_name
+    "organisation_logo"
+  end
+
+  it "error if no parameters passed in" do
+    assert_raises do
+      render_component({})
+    end
+  end
+
+  it "shows an organisation name" do
+    render_component(organisation: { name: "Organisation name" })
+    assert_select ".gem-c-organisation-logo .gem-c-organisation-logo__container", text: "Organisation name"
+  end
+
+  it "adds branding to the wrapping container" do
+    render_component(organisation: { name: "Branded", brand: "department-for-international-trade" })
+    assert_select ".gem-c-organisation-logo.brand--department-for-international-trade", text: "Branded"
+  end
+
+  it "includes a link when a URL is provided" do
+    render_component(organisation: { name: "Linked", url: "/somewhere" })
+    assert_select ".gem-c-organisation-logo a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[href='/somewhere']", text: "Linked"
+  end
+
+  it "doesn't include a link when a URL is not provided" do
+    render_component(organisation: { name: "Linked" })
+    assert_select "a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[href='/somewhere']", false
+    assert_select ".gem-c-organisation-logo__container"
+  end
+
+  it "adds a crest class when specified" do
+    render_component(organisation: { name: "Crested", crest: "single-identity" })
+    assert_select ".gem-c-organisation-logo__container.gem-c-organisation-logo__crest.gem-c-organisation-logo__crest--single-identity"
+  end
+
+  it "renders an image when specified" do
+    render_component(organisation: { name: "Custom image", image: { url: "/url", alt_text: "alt" } })
+    assert_select ".gem-c-organisation-logo__container img[src='/url'][alt='alt']"
+  end
+
+  it "adds data tracking attributes when specified" do
+    data_attributes = {
+      track_category: "someLinkClicked",
+      track_action: 1,
+      track_label: "/some-link",
+      track_options: {
+        dimension28: 2,
+        dimension29: "Organisation link"
+      }
+    }
+
+    render_component(organisation: { url: "/some-link", data_attributes: data_attributes })
+
+    assert_select ".gem-c-organisation-logo[data-module='track-click']"
+    assert_select ".gem-c-organisation-logo a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[data-track-category='someLinkClicked']"
+    assert_select ".gem-c-organisation-logo a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[data-track-action='1']"
+    assert_select ".gem-c-organisation-logo a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[data-track-label='/some-link']"
+    assert_select ".gem-c-organisation-logo a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[data-track-options='{\"dimension28\":2,\"dimension29\":\"Organisation link\"}']"
+  end
+
+  it "doesn't add data tracking attributes when no link is specified" do
+    data_attributes = {
+      track_category: "someLinkClicked"
+    }
+
+    render_component(organisation: { data_attributes: data_attributes })
+    assert_select ".gem-c-organisation-logo a.gem-c-organisation-logo__container.gem-c-organisation-logo__link[data-track-category='someLinkClicked']", false
+  end
+end
