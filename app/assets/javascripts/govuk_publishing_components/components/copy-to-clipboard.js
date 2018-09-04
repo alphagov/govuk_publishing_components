dd24e5ec06166fc1001d5e83c9fb82a560e7c7f6
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2018-09-04 14:26:17 +0100

    Use existing input instead of creating a new one

--- app/assets/javascripts/govuk_publishing_components/components/copy-to-clipboard.js
+++ app/assets/javascripts/govuk_publishing_components/components/copy-to-clipboard.js
@@ -11,17 +11,9 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       copyButton.addEventListener('click', function (event) {
         event.preventDefault()
         var input = element[0].querySelector('.gem-c-input')
-        copyToClipboard(input.value)
+        input.select()
+        document.execCommand('copy')
       })
-    }
-
-    function copyToClipboard (text) {
-      var temp = document.createElement('input')
-      temp.value = text
-      document.body.appendChild(temp)
-      temp.select()
-      document.execCommand('copy')
-      document.body.removeChild(temp)
     }
   }
 })(window, window.GOVUK)
