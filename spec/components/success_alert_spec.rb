aa96fbe7fe667485888d2947501cbf446f5c592f
Tijmen Brommet <tijmen@gmail.com>
2018-09-05 13:45:28 +0100

    Support a description for Success Alert component
    This will be used in content publisher to support flash messages with
    bodies.
    
    https://trello.com/c/9szY348f

---
+++ spec/components/success_alert_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "Success Alert", type: :view do
+  def component_name
+    "success_alert"
+  end
+
+  it "shows the error message" do
+    render_component(message: "Foo")
+    assert_select ".gem-c-success-alert__message", text: "Foo"
+  end
+
+  it "allows a block to be given for description" do
+    render_component(message: "Foo") do
+      "Bar"
+    end
+
+    assert_select ".gem-c-success-summary__title", text: "Foo"
+    assert_select ".gem-c-success-summary__body", text: "Bar"
+  end
+end
