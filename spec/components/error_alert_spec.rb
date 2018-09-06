dca00c1b39e87e7f9a73d9eaf944eb4fc599d5af
Tijmen Brommet <tijmen@gmail.com>
2018-09-06 15:29:48 +0100

    Add error alert component
    This is largely duplicated from the success alert component.

---
+++ spec/components/error_alert_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Error Alert", type: :view do
+  def component_name
+    "error_alert"
+  end
+
+  it "shows the error message" do
+    render_component(message: "Foo")
+    assert_select ".gem-c-error-alert__message", text: "Foo"
+  end
+
+  it "allows a block to be given for description" do
+    render_component(message: "Foo", description: "Bar")
+
+    assert_select ".gem-c-error-summary__title", text: "Foo"
+    assert_select ".gem-c-error-summary__body", text: "Bar"
+  end
+end
