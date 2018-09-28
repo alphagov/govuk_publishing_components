b89cbe22d0cd58a02acb35403500b98c48d78663
Tijmen Brommet <tijmen@gmail.com>
2018-09-28 15:30:24 +0100

    Add environment helper
    This code is used in content-publisher and content-data-admin, so we
    should put it here.

---
+++ lib/govuk_publishing_components/app_helpers/environment.rb
@@ -0,0 +1,18 @@
+module GovukPublishingComponents
+  module AppHelpers
+    class Environment
+      GOVUK_ENVIRONMENTS = {
+        "production" => "production",
+        "staging" => "staging",
+        "integration-blue-aws" => "integration",
+      }.freeze
+
+      # The "acceptance environment" we're in - not the same as Rails env.
+      #
+      # Can be "production", "staging", "integration", or "development"
+      def self.current_acceptance_environment
+        GOVUK_ENVIRONMENTS.fetch(ENV["ERRBIT_ENVIRONMENT_NAME"], "development")
+      end
+    end
+  end
+end
---
+++ spec/lib/app_helpers/environment_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe GovukPublishingComponents::AppHelpers::Environment do
+  describe ".current_acceptance_environment" do
+    it "returns development by default" do
+      expect(environment_name_for(nil)).to eql("development")
+      expect(environment_name_for("something-else")).to eql("development")
+    end
+
+    it "returns integration as well" do
+      expect(environment_name_for("integration-blue-aws")).to eql("integration")
+    end
+  end
+
+  def environment_name_for(actual_environment_name)
+    previous = ENV["ERRBIT_ENVIRONMENT_NAME"]
+    ENV["ERRBIT_ENVIRONMENT_NAME"] = actual_environment_name
+    name = GovukPublishingComponents::AppHelpers::Environment.current_acceptance_environment
+    ENV["ERRBIT_ENVIRONMENT_NAME"] = previous
+    name
+  end
+end
