898d144af87b065382e139dc6e430661cec7cb1c
Tijmen Brommet <tijmen@gmail.com>
2018-10-11 13:49:10 +0100

    Don't include Javascripts twice in component guide
    The component guide already includes these.

--- app/assets/javascripts/component_guide/application.js
+++ app/assets/javascripts/component_guide/application.js
@@ -1,4 +1,2 @@
-//= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/all_components
 //= require_tree ./vendor
 //= require_tree .
--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -1,2 +1,3 @@
 //= require govuk/modules
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/all_components
