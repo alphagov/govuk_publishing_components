494f406b14e048a4063f95f6c3806db78ff97a77
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-11-23 07:28:09 +0000

    Move heading logic into helper

--- lib/govuk_publishing_components/presenters/checkboxes_helper.rb
+++ lib/govuk_publishing_components/presenters/checkboxes_helper.rb
@@ -1,11 +1,15 @@
 module GovukPublishingComponents
   module Presenters
     class CheckboxesHelper
-      attr_reader :items, :css_classes, :error, :has_conditional, :has_nested, :id, :heading, :hint_text, :is_page_heading
+      include ActionView::Helpers
+      include ActionView::Context
+
+      attr_reader :items, :name, :css_classes, :error, :has_conditional, :has_nested, :id, :hint_text
 
       def initialize(options)
-        @items = options[:items] ? options[:items] : []
-        @css_classes = %w( gem-c-checkboxes govuk-form-group )
+        @items = options[:items] || []
+        @name = options[:name]
+        @css_classes = %w(gem-c-checkboxes govuk-form-group)
         @css_classes << "govuk-form-group--error" if options[:error]
         @error = true if options[:error]
 
@@ -13,10 +17,25 @@ module GovukPublishingComponents
         @has_conditional = options[:items].any? { |item| item.is_a?(Hash) && item[:conditional] }
         @has_nested = options[:items].any? { |item| item.is_a?(Hash) && item[:items] }
 
-        @id = options[:id] ? options[:id] : "checkboxes-#{SecureRandom.hex(4)}"
+        @id = options[:id] || "checkboxes-#{SecureRandom.hex(4)}"
         @heading = options[:heading]
-        @hint_text = options[:hint_text] ? options[:hint_text] : "Select all that apply."
         @is_page_heading = options[:is_page_heading]
+        @hint_text = options[:hint_text] || "Select all that apply."
+      end
+
+      def heading_markup
+        return unless @heading.present?
+
+        if @is_page_heading
+          content_tag(
+            :legend,
+            class: "govuk-fieldset__legend govuk-fieldset__legend--xl gem-c-title gem-c-title--margin-bottom-5"
+          ) do
+            content_tag(:h1, @heading, class: "gem-c-title__text")
+          end
+        else
+          content_tag(:legend, @heading, class: "govuk-fieldset__legend govuk-fieldset__legend--m")
+        end
       end
     end
   end
