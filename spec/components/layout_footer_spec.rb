d7694a2ab82c975b2b4a0de7800c4ba886984ee7
default <vagrant@development.development>
2018-12-12 09:13:37 +0000

    Make layout footer support spawning new windows for links
    Enhancing the layout footer component so that it can make pressing a link open a new window
    or change the page of the current window.
    - Making use of link_to in _layout_footer.html.erb to handle the target attribute and
      explicitly setting target to '' if no target attribute is passed to the component.
    - Adding a new example to layout_footer.yml so users can see examples of using the
      layout footer component to open links in either the same or a new window.

--- spec/components/layout_footer_spec.rb
+++ spec/components/layout_footer_spec.rb
@@ -36,4 +36,39 @@ describe "Layout footer", type: :view do
     assert_select ".govuk-footer__navigation .govuk-footer__heading", text: "Services and information"
     assert_select ".govuk-footer__navigation .govuk-footer__list--columns-2 .govuk-footer__link[href='/browse/benefits']", text: "Benefits"
   end
+
+  it "renders links that can either spawn a new window or use the same window" do
+    render_component(
+      navigation: [
+        {
+          title: "Services and information",
+          columns: 2,
+          items: [
+            {
+              href: "/browse/benefits",
+              text: "Benefits",
+              target: "_blank"
+            }
+          ]
+        }
+      ]
+    )
+    assert_select ".govuk-footer__navigation .govuk-footer__list--columns-2 .govuk-footer__link[target='_blank']", text: "Benefits"
+
+    render_component(
+      navigation: [
+        {
+          title: "Services and information",
+          columns: 2,
+          items: [
+            {
+              href: "/browse/benefits",
+              text: "Benefits"
+            }
+          ]
+        }
+      ]
+    )
+    assert_select ".govuk-footer__navigation .govuk-footer__list--columns-2 .govuk-footer__link[target='']", text: "Benefits"
+  end
 end
