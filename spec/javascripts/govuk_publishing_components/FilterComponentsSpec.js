c7c3f006850dc967d52a9b8589ce205b2a563698
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-01-04 15:23:43 +0000

    Add search filter to component guide

---
+++ spec/javascripts/govuk_publishing_components/FilterComponentsSpec.js
@@ -0,0 +1,72 @@
+/* global describe, afterEach, it, expect */
+
+var FilterComponents = window.GOVUK.FilterComponents;
+
+function addFormInput() {
+  var form = document.createElement('form');
+  form.setAttribute('data-module', 'filter-components');
+  document.body.appendChild(form);
+};
+
+function addComponents() {
+  var list = document.createElement('ul');
+  list.classList.add("component-list");
+
+  // Set up accordion component
+  var accordionLi = document.createElement('li');
+  var accordionLink = document.createElement('a');
+  var accordionP = document.createElement('p');
+  accordionLink.innerText = "Accordion";
+  accordionP.innerText = "A group of expanding/collapsing sections";
+  accordionLi.setAttribute('id', 'accordion');
+  accordionLi.appendChild(accordionP);
+  accordionLi.appendChild(accordionLink);
+
+  // Set up back link component
+  var backLinkLi = document.createElement('li');
+  var backLink = document.createElement('a');
+  var backLinkP = document.createElement('p');
+  backLink.innerText = "Back link";
+  backLinkP.innerText = "A link used to help users get back, useful when not using other navigation such as breadcrumbs";
+  backLinkLi.setAttribute('id', 'back-link');
+  backLinkLi.appendChild(backLinkP);
+  backLinkLi.appendChild(backLink);
+
+  list.appendChild(accordionLi);
+  list.appendChild(backLinkLi);
+
+  document.body.appendChild(list);
+};
+
+describe('FilterComponents', function() {
+  beforeAll(function() {
+    addFormInput();
+    addComponents();
+  });
+
+  it('hides all components that do not match search criteria', function() {
+    FilterComponents("Accordion");
+
+    expect(document.getElementById('back-link').classList.toString()).toBe('component-guide-hidden');
+  });
+
+  it('shows all components that match search criteria', function() {
+    FilterComponents("Accordion");
+
+    expect(document.getElementById('accordion').classList.toString()).toBe('');
+  });
+
+  it('searches both the component name and description', function() {
+    FilterComponents("navigation");
+
+    expect(document.getElementById('back-link').classList.toString()).toBe('');
+    expect(document.getElementById('accordion').classList.toString()).toBe('component-guide-hidden');
+  });
+
+  it('the search is not case-sensitive', function() {
+    FilterComponents("ACCORDION");
+
+    expect(document.getElementById('accordion').classList.toString()).toBe('');
+    expect(document.getElementById('back-link').classList.toString()).toBe('component-guide-hidden');
+  });
+});
