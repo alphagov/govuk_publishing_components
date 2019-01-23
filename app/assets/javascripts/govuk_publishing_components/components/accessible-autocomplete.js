7370071493e4a32a01c15bc3811535d81f13695d
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-01-23 11:00:10 +0000

    Extend accessible autocomplete onConfirm function
    - needed for finder-frontend's remove facet functionality, we need to be able to clear the autocomplete and not have the dropdown menu appear, triggered externally to the component

--- app/assets/javascripts/govuk_publishing_components/components/accessible-autocomplete.js
+++ app/assets/javascripts/govuk_publishing_components/components/accessible-autocomplete.js
@@ -7,8 +7,10 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   "use strict";
 
   Modules.AccessibleAutocomplete = function () {
+    var $selectElem;
+
     this.start = function ($element) {
-      var $selectElem = $element.find('select');
+      $selectElem = $element.find('select');
 
       var configOptions = {
         selectElement: document.getElementById($selectElem.attr('id')),
@@ -18,20 +20,36 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         defaultValue: ""
       };
 
-      configOptions.onConfirm = function(label) {
-        if ($selectElem.data('track-category') !== undefined && $selectElem.data('track-action') !== undefined) {
-          track($selectElem.data('track-category'), $selectElem.data('track-action'), label, $selectElem.data('track-options'));
-        }
-        // This is to compensate for the fact that the accessible-autocomplete library will not
-        // update the hidden select if the onConfirm function is supplied
-        // https://github.com/alphagov/accessible-autocomplete/issues/322
-        var value = $selectElem.children("option").filter(function () { return $(this).html() == label; }).val();
-        if (typeof value !== 'undefined') {
-          $selectElem.val(value).trigger( "change" );
-        }
-      };
+      configOptions.onConfirm = this.onConfirm;
 
       new accessibleAutocomplete.enhanceSelectElement(configOptions);
+      //attach the onConfirm function to data attr, to call it in finder-frontend when clearing facet tags
+      $selectElem.data('onconfirm', this.onConfirm);
+    };
+
+    this.onConfirm = function(label, removeDropDown) {
+      if ($selectElem.data('track-category') !== undefined && $selectElem.data('track-action') !== undefined) {
+        track($selectElem.data('track-category'), $selectElem.data('track-action'), label, $selectElem.data('track-options'));
+      }
+      // This is to compensate for the fact that the accessible-autocomplete library will not
+      // update the hidden select if the onConfirm function is supplied
+      // https://github.com/alphagov/accessible-autocomplete/issues/322
+      var value = $selectElem.children("option").filter(function () { return $(this).html() == label; }).val();
+      if (typeof value !== 'undefined') {
+        $selectElem.val(value).trigger( "change" );
+      }
+
+      // used to clear the autocomplete when clicking on a facet tag in finder-frontend
+      // very brittle but menu visibility is determined by autocomplete after this function is called
+      // setting autocomplete val to '' causes menu to appear, we don't want that, this solves it
+      // ideally will rewrite autocomplete to have better hooks in future
+      if (removeDropDown) {
+        $selectElem.closest('.gem-c-accessible-autocomplete').addClass('gem-c-accessible-autocomplete--hide-menu');
+        setTimeout(function() {
+          $('.autocomplete__menu').remove(); // this element is recreated every time the user starts typing
+          $selectElem.closest('.gem-c-accessible-autocomplete').removeClass('gem-c-accessible-autocomplete--hide-menu');
+        }, 100);
+      }
     };
 
     function track (category, action, label, options) {
