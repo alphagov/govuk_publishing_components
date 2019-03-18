da28d58a4190bd20903e830657639f77a82d4503
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-03-18 15:38:33 +0000

    Delete bind-polyfill.js

--- app/assets/javascripts/component_guide/vendor/bind-polyfill.js
+++
@@ -1,8 +0,0 @@
-if (typeof Function.prototype.bind === 'undefined') {
-  Function.prototype.bind = function (target) {
-    var f = this
-    return function() {
-      return f.apply(target, arguments)
-    }
-  }
-}
