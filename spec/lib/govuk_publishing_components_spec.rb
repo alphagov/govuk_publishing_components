83b670c01a236bd05b97c3def312772fc31079a3
Kevin Dew <kevindew@me.com>
2019-04-30 20:56:45 +0100

    Add method to render components outside Rails
    This adds a render method to GovukPublishingComponents which allows
    rendering a component outside of a view context. This is added
    specifically so that govspeak can call a component, but more generally
    this allows calling components from anywhere in the code.
    
    To allow this to work outside of a Rails environment the paths for views
    and locale files are loaded in. In a Rails application these will
    already be present.

---
+++ spec/lib/govuk_publishing_components_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe GovukPublishingComponents do
+  describe ".render" do
+    it "can render a component" do
+      rendered = GovukPublishingComponents.render(
+        "govuk_publishing_components/components/button"
+      )
+
+      expect(rendered).to match(/<button/)
+    end
+
+    it "can pass the component options" do
+      rendered = GovukPublishingComponents.render(
+        "govuk_publishing_components/components/button",
+        text: "My Button"
+      )
+
+      expect(rendered).to match(/My Button/)
+    end
+
+    it "can specify the locale to render the component in" do
+      rendered = GovukPublishingComponents.render(
+        "govuk_publishing_components/components/back_link",
+        locale: "cy",
+        href: "./",
+      )
+
+      expect(rendered).to match(/Yn ôl/)
+    end
+  end
+end
