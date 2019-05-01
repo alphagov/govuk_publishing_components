b30be705ecfc7631b85a0492e00b429b3483ec3c
Kevin Dew <kevindew@me.com>
2019-05-01 17:49:39 +0100

    Attachment link component
    This ports the attachment link styling that is currently in govspeak and
    Whitehall into govuk_publishing_components. It is based on [1].
    
    This code is unlikely to be imminently removed from either Govspeak or
    Whitehall as Govspeak uses it for Specialist Publisher attachments and
    Whitehall uses it for it's own rendering. However it is hoped that in
    time it can be eventually removed from both of these.
    
    This component is somewhat unusual in that it's expected to inherit
    styling from it's container. I felt this was better than specifying font
    styling for it as this would cause problems if it was ever used in
    sections with larger or smaller text. It ports over the same abbr styles
    that are used in govspeak for consistency, as there is so few of them I
    thought it was simpler to move them over than to create a mixin.
    
    This component is somewhat intertwined with the work the Publishing
    Workflow team are doing on attachments so I've marked this as
    experimental while this is under development. The intention is to remove
    the experimental flag once Content Publisher supports attachments for news
    (hopefully by June 2019).
    
    [1]: https://github.com/alphagov/govspeak/blob/c3a0742c87537a371108d097cea23688efa90a14/lib/govspeak.rb#L262-L269

---
+++ spec/components/attachment_link_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+describe "Attachment Link", type: :view do
+  def component_name
+    "attachment_link"
+  end
+
+  it "fails to render when no attachment is given" do
+    assert_raises do
+      render_component({})
+    end
+  end
+
+  it "renders an attachment link" do
+    render_component(attachment: { title: "Attachment", url: "https://gov.uk/attachment" })
+    assert_select ".gem-c-attachment-link"
+    assert_select "a[href='https://gov.uk/attachment']", text: "Attachment"
+  end
+
+  it "can have a target specified" do
+    render_component(
+      attachment: { title: "Attachment", url: "attachment" },
+      target: "_blank",
+    )
+    assert_select "a[href=attachment][target=_blank]"
+  end
+
+  it "can include attribute metadata" do
+    render_component(
+      attachment: {
+        title: "Attachment",
+        url: "attachment",
+        content_type: "text/xml",
+        filename: "attachment.xml",
+        file_size: 2048,
+        number_of_pages: 2,
+      },
+    )
+    assert_select "abbr.gem-c-attachment-link__abbr[title='XML Document']", text: "XML"
+    expect(rendered).to match(/2 KB/)
+    expect(rendered).to match(/2 pages/)
+  end
+end
