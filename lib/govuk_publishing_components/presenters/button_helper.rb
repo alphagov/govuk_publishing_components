13a5e66b43cf2a64a909567ac5005016ff25a540
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-05-09 11:33:07 +0100

    Prevent double click only for submit buttons

--- lib/govuk_publishing_components/presenters/button_helper.rb
+++ lib/govuk_publishing_components/presenters/button_helper.rb
@@ -13,7 +13,7 @@ module GovukPublishingComponents
         @info_text = local_assigns[:info_text]
         @rel = local_assigns[:rel]
         @default_data_attributes = { "prevent-double-click" => true }
-        @data_attributes = local_assigns[:data_attributes] ? local_assigns[:data_attributes].merge(@default_data_attributes) : @default_data_attributes
+        @data_attributes = local_assigns[:data_attributes]
         @margin_bottom = local_assigns[:margin_bottom]
         @target = local_assigns[:target]
         @type = local_assigns[:type]
@@ -32,7 +32,8 @@ module GovukPublishingComponents
         options[:role] = "button" if link?
         options[:type] = button_type
         options[:rel] = rel if rel
-        options[:data] = data_attributes if data_attributes
+        options[:data] = data_attributes ? data_attributes.merge(@default_data_attributes) : @default_data_attributes
+        options[:data] = data_attributes if button_type != "submit"
         options[:title] = title if title
         options[:target] = target if target
         options
