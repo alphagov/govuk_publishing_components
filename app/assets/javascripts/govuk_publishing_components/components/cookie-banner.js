7d4207ba5ee201938b7e97d751ba1eab286abf58
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-05-24 09:39:20 +0000

    Check the consent cookie before setting cookies
    We need to check what cookies a user has consented to before we set each cookie.
    
    This also tidies up the cookie banner code so the old banner and new banner are more separate. This will make it easier for us to
    remove the old banner code once the new banner has been deployed.

--- app/assets/javascripts/govuk_publishing_components/components/cookie-banner.js
+++ app/assets/javascripts/govuk_publishing_components/components/cookie-banner.js
@@ -10,42 +10,65 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     this.$module.showConfirmationMessage = this.showConfirmationMessage.bind(this)
     this.$module.setCookieConsent = this.setCookieConsent.bind(this)
 
+    this.$module.newCookieBanner = document.querySelector('.gem-c-cookie-banner--new')
+
+    // Temporary check while we have 2 banners co-existing.
+    // Once the new banner has been deployed, we will be able to remove code relating to the old banner
+    // Separating the code out like this does mean some repetition, but will make it easier to remove later
+    if (this.$module.newCookieBanner) {
+      this.setupNewCookieMessage()
+    } else {
+      this.setupCookieMessage()
+    }
+  }
+
+  CookieBanner.prototype.setupCookieMessage = function () {
+    this.$hideLink = this.$module.querySelector('a[data-hide-cookie-banner]')
+    if (this.$hideLink) {
+      this.$hideLink.addEventListener('click', this.$module.hideCookieMessage)
+    }
+
+    this.showCookieMessage()
+  }
+
+  CookieBanner.prototype.setupNewCookieMessage = function () {
+    this.$hideLink = this.$module.querySelector('button[data-hide-cookie-banner]')
+    if (this.$hideLink) {
+      this.$hideLink.addEventListener('click', this.$module.hideCookieMessage)
+    }
+
+    this.$acceptCookiesLink = this.$module.querySelector('button[data-accept-cookies]')
+    if (this.$acceptCookiesLink) {
+      this.$acceptCookiesLink.addEventListener('click', this.$module.setCookieConsent)
+    }
+
+    this.showNewCookieMessage()
+  }
+
+  CookieBanner.prototype.showCookieMessage = function () {
+    var shouldHaveCookieMessage = (this.$module && window.GOVUK.cookie('seen_cookie_message') !== 'true')
+
+    if (shouldHaveCookieMessage) {
+      this.$module.style.display = 'block'
+    }
+  }
+
+  CookieBanner.prototype.showNewCookieMessage = function () {
     var newCookieBanner = document.querySelector('.gem-c-cookie-banner--new')
 
     // Hide the cookie banner on the cookie settings page, to avoid circular journeys
     if (newCookieBanner && window.location.pathname === '/help/cookies') {
       this.$module.style.display = 'none'
-    } else {
-      this.$hideLink = this.$module.querySelector('a[data-hide-cookie-banner], button[data-hide-cookie-banner]')
-      if (this.$hideLink) {
-        this.$hideLink.addEventListener('click', this.$module.hideCookieMessage)
-      }
-
-      this.$acceptCookiesLink = this.$module.querySelector('button[data-accept-cookies]')
-      if (this.$acceptCookiesLink) {
-        this.$acceptCookiesLink.addEventListener('click', this.$module.setCookieConsent)
-      }
-
-      this.showCookieMessage()
     }
-  }
 
-  CookieBanner.prototype.showCookieMessage = function () {
-    var newCookieBanner = document.querySelector('.gem-c-cookie-banner--new')
-    var hasCookieMessage = (this.$module && window.GOVUK.cookie('seen_cookie_message') !== 'true')
-    if (hasCookieMessage) {
+    var shouldHaveCookieMessage = (this.$module && window.GOVUK.cookie('seen_cookie_message') !== 'true')
+
+    if (shouldHaveCookieMessage) {
       this.$module.style.display = 'block'
-    }
 
-    if (newCookieBanner && hasCookieMessage) {
+      // Set the default consent cookie if it isn't already present
       if (!window.GOVUK.cookie('cookie_policy')) {
         window.GOVUK.setDefaultConsentCookie()
-      }
-    } else if (!newCookieBanner) {
-      // Remove the consent cookie if we're using the old cookie banner
-      // TODO: this can be removed later when we switch to the new banner
-      if (window.GOVUK.cookie('cookie_policy')) {
-        window.GOVUK.cookie('cookie_policy', null)
       }
     }
   }
