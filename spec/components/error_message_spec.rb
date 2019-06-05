a45a4c7d8d06817556bdba3b0d9bdcd20b9b7123
Kevin Dew <kevindew@me.com>
2019-06-05 13:03:48 +0100

    Accept items on error_message component
    There is a common pattern for collections of errors, known as
    error_items. This an array of hashes where each entry may have a text
    attribute of the error, this pattern is common as it is the same input
    that is used for error summary.
    
    The conversion of this pattern into input for error message is common
    across this project with implementations of it in input component, date
    component, textarea component and radio component. Apps, such as Content
    Publisher, are even duplicating the code with in app code.
    
    This level of repetition isn't ideal - especially as it deals with raw
    HTML. So I've moved this to the error_message component to consolidate
    this logic.
    
    This also resolves a potential XSS issue as any item[:text] values will
    be escaped unless they are previously marked with html_safe.

--- spec/components/error_message_spec.rb
+++ spec/components/error_message_spec.rb
@@ -10,4 +10,14 @@ describe "Error message", type: :view do
 
     assert_select(".govuk-error-message", text: "Error: Please enter your National Insurance number")
   end
+
+  it "escapes HTML in items and allows HTML safe strings" do
+    render_component(items: [
+      { text: "Error where HTML is <strong>escaped</strong>" },
+      { text: "Error where HTML is <strong>maintained</strong>".html_safe },
+    ])
+
+    assert_select(".govuk-error-message", html: %r{Error where HTML is &lt;strong&gt;escaped&lt;/strong&gt;})
+    assert_select(".govuk-error-message", html: %r{Error where HTML is <strong>maintained</strong>})
+  end
 end
