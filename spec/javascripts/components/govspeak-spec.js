483e17c53775c30f0cfd9decae22f527e102c1a9
kr8n3r <jani@kraner.dev>
2019-06-05 14:12:42 +0100

    Replace Nomensa player with Youtube media player
    There are a number of accessibility issues with the current video
    player, so a prototype of shortlisted players was reviewed against
    the criteria in https://docs.google.com/document/d/1ZhrZH-nKCXs-uMz8HIXPx7FgAs4p3fsrmNMItDj_dt0/edit and the recommendation was to use just the Youtube iframe player with some tweaks:
    
    - Add a title attribute to the iframe, taken from the fallback link text,
    - Disable option to allow single key shortcuts (disablekb) due to WCAG SC 2.1.4
    - use the no-cookie version
    
    This commit also:
    
    - Avoids the YoutubeLinkEnhancement class having to be concerned with jQuery
    - Does the campaign cookie check before proceeding at all with any embeds (previously it checked this on each loop iteration)

--- spec/javascripts/components/govspeak-spec.js
+++ spec/javascripts/components/govspeak-spec.js
@@ -1,42 +1,60 @@
 /* eslint-env jasmine, jquery */
 /* global GOVUK */
 
-describe('Govspeak component', function () {
+describe('Govspeak', function () {
   var govspeakModule = new GOVUK.Modules.Govspeak()
 
-  it('embeds youtube videos', function () {
-    var $element = $(
-      '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
-        '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
-      '<div>'
-    )
-    govspeakModule.start($element)
-    expect($element.find('.media-player').length).toBe(1)
+  describe('youtube enhancement', function () {
+    var container
+
+    afterEach(function () {
+      document.body.removeChild(container)
+    })
+
+    it('embeds youtube videos', function () {
+      container = document.createElement('div')
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
+
+      var element = document.querySelector('.gem-c-govspeak')
+      govspeakModule.start($(element))
+
+      expect(document.querySelectorAll('.gem-c-govspeak__youtube-video').length).toBe(1)
+    })
+
+    it('allows disabling embeds of youtube videos', function () {
+      container = document.createElement('div')
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak disable-youtube" data-module="govspeak">' +
+          '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
+
+      var element = document.querySelector('.gem-c-govspeak')
+      govspeakModule.start($(element))
+
+      expect(document.querySelectorAll('.gem-c-govspeak__youtube-video').length).toBe(0)
+    })
   })
 
-  it('embeds barcharts', function () {
-    var $element = $(
-      '<div id="govspeak-barchart" class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
-        '<table class="js-barchart-table mc-auto-outdent">' +
-          '<tbody>' +
-            '<tr>' +
-              '<td>row 1</td><td>10</td>' +
-            '</tr>' +
-          '</tbody>' +
-        '</table>' +
-      '<div>'
-    )
-    govspeakModule.start($element)
-    expect($element.find('.mc-chart').length).toBe(1)
-  })
-
-  it('allows disabling embeds of youtube videos', function () {
-    var $element = $(
-      '<div class="gem-c-govspeak govuk-govspeak disable-youtube" data-module="govspeak">' +
-        '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
-      '<div>'
-    )
-    govspeakModule.start($element)
-    expect($element.find('.media-player').length).toBe(0)
+  describe('barchart enhancement', function () {
+    it('embeds barcharts', function () {
+      var $element = $(
+        '<div id="govspeak-barchart" class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<table class="js-barchart-table mc-auto-outdent">' +
+            '<tbody>' +
+              '<tr>' +
+                '<td>row 1</td><td>10</td>' +
+              '</tr>' +
+            '</tbody>' +
+          '</table>' +
+        '<div>'
+      )
+      govspeakModule.start($element)
+      expect($element.find('.mc-chart').length).toBe(1)
+    })
   })
 })
