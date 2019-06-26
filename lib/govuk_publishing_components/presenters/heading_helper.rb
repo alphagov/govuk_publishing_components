b5272b484077c8ee8f99717402ebdbd306cec08e
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-06-26 08:34:18 +0100

    Use shared_helper for heading margin
    - component previously used a custom scale to provide a variable bottom margin
    - replacing this with the now-standard shared_helper way of doing this
    - default options remain the same so there is no visual change in the component
    - since the numbers in the two scales are different this will mean updating any instance of the component where a specific margin is called

--- lib/govuk_publishing_components/presenters/heading_helper.rb
+++ lib/govuk_publishing_components/presenters/heading_helper.rb
@@ -10,7 +10,6 @@ module GovukPublishingComponents
         @classes << " gem-c-heading--font-size-#{options[:font_size]}" if [24, 19].include? options[:font_size]
         @classes << " gem-c-heading--mobile-top-margin" if options[:mobile_top_margin]
         @classes << " gem-c-heading--padding" if options[:padding]
-        @classes << " gem-c-heading--margin-bottom-#{options[:margin_bottom]}" if [2, 4].include? options[:margin_bottom]
         @classes << " gem-c-heading--border-top-#{options[:border_top]}" if [1, 2, 5].include? options[:border_top]
       end
     end
