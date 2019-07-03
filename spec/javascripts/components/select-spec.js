140e5c04acb2de2e7998d29302500dd54d828495
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-07-03 10:45:08 +0100

    Lint select spec

--- spec/javascripts/components/select-spec.js
+++ spec/javascripts/components/select-spec.js
@@ -1,15 +1,16 @@
-describe('Track select change', function () {
+/* eslint-env jasmine, jquery */
+/* global GOVUK */
 
-  var GOVUK = window.GOVUK || {};
-  var tracker;
-  var $element;
+describe('Track select change', function () {
+  var tracker
+  var $element
 
   GOVUK.analytics = {
     trackEvent: function () {}
-  };
+  }
 
   beforeEach(function () {
-    spyOn(GOVUK.analytics, 'trackEvent');
+    spyOn(GOVUK.analytics, 'trackEvent')
 
     $element = $(
       '<select id="taxon-list" name="taxon-list" data-module="track-select-change"' +
@@ -17,27 +18,27 @@ describe('Track select change', function () {
         '<option data-track-category="filterClicked" data-track-action="taxon-list" data-track-label="Social care" value="7d67047c-bf22-4c34-b566-b46d6973f961">Social care</option>' +
         '<option data-track-label="Social care" value="no-data-attributes">No data attributes</option>' +
       '</select>'
-    );
+    )
 
-    tracker = new GOVUK.Modules.TrackSelectChange();
-    tracker.start($element);
-  });
+    tracker = new GOVUK.Modules.TrackSelectChange()
+    tracker.start($element)
+  })
 
   afterEach(function () {
-    GOVUK.analytics.trackEvent.calls.reset();
-  });
+    GOVUK.analytics.trackEvent.calls.reset()
+  })
 
   it('tracks when the selected value is changed', function () {
-    $element.val("7d67047c-bf22-4c34-b566-b46d6973f961").change();
+    $element.val('7d67047c-bf22-4c34-b566-b46d6973f961').change()
 
     expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith(
-      'filterClicked', 'taxon-list', { transport: 'beacon', label: "Social care" }
+      'filterClicked', 'taxon-list', { transport: 'beacon', label: 'Social care' }
     )
-  });
+  })
 
-  it('does not fire an event if track category and track action are not present', function() {
-    $element.val("no-data-attributes").change();
+  it('does not fire an event if track category and track action are not present', function () {
+    $element.val('no-data-attributes').change()
 
     expect(GOVUK.analytics.trackEvent.calls.count()).toEqual(0)
-  });
+  })
 })
