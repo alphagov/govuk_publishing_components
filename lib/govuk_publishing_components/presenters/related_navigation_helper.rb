1a6bf25736a1a2832fef609924335b381560cd5c
Ian James <ian.james@digital.cabinet-office.gov.uk>
2019-08-09 18:26:49 +0100

    Adds lang attribute to related links headings  - including fallback option.

--- lib/govuk_publishing_components/presenters/related_navigation_helper.rb
+++ lib/govuk_publishing_components/presenters/related_navigation_helper.rb
@@ -90,11 +90,47 @@ module GovukPublishingComponents
         related_navigation.flat_map(&:last).any?
       end
 
+      def t_locale(content, options = {})
+        # Check if the content string has a translation
+        content_translation_available = translation_present?(content)
+
+        # True, return locale
+        this_locale = I18n.locale if content_translation_available
+        # If false, return default locale
+        this_locale = I18n.default_locale unless content_translation_available
+
+        # Check if default string passed in
+        if options[:default].present?
+          # Check if the default string has a translation
+          default_translation_available = translation_present?(options[:default])
+          # If true, return locale
+          this_locale = I18n.locale if default_translation_available
+          # If false, return default_locale
+          this_locale = I18n.default_locale unless default_translation_available
+        end
+
+        this_locale
+      end
+
+      def t_lang(content, options = {})
+        locale = t_locale(content, options)
+        "lang=#{locale}" unless locale.eql?(I18n.locale)
+      end
+
       def t_locale_check(locale)
-        locale.presence unless locale.eql?(I18n.locale.to_s)
+        locale.presence unless locale.to_s.eql?(I18n.locale.to_s)
       end
 
     private
+
+      def translation_present?(content)
+        default_string = 'This is a string to act as a default for the `I18n.translate` method. Comparing the result reveals if there is a translation in the i18n files.'
+        I18n.translate(
+          content,
+          default: default_string,
+          fallback: false
+        ) != default_string
+      end
 
       def related_items
         related_quick_links = content_item_details_for('quick_links')
