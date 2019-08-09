285a6c24ce32a8e53f300e64c7d6525d2d4395ed
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-08-09 16:23:07 +0100

    Remove unused assets from initializers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,6 @@
 return unless Rails.application.config.respond_to?(:assets)
 
+# GOV.UK Publishing Components assets
 Rails.application.config.assets.precompile += %w(
   component_guide/accessibility-test.js
   component_guide/application.js
@@ -8,26 +9,19 @@ Rails.application.config.assets.precompile += %w(
   govuk_publishing_components/all_components.js
   govuk_publishing_components/vendor/modernizr.js
   govuk_publishing_components/component_guide.css
-  govuk_publishing_components/search-button.png
   govuk_publishing_components/favicon-development.png
-  govuk_publishing_components/favicon-integration.png
   govuk_publishing_components/favicon-example.png
+  govuk_publishing_components/favicon-integration.png
   govuk_publishing_components/favicon-production.png
   govuk_publishing_components/favicon-staging.png
-  govuk_publishing_components/favicon-test.png
-  govuk_publishing_components/feed-icon-black.png
-  govuk_publishing_components/mail-icon-x2.png
-  govuk_publishing_components/mail-icon.png
-  govuk_publishing_components/icon-pointer.png
-  govuk_publishing_components/icon-pointer-2x.png
   govuk_publishing_components/govuk-logo.png
   govuk_publishing_components/govuk-schema-placeholder-1x1.png
   govuk_publishing_components/govuk-schema-placeholder-4x3.png
   govuk_publishing_components/govuk-schema-placeholder-16x9.png
+  govuk_publishing_components/search-button.png
 )
 
-# Add GOV.Frontend assets
-
+# GOV.UK Frontend assets
 Rails.application.config.assets.precompile += %w(
   govuk-logotype-crown.png
 )
