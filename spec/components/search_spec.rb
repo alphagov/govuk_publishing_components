f71716a39323910f1e7acb128e218ee9afeb95ee
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-08-13 21:03:38 +0000

    Search component custom margins
    Modify search component to use shared helper for applying custom
    margins.
    
    The initial reason for this change was that we don't want the default
    30px bottom margin spacing within the GOV.UK header.

--- spec/components/search_spec.rb
+++ spec/components/search_spec.rb
@@ -62,4 +62,12 @@ describe "Search", type: :view do
     render_component(aria_controls: "something-else")
     assert_select "input[aria-controls='something-else']"
   end
+
+  it "renders a search box with custom margins" do
+    render_component(
+      margin_top: 3,
+      margin_bottom: 2,
+    )
+    assert_select '.gem-c-search.govuk-\!-margin-top-3.govuk-\!-margin-bottom-2'
+  end
 end
