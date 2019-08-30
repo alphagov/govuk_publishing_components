faffbec6f7253c68c42d1e175d8f8474b5c8faed
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-08-30 15:43:05 +0100

    Allow inset-text to take a block
    Can now pass an arbitrary block to the inset-text component to
    have content rendered inset. If both 'text' and a block are
    provided, 'text' takes precedence (mutually exclusive).
    
    This is a compromise solution, rather than copying the inset-prompt
    component from content-publisher (for use in collections-publisher),
    for the reason that under current GOV.UK architecture the component
    CSS would be added to the stylesheets for our end users (not just
    the users using the publishing app). See:
    https://github.com/alphagov/govuk_publishing_components/pull/1077
    
    Once merged, we will use this latest inset-text component and
    manually recreate what the inset-prompt component looks like by
    passing components to the inset-text component as a block.
    
    Trello: https://trello.com/c/TcBpLRuC/48-add-inset-prompt-component-to-govukpublishingcomponents

--- spec/components/inset_text_spec.rb
+++ spec/components/inset_text_spec.rb
@@ -10,4 +10,23 @@ describe "Inset text", type: :view do
 
     assert_select(".govuk-inset-text", text: "It can take up to 8 weeks to register a lasting power of attorney if there are no mistakes in the application.")
   end
+
+  it "renders a block" do
+    block = "
+      <h2 id='heading'>To publish this step by step you need to</h2>
+      <a href='/foo'>Check for broken links</a>
+    ".html_safe
+    render_component({}) { block }
+
+    assert_select(".govuk-inset-text h2#heading", text: "To publish this step by step you need to")
+    assert_select(".govuk-inset-text a[href='/foo']", text: "Check for broken links")
+  end
+
+  it "renders only the text if both text and block are provided" do
+    block = "<p id='foo'>Foo</p>".html_safe
+    render_component(text: 'Bar') { block }
+
+    assert_select(".govuk-inset-text", text: "Bar")
+    assert_select(".govuk-inset-text p#foo", false, "Block should not have rendered")
+  end
 end
