a18db8db5950ea735174a1f4634d18d430098136
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-06 17:25:07 +0100

    Generate the layout within the component
    Instead of requiring this component to be placed in a certain layout container and the associated input in another one, we now generate the layout containers within the component allowing the input to be passed as a block.

--- spec/components/contextual_guidance_spec.rb
+++ spec/components/contextual_guidance_spec.rb
@@ -11,12 +11,17 @@ describe "Contextual guidance", type: :view do
     assert_select ".gem-c-contextual-guidance"
   end
 
-  it "renders the contextual guidance with title and content" do
-    render_component(id: 'news-title-guidance', title: 'Writing a news title') do
-      tag.p "The title must make clear what the content offers users"
+  it "renders the contextual guidance with input, title and content" do
+    render_component(
+      id: "news-title-guidance",
+      title: "Writing a news title",
+      content: sanitize("<p>The title must make clear what the content offers users</p>")
+    ) do
+      tag.input name: "news-title", type: "text", data: { "contextual-guidance": "news-title-guidance" }
     end
 
-    assert_select '.gem-c-contextual-guidance .govuk-heading-s', text: 'Writing a news title'
-    assert_select '.gem-c-contextual-guidance p', text: 'The title must make clear what the content offers users'
+    assert_select "input[name='news-title'][type='text'][data-contextual-guidance='news-title-guidance']"
+    assert_select ".gem-c-contextual-guidance .govuk-heading-s", text: "Writing a news title"
+    assert_select ".gem-c-contextual-guidance p", text: "The title must make clear what the content offers users"
   end
 end
