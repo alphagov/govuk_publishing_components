fb3cc245fcc33c95bc279bfe96da163cdfc501f1
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-10 18:02:58 +0100

    Review precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,12 +2,9 @@ return unless Rails.application.config.respond_to?(:assets)
 
 # GOV.UK Publishing Components assets
 Rails.application.config.assets.precompile += %w(
-  component_guide/accessibility-test.js
   component_guide/application.js
-  component_guide/filter-components.js
-  component_guide/visual-regression.js
+  govuk_publishing_components/dependencies.js
   govuk_publishing_components/all_components.js
-  govuk_publishing_components/modules.js
   govuk_publishing_components/vendor/modernizr.js
   govuk_publishing_components/component_guide.css
   govuk_publishing_components/favicon-development.png
@@ -19,15 +16,10 @@ Rails.application.config.assets.precompile += %w(
   govuk_publishing_components/govuk-schema-placeholder-1x1.png
   govuk_publishing_components/govuk-schema-placeholder-4x3.png
   govuk_publishing_components/govuk-schema-placeholder-16x9.png
-  govuk_publishing_components/search-button.png
   govuk_publishing_components/icon-file-download.svg
   govuk_publishing_components/icon-important.svg
-  govuk_publishing_components/chevron-banner.svg
-  govuk_publishing_components/chevron-banner-focus.svg
-  govuk_publishing_components/chevron-banner-hover-border.svg
-  govuk_publishing_components/chevron-banner-hover.svg
-  govuk_publishing_components/chevron-banner-small.svg
-  govuk_publishing_components/chevron-banner-small-focus.svg
+  govuk_publishing_components/search-button.png
+  govuk_publishing_components/chevron-banner/*.svg
   govuk_publishing_components/crests/*.png
 )
 
