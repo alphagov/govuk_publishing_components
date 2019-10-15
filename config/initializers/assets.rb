c4a11db5096431ab36a97c1ec4c537b671ada91f
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-10-15 09:27:22 +0100

    Disable concurrent export
    Disable concurency otherwise the assets will be compiled in a random order resulting in failures where, for example, a mixin is called but not yet compiled

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -49,3 +49,9 @@ Rails.application.config.assets.paths += %W(
   #{__dir__}/../../node_modules/jquery/dist
   #{__dir__}/../../node_modules/axe-core
 )
+
+# Disable concurency otherwise the assets will be compiled in a random order
+# resulting in failures where, for example, a mixin is called but not yet compiled
+Rails.application.config.assets.configure do |environment|
+  environment.export_concurrent = false
+end
