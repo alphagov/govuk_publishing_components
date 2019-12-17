30119265a710392995907e60462f1b61b7b0d645
Alex <alex-ju@users.noreply.github.com>
2019-12-17 12:06:17 +0000

    Add guidance_id to contextual guidance component
    To be used by inputs and textareas to point to the guidance text as description (via `aria-described-by`)

--- spec/components/contextual_guidance_spec.rb
+++ spec/components/contextual_guidance_spec.rb
@@ -15,13 +15,14 @@ describe "Contextual guidance", type: :view do
     render_component(
       html_for: "news-title",
       title: "Writing a news title",
+      guidance_id: 'news-title-guidance',
       content: sanitize("<p>The title must make clear what the content offers users</p>")
     ) do
       tag.input id: "news-title", name: "news-title", type: "text"
     end
 
     assert_select ".gem-c-contextual-guidance__input-field input[id='news-title'][name='news-title'][type='text']"
-    assert_select ".gem-c-contextual-guidance__wrapper[for='news-title']"
+    assert_select ".gem-c-contextual-guidance__wrapper[for='news-title'][id='news-title-guidance']"
     assert_select ".gem-c-contextual-guidance__guidance .govuk-heading-s", text: "Writing a news title"
     assert_select ".gem-c-contextual-guidance__guidance p", text: "The title must make clear what the content offers users"
   end
