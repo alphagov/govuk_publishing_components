3892796d2d090080a18e6bff352a022782ea8a0e
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-02-20 11:01:41 +0000

    Remove redundant new_cookie_banner flag
    This was added in the first iteration of the cookie banner and has since been removed, but the tests were missed

--- spec/components/cookie_banner_spec.rb
+++ spec/components/cookie_banner_spec.rb
@@ -14,30 +14,30 @@ describe "Cookie banner", type: :view do
   end
 
   it "renders a button for accepting cookies" do
-    render_component(new_cookie_banner: true)
+    render_component({})
     assert_select '.gem-c-cookie-banner__buttons .gem-c-button', text: "Accept all cookies"
     assert_select '.gem-c-cookie-banner__buttons .gem-c-button[data-module=track-click][data-track-category=cookieBanner][data-track-action="Cookie banner accepted"]'
   end
 
   it "renders a button for viewing cookie settings" do
-    render_component(new_cookie_banner: true)
+    render_component({})
     assert_select '.gem-c-cookie-banner__buttons .gem-c-button', text: "Set cookie preferences"
     assert_select '.gem-c-cookie-banner__buttons .gem-c-button[data-module=track-click][data-track-category=cookieBanner][data-track-action="Cookie banner settings clicked"]'
   end
 
   it "renders a confirmation message" do
-    render_component(new_cookie_banner: true)
+    render_component({})
     assert_select '.gem-c-cookie-banner__confirmation-message', text: "You’ve accepted all cookies. You can change your cookie settings at any time."
   end
 
   it "renders a link to the settings page within the confirmation message" do
-    render_component(new_cookie_banner: true)
+    render_component({})
     assert_select '.gem-c-cookie-banner__confirmation-message a', text: "change your cookie settings"
     assert_select '.gem-c-cookie-banner__confirmation-message a[data-module=track-click][data-track-category=cookieBanner][data-track-action="Cookie banner settings clicked from confirmation"]'
   end
 
   it "renders a hide link within the confirmation banner" do
-    render_component(new_cookie_banner: true)
+    render_component({})
 
     assert_select '.gem-c-cookie-banner__confirmation .gem-c-cookie-banner__hide-button', text: "Hide"
     assert_select '.gem-c-cookie-banner__hide-button[data-module=track-click][data-track-category=cookieBanner][data-track-action="Hide cookie banner"]'
