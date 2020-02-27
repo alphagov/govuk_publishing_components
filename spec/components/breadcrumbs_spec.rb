40d1bd6a543a464ee9652314146ba482e73e76fc
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-02-27 19:38:39 +0000

    Allow breadcrumb items without URLs
    Essentially a revert of #1324. This was too restrictive and would have broken breadcrumb use in some applications.

--- spec/components/breadcrumbs_spec.rb
+++ spec/components/breadcrumbs_spec.rb
@@ -128,14 +128,17 @@ describe "Breadcrumbs", type: :view do
     assert_select ".gem-c-breadcrumbs--inverse"
   end
 
-  it "ignore breadcrumb items without url" do
+  it "renders breadcrumb items without link as text" do
     render_component(
       breadcrumbs: [
         { title: 'Topic', url: '/topic' },
         { title: 'Current Page' },
       ]
     )
-    assert_select('.govuk-breadcrumbs__list-item:last-child', 'Topic')
+
+    assert_link_with_text_in('.govuk-breadcrumbs__list-item:first-child', '/topic', 'Topic')
+    assert_select('.govuk-breadcrumbs__list-item:last-child', 'Current Page')
+    assert_select('.govuk-breadcrumbs__list-item:last-child a', false)
   end
 
   it "collapses on mobile if passed a flag" do
