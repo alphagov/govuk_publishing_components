370708acf0bfb6e9501b9439092798d4052d3fa4
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-05-14 09:24:52 +0100

    Add action link component from collections
    - code is unchanged from collections, apart from app-c class renamed to gem-c, path changes for images in sass, and test wrapping syntax

---
+++ spec/components/action_link_spec.rb
@@ -0,0 +1,71 @@
+require "rails_helper"
+
+describe "Action link", type: :view do
+  def component_name
+    "action_link"
+  end
+
+  it "renders nothing without a href" do
+    assert_empty render_component(text: "Get more info")
+  end
+
+  it "renders nothing without text" do
+    assert_empty render_component(href: "/coronavirus")
+  end
+
+  it "renders non wrapping text if nowrap text is passed through" do
+    render_component(
+      text: "Get more info",
+      nowrap_text: "about COVID",
+      href: "/coronavirus",
+    )
+    assert_select ".gem-c-action-link .gem-c-action-link__nowrap-text", text: "about COVID"
+  end
+
+  it "renders simple icon version" do
+    render_component(
+      text: "Get more info",
+      href: "/coronavirus",
+      simple: true,
+    )
+    assert_select ".gem-c-action-link--simple"
+  end
+
+  it "renders dark icon version" do
+    render_component(
+      text: "Get more info",
+      href: "/coronavirus",
+      dark_icon: true,
+    )
+    assert_select ".gem-c-action-link--dark-icon"
+  end
+
+  it "renders light text version" do
+    render_component(
+      text: "Get more info",
+      href: "/coronavirus",
+      light_text: true,
+    )
+    assert_select ".gem-c-action-link--light-text"
+  end
+
+  it "renders compact version" do
+    render_component(
+      text: "Get more info",
+      href: "/coronavirus",
+      compact: true,
+    )
+    assert_select ".gem-c-action-link--compact"
+  end
+
+  it "has data attributes if data attributes are passed in" do
+    render_component(
+      text: "Get more info",
+      href: "/coronavirus",
+      data: {
+        testing: "hasDataAttribute",
+      },
+    )
+    assert_select ".gem-c-action-link__link[data-testing='hasDataAttribute']"
+  end
+end
