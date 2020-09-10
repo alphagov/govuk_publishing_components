6c6b3756e919e1f44bf03fe4216769ac7350f6e1
Dana Cotoran <dana.cotoran@gmail.com>
2020-09-10 13:16:49 +0100

    Mobile search toggle button
    The button that opens/closes the layout header search form on mobile is
    not actually a button, but an anchor tag. Therefore, it does not behave
    as a button (for instance, it does not activate when using the space
    key)
    
    This is semantically incorrect and cannot be easily understood by screen
    reader users, or used by voice control users.
    
    Use button markup instead of an anchor tag, to more closely match the
    presentation and function of this element.
    
    Hide element when Javascript is disabled as the element is not
    functional without JS.

--- app/assets/javascripts/govuk_publishing_components/lib/header-navigation.js
+++ app/assets/javascripts/govuk_publishing_components/lib/header-navigation.js
@@ -10,7 +10,7 @@
     for (i = 0, _i = els.length; i < _i; i++) {
       els[i].addEventListener('click', function (e) {
         e.preventDefault()
-        var target = document.getElementById(this.getAttribute('href').substr(1))
+        var target = this.getAttribute('href') ? document.getElementById(this.getAttribute('href').substr(1)) : document.getElementById(this.getAttribute('data-search-toggle-for'))
         var targetClass = target.getAttribute('class') || ''
         var sourceClass = this.getAttribute('class') || ''
 
@@ -21,8 +21,10 @@
         }
         if (sourceClass.indexOf('js-visible') !== -1) {
           this.setAttribute('class', sourceClass.replace(/(^|\s)js-visible(\s|$)/, ''))
+          this.innerText = 'Show search'
         } else {
           this.setAttribute('class', sourceClass + ' js-visible')
+          this.innerText = 'Hide search'
         }
         this.setAttribute('aria-expanded', this.getAttribute('aria-expanded') !== 'true')
         target.setAttribute('aria-hidden', target.getAttribute('aria-hidden') === 'false')
