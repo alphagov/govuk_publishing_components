2f7e030e5f1657eefde5b903f36494d0cad7c681
Alex <alex-ju@users.noreply.github.com>
2020-11-05 19:51:52 +0000

    Covert details script module

--- app/assets/javascripts/govuk_publishing_components/components/details.js
+++ app/assets/javascripts/govuk_publishing_components/components/details.js
@@ -4,47 +4,47 @@ window.GOVUK = window.GOVUK || {}
 window.GOVUK.Modules = window.GOVUK.Modules || {};
 
 (function (Modules) {
-  'use strict'
+  function GovukDetails () { }
 
-  Modules.GovukDetails = function () {
-    this.start = function (element) {
-      var customTrackLabel = element[0].getAttribute('data-track-label')
+  GovukDetails.prototype.start = function ($module) {
+    this.$module = $module[0]
 
-      // If a custom label has been provided, we can simply call the tracking module
-      if (customTrackLabel) {
-        var trackDetails = new window.GOVUK.Modules.TrackClick()
-        trackDetails.start(element)
-      } else {
-        // If no custom label is set, we use the open/close status as the label
-        var detailsComponent = element[0]
-        var detailsClick = detailsComponent.querySelector('[data-details-track-click]')
-        var that = this
+    var customTrackLabel = this.$module.getAttribute('data-track-label')
 
-        if (detailsClick) {
-          detailsClick.addEventListener('click', function (event) {
-            that.trackDefault(detailsComponent)
-          })
-        }
-      }
-    }
+    // If a custom label has been provided, we can simply call the tracking module
+    if (customTrackLabel) {
+      var trackDetails = new window.GOVUK.Modules.TrackClick()
+      trackDetails.start($module)
+    } else {
+      // If no custom label is set, we use the open/close status as the label
+      var detailsClick = this.$module.querySelector('[data-details-track-click]')
 
-    this.trackDefault = function (element) {
-      if (window.GOVUK.analytics && window.GOVUK.analytics.trackEvent) {
-        var componentStatus = (element.getAttribute('open') == null) ? 'open' : 'closed'
-        var trackCategory = element.getAttribute('data-track-category')
-        var trackAction = element.getAttribute('data-track-action')
-        var trackOptions = element.getAttribute('data-track-options')
-
-        if (typeof trackOptions !== 'object' || trackOptions === null) {
-          trackOptions = {}
-        }
-
-        trackOptions.label = componentStatus
-
-        if (trackAction && trackCategory) {
-          window.GOVUK.analytics.trackEvent(trackCategory, trackAction, trackOptions)
-        }
+      if (detailsClick) {
+        detailsClick.addEventListener('click', function (event) {
+          this.trackDefault(this.$module)
+        }.bind(this))
       }
     }
   }
+
+  GovukDetails.prototype.trackDefault = function (element) {
+    if (window.GOVUK.analytics && window.GOVUK.analytics.trackEvent) {
+      var componentStatus = (element.getAttribute('open') == null) ? 'open' : 'closed'
+      var trackCategory = element.getAttribute('data-track-category')
+      var trackAction = element.getAttribute('data-track-action')
+      var trackOptions = element.getAttribute('data-track-options')
+
+      if (typeof trackOptions !== 'object' || trackOptions === null) {
+        trackOptions = {}
+      }
+
+      trackOptions.label = componentStatus
+
+      if (trackAction && trackCategory) {
+        window.GOVUK.analytics.trackEvent(trackCategory, trackAction, trackOptions)
+      }
+    }
+  }
+
+  Modules.GovukDetails = GovukDetails
 })(window.GOVUK.Modules)
