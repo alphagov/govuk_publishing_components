0184b8c3072956bf3d00029f45552aeb3ccce1ba
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2021-01-05 16:50:33 +0000

    Put button class in button template
    - there's a test that checks to see a component template has the expected class name in it e.g. `gem-c-button`
    - for the button component, that class is provided via a helper function, and the component is excluded from this check
    - for the checkboxes component, the same happens but the template contains a comment with the classname in, so the test passes
    - I _think_ doing the same with the button component makes sense, the other exclusions in the test are for different reasons, e.g. class name would be unnecessarily long

--- spec/components/all_components_spec.rb
+++ spec/components/all_components_spec.rb
@@ -24,7 +24,7 @@ describe "All components" do
       end
 
       it "has the correct class in the ERB template",
-         skip: component_name.in?(%w[button step_by_step_nav_related step_by_step_nav_header step_by_step_nav previous_and_next_navigation]),
+         skip: component_name.in?(%w[step_by_step_nav_related step_by_step_nav_header step_by_step_nav previous_and_next_navigation]),
          not_applicable: component_name.in?(%w[meta_tags machine_readable_metadata google_tag_manager_script table admin_analytics]) do
         erb = File.read(filename)
 
