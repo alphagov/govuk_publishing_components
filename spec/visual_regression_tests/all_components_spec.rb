75cb0f94f8bdbd9a263beaf5d86bdf5c0fb3f867
Ian James <ian.james@digital.cabinet-office.gov.uk>
2021-03-24 10:39:50 +0000

    Update visual regression tests
     - Add public component preview
     - Update selector that the tests waits for before screenshotting - the public
       layout component can have multiple `#wrapper`s on the same page.
     - Update tests to unfreeze time
     - Visual regression tests to run on all changes to a pull request
     - Update change log

--- spec/visual_regression_tests/all_components_spec.rb
+++ spec/visual_regression_tests/all_components_spec.rb
@@ -4,38 +4,47 @@ require "uri"
 
 describe "visual regression test runner Percy", visual_regression: true do
   it "takes a screenshot of each component" do
-    # Freeze time for transition countdown
-    travel_to(Time.zone.local(2020, 12, 1, 6, 0, 0))
+    # Freeze time for consistency
+    travel_to Time.zone.local(2020, 12, 1, 6, 0, 0)
+
+    skip_as_govspeak_times_out = %w[
+      /component-guide/govspeak/preview
+    ]
+
+    skip_non_visible_components = %w[
+      /component-guide/meta_tags/preview
+      /component-guide/machine_readable_metadata/preview
+      /component-guide/admin_analytics/preview
+      /component-guide/google_tag_manager_script/preview
+      /component-guide/layout_for_admin/preview
+    ]
+
+    all_components_to_skip = skip_as_govspeak_times_out + skip_non_visible_components
 
     visit("/component-guide")
 
     component_links = find("#list-all-components-in-the-gem")
       .all("a")
       .map do |link|
-        URI(link[:href])
+        URI("#{link[:href]}/preview")
       end
 
+    # The public layout component has two previews - one with multiple versions
+    # as part of the component guide, and another one at `/public`.
+    component_links << URI("/public")
+
     component_links.each do |link|
-      skip_as_govspeak_times_out = [
-        "/component-guide/govspeak",
-      ]
-
-      skip_non_visible_components = [
-        "/component-guide/meta_tags",
-        "/component-guide/machine_readable_metadata",
-        "/component-guide/admin_analytics",
-        "/component-guide/google_tag_manager_script",
-        "/component-guide/layout_for_admin",
-      ]
-
-      all_components_to_skip = skip_as_govspeak_times_out + skip_non_visible_components
-
       next if all_components_to_skip.include?(link.path)
 
-      visit("#{link}/preview")
+      visit(link)
+
       name = title.gsub(/(: Default|) preview - Component Guide/, "")
-      page.find(:css, "#wrapper", wait: 10)
+
+      page.find(:css, "body > #wrapper", wait: 10)
+
       Percy.snapshot(page, name: name)
     end
+
+    travel_back
   end
 end
