ea9123fae03138722bf065597f8506f88bcd8c9d
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2021-04-06 11:53:39 +0100

    Tidy up success alert markup

--- spec/components/success_alert_spec.rb
+++ spec/components/success_alert_spec.rb
@@ -11,9 +11,16 @@ describe "Success Alert", type: :view do
   end
 
   it "allows a block to be given for description" do
-    render_component(message: "Foo", description: "Bar")
+    render_component(message: "Foo", description: raw("<p class=\"govuk-body\">Bar</p>"))
 
     assert_select ".govuk-notification-banner__heading", text: "Foo"
-    assert_select ".govuk-notification-banner__content div", text: "Bar"
+    assert_select ".govuk-notification-banner__content .govuk-body", text: "Bar"
+  end
+
+  it "allows the block to be given a custom component label id" do
+    render_component(message: "Foo", title_id: "Bar")
+
+    assert_select ".gem-c-success-alert", aria: { labelledby: "Bar" }
+    assert_select ".govuk-notification-banner__title", id: "Bar"
   end
 end
