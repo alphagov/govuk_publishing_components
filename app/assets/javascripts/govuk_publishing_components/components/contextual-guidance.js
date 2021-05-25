f36515f822d923523341a5a4be4fe5f4313cde03
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2021-05-25 14:10:43 +0100

    Make contextual guidance tests work in IE10+
    - 'dataset' can't be used like that in IE, apparently, so switching to getAttribute
    - also adding a test for the 'hide-only' functionality, although apparently it's not in use at the moment

--- app/assets/javascripts/govuk_publishing_components/components/contextual-guidance.js
+++ app/assets/javascripts/govuk_publishing_components/components/contextual-guidance.js
@@ -15,7 +15,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
   ContextualGuidance.prototype.handleFocus = function (event) {
     this.hideAllGuidance()
-    if (!event.target.dataset.contextualGuidanceHideOnly) {
+    if (!event.target.getAttribute('data-contextual-guidance-hide-only')) {
       this.$guidance.style.display = 'block'
     }
   }
