3818d17c05ba1c21a660edcdcd201e2c4d6c097c
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2021-06-09 15:45:04 +0100

    Remove hide-only option from contextual guidance
    - "the contextual guidance was designed to be visible when a certain input element is focused, and be hidden when the input is 'blurred'. This exception was set in place for a certain case where we only wanted to make sure the contextual guidance is hidden on blur, without showing it on focus"
    - this feature is no longer being used, and isn't documented, so removing it

--- spec/javascripts/components/contextual-guidance-spec.js
+++ spec/javascripts/components/contextual-guidance-spec.js
@@ -29,27 +29,14 @@ describe('Contextual guidance component', function () {
             '<p>The summary should explain the main point of the story. It is the first line of the story so don’t repeat it in the body and end with a full stop.</p>' +
           '</div>' +
         '</div>' +
-      '</div>' +
-
-      '<div id="document-description-guidance" class="gem-c-contextual-guidance" data-module="contextual-guidance">' +
-        '<textarea id="document-description" class="gem-c-textarea govuk-textarea" data-contextual-guidance-hide-only="true"></textarea>' +
-
-        '<div class="gem-c-contextual-guidance__wrapper" for="document-description">' +
-          '<div class="gem-c-contextual-guidance__guidance">' +
-            '<h2 class="govuk-heading-s">Summary</h2>' +
-            '<p>The description should describe the story.</p>' +
-          '</div>' +
-        '</div>' +
       '</div>'
 
     document.body.classList.add('js-enabled')
     document.body.appendChild(container)
     var titleContextualGuidance = document.getElementById('document-title-guidance')
     var summaryContextualGuidance = document.getElementById('document-summary-guidance')
-    var descriptionContextualGuidance = document.getElementById('document-description-guidance')
     new GOVUK.Modules.ContextualGuidance().start($(titleContextualGuidance))
     new GOVUK.Modules.ContextualGuidance().start($(summaryContextualGuidance))
-    new GOVUK.Modules.ContextualGuidance().start($(descriptionContextualGuidance))
   })
 
   afterEach(function () {
@@ -80,19 +67,5 @@ describe('Contextual guidance component', function () {
 
     expect(titleGuidance.style.display).toEqual('none')
     expect(summaryGuidance.style.display).toEqual('block')
-  })
-
-  it('should not show associated guidance on focus if the hide-only attribute is present', function () {
-    var description = document.getElementById('document-description')
-    var descriptionGuidance = document.querySelector('#document-description-guidance .gem-c-contextual-guidance__wrapper')
-
-    var titleGuidance = document.querySelector('#document-title-guidance .gem-c-contextual-guidance__wrapper')
-    var summaryGuidance = document.querySelector('#document-summary-guidance .gem-c-contextual-guidance__wrapper')
-
-    description.dispatchEvent(new window.Event('focus'))
-
-    expect(titleGuidance.style.display).toEqual('none')
-    expect(summaryGuidance.style.display).toEqual('none')
-    expect(descriptionGuidance.style.display).toEqual('none')
   })
 })
