e5eef0b9d6d5ef604510588138a0a22a727d0bd7
Ian James <ian.james@digital.cabinet-office.gov.uk>
2021-06-21 14:47:25 +0100

    Update LUX to activate when usage cookies are accepted
    Previously LUX was only fired on pages once usage tracking cookies had
    been accepted - this updates the set up to measure as soon as the page
    loads, but only sends that information once a user has accepted usage
    tracking cookies[1]. Information is sent when the event `cookie-consent`
    is fired, or when the usage cookie has been set to true.
    
    The LUX JavaScript files have been renamed to make it a bit clearer what
    each particular script does.
    
    [1]: https://support.speedcurve.com/en/articles/4468508-using-lux-with-cookie-consent-banners

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,8 @@ Rails.application.config.assets.precompile += %w[
   component_guide/visual-regression.js
   component_guide/print.css
   govuk_publishing_components/rum-loader.js
-  govuk_publishing_components/vendor/lux.js
-  govuk_publishing_components/vendor/lux-polyfill.js
+  govuk_publishing_components/vendor/lux/lux-reporter.js
+  govuk_publishing_components/vendor/lux/lux-measurer.js
   govuk_publishing_components/all_components.js
   govuk_publishing_components/ie.js
   govuk_publishing_components/modules.js
