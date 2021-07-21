8512933bd547ef302cbb19ea8db67c9421f46933
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2021-07-21 17:22:12 +0100

    Add GA tracking code for Intervention

--- spec/components/intervention_spec.rb
+++ spec/components/intervention_spec.rb
@@ -9,6 +9,12 @@ describe "Intervention", type: :view do
     render_component({})
     assert_select ".gem-c-intervention__title", text: "Check the next steps for your limited company"
     assert_select ".gem-c-intervention__paragraph", text: "You might be interested in this because you’ve been browsing guidance relevant to starting a limited company."
-    assert_select ".gem-c-intervention__paragraph", text: "Hide this suggestion if it’s not relevant to you"
+    assert_select ".gem-c-intervention__paragraph a", text: "Hide this suggestion"
+  end
+
+  it "renders the tracking attributes" do
+    render_component({})
+    assert_select ".gem-c-intervention__title a[data-track-category=\"interventionBanner\"][data-track-action=\"interventionClicked\"][data-track-label=\"/next-steps-for-your-business\"][data-track-options=\"{'dimension29':'Check the next steps for your limited company'}\"]"
+    assert_select ".gem-c-intervention__paragraph a[data-track-category=\"interventionBanner\"][data-track-action=\"interventionDismissed\"][data-track-label=\"?hide-intervention=true\"][data-track-options=\"{'dimension29':'Hide this suggestion'}\"]"
   end
 end
