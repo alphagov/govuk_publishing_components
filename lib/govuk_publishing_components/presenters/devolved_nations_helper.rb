5acd0ea281ee0a9e0b75143f8d5a603dfd9ec70c
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2021-08-27 15:49:23 +0100

    Create devolved nations component

---
+++ lib/govuk_publishing_components/presenters/devolved_nations_helper.rb
@@ -0,0 +1,27 @@
+module GovukPublishingComponents
+  module Presenters
+    class DevolvedNationsHelper
+      attr_reader :national_applicability
+
+      def initialize(local_assigns)
+        @national_applicability = local_assigns[:national_applicability]
+      end
+
+      def applicable_nations_title_text
+        @national_applicability
+          .select { |_, v| v[:applicable] == true }
+          .map { |k, _| I18n.t("components.devolved_nations.#{k}") }
+          .sort
+          .to_sentence(last_word_connector: " and ")
+      end
+
+      def nations_with_urls
+        @national_applicability
+          .select do |_, v|
+            v[:alternative_url]
+            .present?
+          end
+      end
+    end
+  end
+end
