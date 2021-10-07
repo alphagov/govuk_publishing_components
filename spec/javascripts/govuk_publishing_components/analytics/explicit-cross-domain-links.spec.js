6452c780c066f87bcee7f5b2b03ed89f9f354523
Alex Whitehead-Smith <alex.smith@digital.cabinet-office.gov.uk>
2021-10-07 15:28:27 +0100

    Add cookie_consent parameter to links to Single Sign On
    We want to share a cookie consent between www.gov.uk and account.gov.uk
    (or whatever the DI domain ends up being).  Rather than try to tie this
    to clicking the accept / reject button in the banner, we've decided to
    go for the simpler approach of passing a query parameter around.
    
    If a user hasn't engaged with the cookie banner and clicks on a link to
    the other domain, add `cookie_consent=not-engaged`.
    
    If a user has rejected usage cookies and clicks on a link to the other
    domain, add `cookie_consent=reject`.
    
    Otherwise, if a user has accepted usage cookies and clicks on a link to
    the other domain, add `cookie_consent=accept` and the GA client ID if
    available.

--- spec/javascripts/govuk_publishing_components/analytics/explicit-cross-domain-links.spec.js
+++ spec/javascripts/govuk_publishing_components/analytics/explicit-cross-domain-links.spec.js
@@ -31,7 +31,9 @@ describe('Explicit cross-domain linker', function () {
     explicitCrossDomainLinks = new GOVUK.Modules.ExplicitCrossDomainLinks()
 
     spyOn(window.gaplugins, 'Linker').and.returnValue(linker)
-    spyOn(linker, 'decorate').and.returnValue('/somewhere?_ga=abc123')
+    spyOn(linker, 'decorate').and.callFake(function (url) {
+      return url + '&_ga=abc123'
+    })
   })
 
   afterEach(function () {
@@ -45,16 +47,45 @@ describe('Explicit cross-domain linker', function () {
       element = $('<a href="/somewhere">')
     })
 
-    it('leaves the link href if unchanged there are no trackers in ga', function () {
-      trackers = []
+    it('modifies the link href to append cookie_consent parameter "not-engaged" if cookie preferences are not set', function () {
+      GOVUK.cookie('cookies_preferences_set', 'false')
       explicitCrossDomainLinks.start(element)
-      expect(element.attr('href')).toEqual('/somewhere')
+      expect(element.attr('href')).toEqual('/somewhere?cookie_consent=not-engaged')
     })
 
-    it('modifies the link href to append ids from ga to the destination url', function () {
-      trackers = [{ ga_mock: 'foobar' }]
+    it('modifies the link href to append cookie_consent parameter "reject" if usage cookies have been rejected', function () {
+      GOVUK.cookie('cookies_preferences_set', 'true')
+      GOVUK.setConsentCookie({ usage: false })
       explicitCrossDomainLinks.start(element)
-      expect(element.attr('href')).toEqual('/somewhere?_ga=abc123')
+      expect(element.attr('href')).toEqual('/somewhere?cookie_consent=reject')
+    })
+
+    describe('user accepts cookies', function () {
+      it('modifies the link href to append cookie_consent parameter "accept" and adds _ga if trackers are present', function () {
+        GOVUK.cookie('cookies_preferences_set', 'true')
+        GOVUK.setConsentCookie({ usage: true })
+        trackers = [{ ga_mock: 'foobar' }]
+
+        explicitCrossDomainLinks.start(element)
+
+        expect(element.attr('href')).toEqual('/somewhere?cookie_consent=accept&_ga=abc123')
+      })
+
+      it('modifies the link href to only append cookie_consent "accept" if there are no trackers', function () {
+        GOVUK.cookie('cookies_preferences_set', 'true')
+        GOVUK.setConsentCookie({ usage: true })
+        trackers = []
+        explicitCrossDomainLinks.start(element)
+        expect(element.attr('href')).toEqual('/somewhere?cookie_consent=accept')
+      })
+
+      it('modifies the link href to only append cookie_consent "accept" if ga is not initalised on window', function () {
+        GOVUK.cookie('cookies_preferences_set', 'true')
+        GOVUK.setConsentCookie({ usage: true })
+        window.ga = undefined
+        explicitCrossDomainLinks.start(element)
+        expect(element.attr('href')).toEqual('/somewhere?cookie_consent=accept')
+      })
     })
   })
 
@@ -66,16 +97,43 @@ describe('Explicit cross-domain linker', function () {
              '</form>')
     })
 
-    it('leaves the form action if unchanged there are no trackers in ga', function () {
-      trackers = []
+    it('modifies the form action to append cookie_consent parameter "not-engaged" if cookie preferences are not set', function () {
+      GOVUK.cookie('cookies_preferences_set', 'false')
       explicitCrossDomainLinks.start(element)
-      expect(element.attr('action')).toEqual('/somewhere')
+      expect(element.attr('action')).toEqual('/somewhere?cookie_consent=not-engaged')
     })
 
-    it('modifies the form action to append ids from ga to the destination url', function () {
-      trackers = [{ ga_mock: 'foobar' }]
+    it('modifies the form action to append cookie_consent parameter "reject" if usage cookies have been rejected', function () {
+      GOVUK.cookie('cookies_preferences_set', 'true')
+      GOVUK.setConsentCookie({ usage: false })
       explicitCrossDomainLinks.start(element)
-      expect(element.attr('action')).toEqual('/somewhere?_ga=abc123')
+      expect(element.attr('action')).toEqual('/somewhere?cookie_consent=reject')
+    })
+
+    describe('user accepts cookies', function () {
+      it('modifies the form action to append cookie_consent parameter "accept" and adds _ga if trackers are present', function () {
+        GOVUK.cookie('cookies_preferences_set', 'true')
+        GOVUK.setConsentCookie({ usage: true })
+        trackers = [{ ga_mock: 'foobar' }]
+        explicitCrossDomainLinks.start(element)
+        expect(element.attr('action')).toEqual('/somewhere?cookie_consent=accept&_ga=abc123')
+      })
+
+      it('modifies the form action to only append cookie_consent "accept" if there are no trackers', function () {
+        GOVUK.cookie('cookies_preferences_set', 'true')
+        GOVUK.setConsentCookie({ usage: true })
+        trackers = []
+        explicitCrossDomainLinks.start(element)
+        expect(element.attr('action')).toEqual('/somewhere?cookie_consent=accept')
+      })
+
+      it('modifies the form action to only append cookie_consent "accept" if ga is not initalized on window', function () {
+        GOVUK.cookie('cookies_preferences_set', 'true')
+        GOVUK.setConsentCookie({ usage: true })
+        window.ga = undefined
+        explicitCrossDomainLinks.start(element)
+        expect(element.attr('action')).toEqual('/somewhere?cookie_consent=accept')
+      })
     })
   })
 })
