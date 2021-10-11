d4d7d1c4a8836a9fddeee448134b221cc3533a17
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2021-10-11 11:23:32 +0100

    Update devolved nations component to accept type property

--- spec/lib/govuk_publishing_components/components/devolved_nations_helper_spec.rb
+++ spec/lib/govuk_publishing_components/components/devolved_nations_helper_spec.rb
@@ -36,38 +36,38 @@ RSpec.describe GovukPublishingComponents::Presenters::DevolvedNationsHelper do
       expect(devolved_nations_helper.applicable_nations_title_text).to eql("England, Scotland and Wales")
     end
 
-    it "returns title text, which contains one nation, and guidance urls for three nations, correctly" do
+    it "returns title text, which contains one nation, and publication urls for three nations, correctly" do
       devolved_nations_helper = GovukPublishingComponents::Presenters::DevolvedNationsHelper.new(national_applicability: {
         england: {
           applicable: true,
         },
         northern_ireland: {
           applicable: false,
-          alternative_url: "/guidance-northern-ireland",
+          alternative_url: "/publication-northern-ireland",
         },
         scotland: {
           applicable: false,
-          alternative_url: "/guidance-scotland",
+          alternative_url: "/publication-scotland",
         },
         wales: {
           applicable: false,
-          alternative_url: "/guidance-wales",
+          alternative_url: "/publication-wales",
         },
       })
       expect(devolved_nations_helper.applicable_nations_title_text).to eql("England")
-      expect(devolved_nations_helper.nations_with_urls[:northern_ireland][:alternative_url]).to eql("/guidance-northern-ireland")
-      expect(devolved_nations_helper.nations_with_urls[:scotland][:alternative_url]).to eql("/guidance-scotland")
-      expect(devolved_nations_helper.nations_with_urls[:wales][:alternative_url]).to eql("/guidance-wales")
+      expect(devolved_nations_helper.nations_with_urls[:northern_ireland][:alternative_url]).to eql("/publication-northern-ireland")
+      expect(devolved_nations_helper.nations_with_urls[:scotland][:alternative_url]).to eql("/publication-scotland")
+      expect(devolved_nations_helper.nations_with_urls[:wales][:alternative_url]).to eql("/publication-wales")
     end
 
-    it "returns title text, which contains one nation, and guidance urls for three nations, correctly" do
+    it "returns title text, which contains one nation, and publication urls for three nations, correctly" do
       devolved_nations_helper = GovukPublishingComponents::Presenters::DevolvedNationsHelper.new(national_applicability: {
         england: {
           applicable: true,
         },
         northern_ireland: {
           applicable: false,
-          alternative_url: "/guidance-northern-ireland",
+          alternative_url: "/publication-northern-ireland",
         },
         scotland: {
           applicable: true,
@@ -77,7 +77,58 @@ RSpec.describe GovukPublishingComponents::Presenters::DevolvedNationsHelper do
         },
       })
       expect(devolved_nations_helper.applicable_nations_title_text).to eql("England, Scotland and Wales")
-      expect(devolved_nations_helper.nations_with_urls[:northern_ireland][:alternative_url]).to eql("/guidance-northern-ireland")
+      expect(devolved_nations_helper.nations_with_urls[:northern_ireland][:alternative_url]).to eql("/publication-northern-ireland")
+    end
+
+    it "returns title text, which contains one nation, and consultation url for one nation, correctly" do
+      devolved_nations_helper = GovukPublishingComponents::Presenters::DevolvedNationsHelper.new(
+        national_applicability: {
+          england: {
+            applicable: true,
+          },
+          northern_ireland: {
+            applicable: false,
+            alternative_url: "/consultation-northern-ireland",
+          },
+        },
+        type: "consultation",
+      )
+      expect(devolved_nations_helper.applicable_nations_title_text).to eql("England")
+      expect(devolved_nations_helper.alternative_content_text("northern_ireland")).to eql("Consultation for Northern Ireland")
+    end
+
+    it "returns title text, which contains one nation, and guidance url for one nation, correctly" do
+      devolved_nations_helper = GovukPublishingComponents::Presenters::DevolvedNationsHelper.new(
+        national_applicability: {
+          england: {
+            applicable: true,
+          },
+          northern_ireland: {
+            applicable: false,
+            alternative_url: "/guidance-northern-ireland",
+          },
+        },
+        type: "detailed_guide",
+      )
+      expect(devolved_nations_helper.applicable_nations_title_text).to eql("England")
+      expect(devolved_nations_helper.alternative_content_text("northern_ireland")).to eql("Guidance for Northern Ireland")
+    end
+
+    it "returns title text, which contains one nation, and guidance url for one nation, when invalid type provided, correctly" do
+      devolved_nations_helper = GovukPublishingComponents::Presenters::DevolvedNationsHelper.new(
+        national_applicability: {
+          england: {
+            applicable: true,
+          },
+          northern_ireland: {
+            applicable: false,
+            alternative_url: "/publication-northern-ireland",
+          },
+        },
+        type: "invalid_type",
+      )
+      expect(devolved_nations_helper.applicable_nations_title_text).to eql("England")
+      expect(devolved_nations_helper.alternative_content_text("northern_ireland")).to eql("Publication for Northern Ireland")
     end
   end
 end
