ebbd1bbf7cfba86de581644dc0021669651f877b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2021-11-09 09:55:23 +0000

    Add shared accessibility criteria for button

--- app/models/govuk_publishing_components/shared_accessibility_criteria.rb
+++ app/models/govuk_publishing_components/shared_accessibility_criteria.rb
@@ -16,5 +16,22 @@ Links in the component must:
 - have meaningful text
       "
     end
+
+    def self.button
+      "
+Buttons in the component must:
+
+- accept focus
+- be focusable with a keyboard
+- be usable with a keyboard
+- indicate when they have focus
+- change in appearance when touched (in the touch-down state)
+- change in appearance when hovered
+- be usable with touch
+- be usable with [voice commands](https://www.w3.org/WAI/perspectives/voice.html)
+- have visible text
+- have meaningful text
+      "
+    end
   end
 end
