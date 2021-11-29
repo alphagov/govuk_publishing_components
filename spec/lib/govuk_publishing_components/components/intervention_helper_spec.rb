c5ebc06201a4cc13af6e5fd262359986224f5ab5
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2021-11-29 16:05:58 +0000

    Reformat spec
    Add a describe for the method to be clearer and newline before the expect as per convention.

--- spec/lib/govuk_publishing_components/components/intervention_helper_spec.rb
+++ spec/lib/govuk_publishing_components/components/intervention_helper_spec.rb
@@ -1,16 +1,28 @@
 RSpec.describe GovukPublishingComponents::Presenters::InterventionHelper do
   describe "Intervention helper" do
-    it "returns the default dismiss query string" do
-      intervention_helper = GovukPublishingComponents::Presenters::InterventionHelper.new({})
-      default_dismiss_link = intervention_helper.dismiss_link
-      expect(default_dismiss_link).to eql("?hide-intervention=true")
-    end
+    describe ".dismiss_link" do
+      it "returns the default dismiss query string" do
+        intervention_helper = GovukPublishingComponents::Presenters::InterventionHelper.new({})
+        default_dismiss_link = intervention_helper.dismiss_link
 
-    it "adds the dismiss query string parameter" do
-      existing_query_string = "?a=b"
-      intervention_helper = GovukPublishingComponents::Presenters::InterventionHelper.new({ query_string: existing_query_string })
-      new_query_string = intervention_helper.dismiss_link
-      expect(new_query_string).to eql("?a=b&hide-intervention=true")
+        expect(default_dismiss_link).to eql("?hide-intervention=true")
+      end
+
+      it "adds the dismiss query string parameter" do
+        existing_query_string = "?a=b"
+        intervention_helper = GovukPublishingComponents::Presenters::InterventionHelper.new({ query_string: existing_query_string })
+        new_query_string = intervention_helper.dismiss_link
+
+        expect(new_query_string).to eql("?a=b&hide-intervention=true")
+      end
+
+      it "returns the default query string if the one passed is empty" do
+        existing_query_string = ""
+        intervention_helper = GovukPublishingComponents::Presenters::InterventionHelper.new({ query_string: existing_query_string })
+        new_query_string = intervention_helper.dismiss_link
+
+        expect(new_query_string).to eql("?hide-intervention=true")
+      end
     end
 
     it "returns the default query string if the one passed is empty" do
