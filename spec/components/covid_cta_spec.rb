47973b89c8ce3650eca2d2ddec754a70c77833e0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-12-17 10:25:01 +0000

    Add the COVID Booster CTA content

--- spec/components/covid_cta_spec.rb
+++ spec/components/covid_cta_spec.rb
@@ -24,13 +24,13 @@ describe "CovidCta", type: :view do
 
   it "shows standard covid call to action" do
     render_component({ content_item: content_item })
-    assert_select ".gem-c-contextual-sidebar__heading", text: "Covid"
-    assert_link_with_text_in(".gem-c-contextual-sidebar__text", "/covid", "Check what you need to do")
+    assert_select ".gem-c-contextual-sidebar__heading", text: I18n.t("components.related_navigation.covid_booster.title")
+    assert_link_with_text_in(".gem-c-contextual-sidebar__text", I18n.t("components.related_navigation.covid_booster.link_path"), I18n.t("components.related_navigation.covid_booster.link_text"))
   end
 
   it "shows standard covid call to action for Welsh pages" do
     I18n.with_locale(:cy) { render_component({ content_item: welsh_content_item }) }
-    assert_select ".gem-c-contextual-sidebar__heading", text: "Covid"
-    assert_link_with_text_in(".gem-c-contextual-sidebar__text", "/covid.cy", "Gwiriwch beth mae angen i chi wneud")
+    assert_select ".gem-c-contextual-sidebar__heading", text: I18n.t("components.related_navigation.covid_booster.title")
+    assert_link_with_text_in(".gem-c-contextual-sidebar__text", I18n.t("components.related_navigation.covid_booster.link_path"), I18n.t("components.related_navigation.covid_booster.link_text"))
   end
 end
