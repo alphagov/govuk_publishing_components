6b679bbb4db1f7ef3ce5240689aa568aa8972c84
Dana Cotoran <dana.cotoran@gmail.com>
2022-01-06 12:43:36 +0000

    Metadata tweak for "See all updates" interaction
    On some publications, the metadata component "See all updates" link is
    an in-page link which jumps to the list of updates at the bottom of the
    page. The reason for this is that the list of notes can be quite long
    and so it would not be ideal to have this list right at the top of the
    publication page as even when hidden behind a visibility toggle, such a
    list could take a lot of scrolling to get past and inconvenience some
    users.
    
    There was some feedback in UR that suggested that people found it
    confusing that an additional click is required to show the list of
    updates (i.e after clicking "See all updates" at the top, one must click
    "see all updates" again at the bottom; the expected behaviour is that
    the list of update notes should be made visible straight away after the
    click at the top).
    
    This is a workaround in order to achieve the intended outcome, which
    adds a simple script for the metadata component which fires a click
    event on the toggle trigger when the "See all updates" link is clicked.

---
+++ spec/javascripts/components/metadata-spec.js
@@ -0,0 +1,58 @@
+/* eslint-env jasmine,jquery */
+/* global GOVUK */
+
+describe('The metadata component', function () {
+  var element
+  var target
+
+  beforeEach(function () {
+    spyOn(GOVUK.Modules.GemToggle.prototype, 'toggleOnClick')
+  })
+
+  describe('when the "See all updates" link is clicked', function () {
+    it('clicks the target if target exists and is not already expanded', function () {
+      var FIXTURE = '<div data-module="metadata"><a href="#toggle-me" class="js-see-all-updates-link"></a></div><div id="toggle-me" data-module="gem-toggle"><a data-controls="target" data-expanded aria-expanded="false"></a><div id="target" class="js-hidden">Target</div></div>'
+      window.setFixtures(FIXTURE)
+      element = document.querySelector('[data-module="metadata"]')
+      target = document.querySelector('#toggle-me')
+
+      init(element, target)
+
+      var trigger = $(element).find('.js-see-all-updates-link')
+      trigger[0].click()
+      expect(GOVUK.Modules.GemToggle.prototype.toggleOnClick).toHaveBeenCalled()
+    })
+
+    it('does not click target if target does not exist', function () {
+      var FIXTURE = '<div data-module="metadata"><a href="#toggle-me" class="js-see-all-updates-link"></a></div>'
+      window.setFixtures(FIXTURE)
+      element = document.querySelector('[data-module="metadata"]')
+
+      init(element)
+
+      var trigger = $(element).find('.js-see-all-updates-link')
+      trigger[0].click()
+
+      expect(GOVUK.Modules.GemToggle.prototype.toggleOnClick).not.toHaveBeenCalled()
+    })
+
+    it('does not click target if target is already expanded', function () {
+      var FIXTURE = '<div data-module="metadata"><a href="#toggle-me" class="js-see-all-updates-link"></a></div><div id="toggle-me" data-module="gem-toggle"><a aria-expanded="true"></a></div>'
+      window.setFixtures(FIXTURE)
+      element = document.querySelector('[data-module="metadata"]')
+      target = document.querySelector('#toggle-me')
+
+      init(element, target)
+
+      var trigger = $(element).find('.js-see-all-updates-link')
+      trigger[0].click()
+
+      expect(GOVUK.Modules.GemToggle.prototype.toggleOnClick).not.toHaveBeenCalled()
+    })
+  })
+
+  function init (element, target) {
+    new GOVUK.Modules.Metadata(element).init()
+    target && new GOVUK.Modules.GemToggle().start($(target))
+  }
+})
