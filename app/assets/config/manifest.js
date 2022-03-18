f20caa5a159917c7fa80827ce102183c56f0f6ed
Kevin Dew <kevindew@me.com>
2022-03-18 21:13:32 +0000

    Use Sprockets 4 for app usage
    This updates the Gemfile.lock so that this app uses the current version
    of Sprockets when it's running directly as an app.
    
    I needed to add an app/assets/config/manifest.js to get around an
    error when I'd run `rake app:assets:precompile`:
    
    ```
    ➜  govuk_publishing_components git:(backwards-compatible-sprockets-4) ✗ rake app:assets:precompile
    rake aborted!
    Sprockets::Railtie::ManifestNeededError: Expected to find a manifest file in `app/assets/config/manifest.js`
    But did not, please create this file and use it to link any assets that need
    to be rendered by your app:
    
    Example:
      //= link_tree ../images
      //= link_directory ../javascripts .js
      //= link_directory ../stylesheets .css
    and restart your server
    
    For more information see: https://github.com/rails/sprockets/blob/070fc01947c111d35bb4c836e9bb71962a8e0595/UPGRADING.md#manifestjs
    /Users/kevin.dew/govuk/govuk_publishing_components/spec/dummy/config/environment.rb:5:in `<top (required)>'
    Tasks: TOP => environment => app:environment
    (See full trace by running task with --trace)
    ```
    
    This error is peculiar as the app having it's assets precompiled is in
    spec/dummy and has it's own manifest.js in spec/dummy/app/assets/config.
    Perhaps it's a bug?

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+// This file is needed for govuk_publishing_components to support
+// app:assets:precompile with Sprockets 4, it will error without it.
+//
+// See govuk_publishing_components_manifest.js for the list of assets to be
+// pre-compiled.
