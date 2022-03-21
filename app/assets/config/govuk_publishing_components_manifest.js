90a9909c58aeec178563a54169c428a738a975ac
Kevin Dew <kevindew@me.com>
2022-03-21 12:29:59 +0000

    Move the assets to be precompiled to a manifest file
    This is a step towards a more Sprockets 4 world but remains compatible
    with sprockets 3. It allows a much shorter definition of all the files
    that need outputting from the precompiling.

---
+++ app/assets/config/govuk_publishing_components_manifest.js
@@ -0,0 +1,15 @@
+// Pre-compile image and font assets from here and govuk-frontend
+//= link_tree ../images
+//= link_tree ../../../node_modules/govuk-frontend/govuk/assets/images
+//= link_tree ../../../node_modules/govuk-frontend/govuk/assets/fonts
+
+// Create asset files of each of the files in these directory
+//= link_directory ../javascripts/component_guide
+//= link_directory ../javascripts/govuk_publishing_components
+
+// Pre-compile these specific files
+//= link govuk_publishing_components/vendor/modernizr.js
+//= link govuk_publishing_components/vendor/lux/lux-reporter.js
+//= link govuk_publishing_components/vendor/lux/lux-measurer.js
+//= link component_guide/application.css
+//= link component_guide/print.css
