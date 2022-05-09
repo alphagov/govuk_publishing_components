45cf0379f75c2a2d5b68c5e15aa451e07c63e773
Andy Driver <andy@pagezero.net>
2022-05-09 09:59:06 +0100

    WIP feedback component AB test

--- app/assets/javascripts/govuk_publishing_components/components/feedback.js
+++ app/assets/javascripts/govuk_publishing_components/components/feedback.js
@@ -1,244 +1,319 @@
-/* global XMLHttpRequest, FormData */
 window.GOVUK = window.GOVUK || {}
 window.GOVUK.Modules = window.GOVUK.Modules || {};
 
 (function (Modules) {
+  var CONDITION = 0
+  var CALLBACK = 1
+  var NEXT_STATE = 2
+
+  function StateMachine (initialState, transitionTable) {
+    this.state = this.initialState = initialState
+    this.transitionTable = transitionTable
+  }
+
+  StateMachine.prototype.handleInput = function (input) {
+    var transitions = (this.transitionTable[this.state] || {})[input] || []
+    for (var i in transitions) {
+      if (Object.prototype.hasOwnProperty.call(transitions, i)) {
+        var transition = transitions[i]
+        if (this.isConditionMet(transition[CONDITION])) {
+          this.executeCallback(transition[CALLBACK])
+          this.state = transition[NEXT_STATE]
+          return this.state
+        }
+      }
+    }
+    throw new Error('Invalid input for current state')
+  }
+
+  StateMachine.prototype.isConditionMet = function (condition) {
+    if (typeof condition === 'function') {
+      return condition()
+    } else if (condition === null) {
+      return true
+    }
+    return condition
+  }
+
+  StateMachine.prototype.executeCallback = function (callback) {
+    if (typeof callback === 'function') {
+      callback()
+    }
+  }
+
   function Feedback ($module) {
     this.$module = $module
-    this.somethingIsWrongForm = this.$module.querySelector('#something-is-wrong')
-    this.surveyForm = this.$module.querySelector('#page-is-not-useful')
-    this.prompt = this.$module.querySelector('.js-prompt')
-    this.forms = this.$module.querySelectorAll('.js-feedback-form')
-    this.toggleForms = this.$module.querySelectorAll('.js-toggle-form')
+
+    this.states = this.$module.querySelectorAll('.js-feedback-state')
     this.closeForms = this.$module.querySelectorAll('.js-close-form')
-    this.activeForm = false
-    this.pageIsUsefulButton = this.$module.querySelector('.js-page-is-useful')
-    this.pageIsNotUsefulButton = this.$module.querySelector('.js-page-is-not-useful')
-    this.somethingIsWrongButton = this.$module.querySelector('.js-something-is-wrong')
+    this.prompt = this.$module.querySelector('.js-prompt')
     this.promptQuestions = this.$module.querySelectorAll('.js-prompt-questions')
-    this.promptSuccessMessage = this.$module.querySelector('.js-prompt-success')
-    this.surveyWrapper = this.$module.querySelector('#survey-wrapper')
-    this.jshiddenClass = 'js-hidden'
+    this.forms = this.$module.querySelectorAll('.js-feedback-form')
+
+    this.HIDE_ALL = function () { return false }
+    this.SHOW_ALL = function () { return true }
+
+    this.variant = 'variant_A'
+
+    this.workflows = {
+      default: [
+        'feedback-prompt',
+        {
+          'feedback-prompt': {
+            useful: [
+              [null, null, 'feedback-success']
+            ],
+            'not-useful': [
+              [null, null, 'page-is-not-useful']
+            ],
+            'report-a-problem': [
+              [null, null, 'something-is-wrong']
+            ]
+          },
+          'page-is-not-useful': {
+            close: [
+              [null, null, 'feedback-prompt']
+            ],
+            submit: [
+              [null, this.sendSurvey(), 'feedback-success']
+            ]
+          },
+          'something-is-wrong': {
+            close: [
+              [null, null, 'feedback-prompt']
+            ],
+            submit: [
+              [null, this.reportProblem(), 'feedback-success']
+            ]
+          }
+        }
+      ],
+      variant_A: [
+        'useful_or_not',
+        {
+          useful_or_not: {
+            useful: [
+              [null, null, 'thanks']
+            ],
+            not_useful: [
+              [null, null, 'why_not']
+            ]
+          },
+          why_not: {
+            continue: [
+              [this.is_checked('[name="why_not_useful"][value="error"]'), this.set_flag('error', true), 'what_wrong'],
+              [null, this.set_flag('error', false), 'what_doing']
+            ],
+            back: [
+              [null, null, 'useful_or_not']
+            ]
+          },
+          what_wrong: {
+            continue: [
+              [null, null, 'what_doing']
+            ],
+            back: [
+              [null, null, 'why_not']
+            ]
+          },
+          what_doing: {
+            continue: [
+              [null, null, 'use_assistive']
+            ],
+            back: [
+              [this.is_flag_set('error'), null, 'what_wrong'],
+              [null, null, 'why_not']
+            ]
+          },
+          use_assistive: {
+            continue: [
+              [this.is_checked('[name="use_assistive"][value="yes"]'), null, 'which_assistive'],
+              [null, null, 'thanks']
+            ],
+            back: [
+              [null, null, 'what_doing']
+            ]
+          },
+          which_assistive: {
+            continue: [
+              [null, null, 'thanks']
+            ],
+            back: [
+              [null, null, 'use_assistive']
+            ]
+          }
+        }
+      ],
+      variant_B: [
+        'tell_experience',
+        {
+          tell_experience: {
+            submit: [
+              [null, null, 'useful_or_not_b']
+            ]
+          },
+          useful_or_not_b: {
+            continue: [
+              [this.is_checked('[name="useful"][value="yes"]'), null, 'thanks'],
+              [null, null, 'why_not']
+            ],
+            back: [
+              [null, null, 'tell_experience']
+            ]
+          },
+          why_not: {
+            continue: [
+              [this.is_checked('[name="why_not_useful"][value="error"]'), this.set_flag('error', true), 'what_wrong'],
+              [null, this.set_flag('error', false), 'what_doing']
+            ],
+            back: [
+              [null, null, 'useful_or_not_b']
+            ]
+          },
+          what_wrong: {
+            continue: [
+              [null, null, 'what_doing']
+            ],
+            back: [
+              [null, null, 'why_not']
+            ]
+          },
+          what_doing: {
+            continue: [
+              [null, null, 'use_assistive']
+            ],
+            back: [
+              [this.is_flag_set('error'), null, 'what_wrong'],
+              [null, null, 'why_not']
+            ]
+          },
+          use_assistive: {
+            continue: [
+              [this.is_checked('[name="use_assistive"][value="yes"]'), null, 'which_assistive'],
+              [null, null, 'thanks']
+            ],
+            back: [
+              [null, null, 'what_doing']
+            ]
+          },
+          which_assistive: {
+            continue: [
+              [null, null, 'thanks']
+            ],
+            back: [
+              [null, null, 'use_assistive']
+            ]
+          }
+        }
+      ]
+    }
   }
 
   Feedback.prototype.init = function () {
-    this.setInitialAriaAttributes()
-    this.setHiddenValues()
+    this.flags = {}
+    this.initialiseWorkflow(this.getVariant())
+    this.showState(this.workflow.initialState)
+    this.addStateTransitionListeners()
+    this.addFormSubmitHandlers()
+    this.showHide(this.closeForms, this.SHOW_ALL)
+    this.showHide(this.promptQuestions, this.SHOW_ALL)
+  }
 
-    this.prompt.hidden = false
-    for (var k = 0; k < this.promptQuestions.length; k++) {
-      this.promptQuestions[k].hidden = false
+  Feedback.prototype.getVariant = function () {
+    var matches = window.location.search.match(/variant=([^&]*)/)
+    if (matches) {
+      return matches[1] || 'default'
     }
-    this.surveyForm.hidden = true
+    return 'default'
+  }
 
-    for (var j = 0; j < this.toggleForms.length; j++) {
-      this.toggleForms[j].addEventListener('click', function (e) {
-        e.preventDefault()
-        var el = e.target.closest('button')
-        this.toggleForm(el.getAttribute('aria-controls'))
-        this.trackEvent(this.getTrackEventParams(el))
-        this.updateAriaAttributes(el)
-      }.bind(this))
+  Feedback.prototype.initialiseWorkflow = function (variant) {
+    var args = this.workflows[variant]
+    this.workflow = new StateMachine(args[0], args[1])
+  }
+
+  Feedback.prototype.showState = function (id) {
+    var shouldShow = function (state) { return state.id === id }
+
+    // special case for feedback-success because the success message is in the
+    // feedback-prompt div
+    if (id === 'feedback-success') {
+      shouldShow = this.showSuccess
+      this.showHide(this.promptQuestions, this.HIDE_ALL)
     }
 
-    for (var i = 0; i < this.closeForms.length; i++) {
-      this.closeForms[i].hidden = false
-      this.closeForms[i].addEventListener('click', function (e) {
-        e.preventDefault()
+    this.showHide(this.states, shouldShow)
+  }
+
+  Feedback.prototype.showHide = function (elements, filter) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].hidden = !filter(elements[i])
+    }
+  }
+
+  Feedback.prototype.addStateTransitionListeners = function () {
+    var inputs = this.$module.querySelectorAll('.js-feedback-input')
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener('click', function (e) {
         var el = e.target
-        var formToToggle = el.getAttribute('aria-controls')
-        this.toggleForm(formToToggle)
-        this.trackEvent(this.getTrackEventParams(el))
-        this.setInitialAriaAttributes()
-        this.revealInitialPrompt()
-        var refocusClass = '.js-' + formToToggle
-        this.$module.querySelector(refocusClass).focus()
+        if (el.getAttribute('data-prevent-default')) {
+          e.preventDefault()
+        }
+        this.workflow.handleInput(el.getAttribute('data-feedback-input'))
+        this.showState(this.workflow.state)
+        // this.trackEvent(this.getTrackEventParams(el))
       }.bind(this))
     }
+  }
 
-    this.pageIsUsefulButton.addEventListener('click', function (e) {
-      e.preventDefault()
-      this.trackEvent(this.getTrackEventParams(this.pageIsUsefulButton))
-      this.showFormSuccess()
-      this.revealInitialPrompt()
-    }.bind(this))
+  Feedback.prototype.addFormSubmitHandlers = function () {
+    // IE11 does not support JSON as an XMLHttpRequest response type.
+    // This check fails on IE, allowing form submits to happend normally
+    if (typeof window.URLSearchParams !== 'function') return
 
-    this.pageIsNotUsefulButton.addEventListener('click', function (e) {
-      var gaClientId
-      var dummyGaClientId = '111111111.1111111111'
-      if (window.GOVUK.cookie('_ga') === null || window.GOVUK.cookie('_ga') === '') {
-        gaClientId = dummyGaClientId
-      } else {
-        gaClientId = window.GOVUK.cookie('_ga').split('.').slice(-2).join('.')
+    for (var i = 0; i < this.forms.length; i++) {
+      this.forms[i].addEventListener('submit', function (e) {
+        e.preventDefault()
+      })
+    }
+  }
+
+  Feedback.prototype.is_checked = function (selector) {
+    return function () {
+      var radio = this.$module.querySelector(selector)
+      if (radio) {
+        return radio.checked
       }
-      this.setHiddenValuesNotUsefulForm(gaClientId)
-    }.bind(this))
+      return false
+    }.bind(this)
+  }
 
-    // much of the JS needed to support sending the form contents via this script is
-    // unsupported by IE, even IE11. This check causes IE to not intercept form submits
-    // and let them happen normally, which is handled already by the backend
-    if (typeof window.URLSearchParams === 'function') {
-      for (var f = 0; f < this.forms.length; f++) {
-        this.forms[f].addEventListener('submit', function (e) {
-          e.preventDefault()
-          var $form = e.target
-          var xhr = new XMLHttpRequest()
-          var url = $form.getAttribute('action')
-          var params = new FormData($form)
-          params = new URLSearchParams(params).toString()
+  Feedback.prototype.set_flag = function (flag, value) {
+    return function () {
+      this.flags[flag] = !!value
+    }.bind(this)
+  }
 
-          this.done = function () {
-            if (xhr.status === 200) {
-              this.trackEvent(this.getTrackEventParams($form))
-              this.showFormSuccess(xhr.message)
-              this.revealInitialPrompt()
-              this.setInitialAriaAttributes()
-              this.activeForm.hidden = true
-            } else {
-              this.showError(xhr)
-              this.enableSubmitFormButton($form)
-            }
-          }.bind(this)
+  Feedback.prototype.is_flag_set = function (flag) {
+    return function () {
+      return !!this.flags[flag]
+    }.bind(this)
+  }
 
-          xhr.addEventListener('loadend', this.done)
-          xhr.open('POST', url, true)
-          xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+  Feedback.prototype.showSuccess = function (state) {
+    return state.id === 'feedback-success' || state.id === 'feedback-prompt'
+  }
 
-          this.disableSubmitFormButton($form)
-          xhr.send(params)
-        }.bind(this))
-      }
+  Feedback.prototype.sendSurvey = function () {
+    return function () {
+      console.log('send survey')
     }
   }
 
-  Feedback.prototype.disableSubmitFormButton = function ($form) {
-    var formButton = $form.querySelector('[type="submit"]')
-    formButton.setAttribute('disabled', true)
-  }
-
-  Feedback.prototype.enableSubmitFormButton = function ($form) {
-    var formButton = $form.querySelector('[type="submit"]')
-    formButton.removeAttribute('disabled')
-  }
-
-  Feedback.prototype.setInitialAriaAttributes = function () {
-    this.pageIsNotUsefulButton.setAttribute('aria-expanded', false)
-    this.somethingIsWrongButton.setAttribute('aria-expanded', false)
-  }
-
-  Feedback.prototype.setHiddenValues = function () {
-    var javascriptEnabled = document.createElement('input')
-    javascriptEnabled.setAttribute('type', 'hidden')
-    javascriptEnabled.setAttribute('name', 'javascript_enabled')
-    javascriptEnabled.setAttribute('value', true)
-    this.somethingIsWrongForm.appendChild(javascriptEnabled)
-
-    var referrer = document.createElement('input')
-    referrer.setAttribute('type', 'hidden')
-    referrer.setAttribute('name', 'referrer')
-    referrer.setAttribute('value', document.referrer || 'unknown')
-    this.somethingIsWrongForm.appendChild(referrer)
-    this.somethingIsWrongForm.invalidInfoError = [
-      '<h2>Sorry, we’re unable to send your message as you haven’t given us any information.</h2>',
-      ' <p>Please tell us what you were doing or what went wrong</p>'
-    ].join('')
-  }
-
-  Feedback.prototype.setHiddenValuesNotUsefulForm = function (gaClientId) {
-    var currentPathName = window.location.pathname.replace(/[^\s=?&]+(?:@|%40)[^\s=?&]+/, '[email]')
-    var finalPathName = encodeURI(currentPathName)
-    this.surveyForm.invalidInfoError = [
-      '<h2>Sorry, we’re unable to send your message as you haven’t given us a valid email address.</h2>',
-      ' <p>Enter an email address in the correct format, like name@example.com</p>'
-    ].join('')
-    if (document.querySelectorAll('[name="email_survey_signup[ga_client_id]"]').length === 0) {
-      var hiddenInput = document.createElement('input')
-      hiddenInput.setAttribute('type', 'hidden')
-      hiddenInput.setAttribute('name', 'email_survey_signup[ga_client_id]')
-      hiddenInput.setAttribute('value', gaClientId || '0')
-      this.surveyForm.appendChild(hiddenInput)
+  Feedback.prototype.reportProblem = function () {
+    return function () {
+      console.log('report problem')
     }
-
-    if (document.querySelectorAll('.gem-c-feedback__email-link#take-survey').length === 0) {
-      var takeSurvey = document.createElement('a')
-      takeSurvey.setAttribute('href', 'https://www.smartsurvey.co.uk/s/gov-uk-banner/?c=' + finalPathName + '&amp;gcl=' + gaClientId)
-      takeSurvey.setAttribute('class', 'gem-c-feedback__email-link govuk-link')
-      takeSurvey.setAttribute('id', 'take-survey')
-      takeSurvey.setAttribute('target', '_blank')
-      takeSurvey.setAttribute('rel', 'noopener noreferrer')
-      takeSurvey.innerHTML = 'Don’t have an email address?'
-      this.surveyWrapper.appendChild(takeSurvey)
-    }
-  }
-
-  Feedback.prototype.updateAriaAttributes = function (linkClicked) {
-    linkClicked.setAttribute('aria-expanded', true)
-  }
-
-  Feedback.prototype.toggleForm = function (formId) {
-    this.activeForm = this.$module.querySelector('#' + formId)
-    this.activeForm.hidden ? this.activeForm.hidden = false : this.activeForm.hidden = true
-    this.prompt.hidden ? this.prompt.hidden = false : this.prompt.hidden = true
-
-    if (!this.activeForm.hidden) {
-      this.activeForm.querySelectorAll('.gem-c-textarea .govuk-textarea, .gem-c-input.govuk-input')[0]
-        .focus()
-    } else {
-      this.activeForm = false
-    }
-  }
-
-  Feedback.prototype.getTrackEventParams = function ($node) {
-    return {
-      category: $node.getAttribute('data-track-category'),
-      action: $node.getAttribute('data-track-action')
-    }
-  }
-
-  Feedback.prototype.trackEvent = function (trackEventParams) {
-    if (window.GOVUK.analytics && window.GOVUK.analytics.trackEvent) {
-      window.GOVUK.analytics.trackEvent(trackEventParams.category, trackEventParams.action)
-    }
-  }
-
-  Feedback.prototype.showError = function (error) {
-    var genericError = [
-      '<h2>Sorry, we’re unable to receive your message right now.</h2>',
-      ' <p>If the problem persists, we have other ways for you to provide',
-      ' feedback on the <a href="/contact/govuk">contact page</a>.</p>'
-    ].join('')
-    // if the response is not a 404 or 500, show the error message if it exists
-    // otherwise show the generic message
-    if ('response' in error) {
-      if (typeof error.response === 'object' && error.response !== null) {
-        error = error.response.message === 'email survey sign up failure' ? genericError : error.response.message
-      } else {
-        error = genericError
-      }
-    } else if (error.status === 422) {
-      // there's clobbering by nginx on all 422 requests, which is why the response returns a rendered html page instead of the expected JSON
-      // this is a temporary workaround to ensure that we are displaying relevant error messages to the users
-      error = this.activeForm.invalidInfoError || genericError
-    } else {
-      error = genericError
-    }
-    var $errors = this.activeForm.querySelector('.js-errors')
-    $errors.innerHTML = error
-    $errors.hidden = false
-    $errors.focus()
-  }
-
-  Feedback.prototype.showFormSuccess = function () {
-    for (var i = 0; i < this.promptQuestions.length; i++) {
-      this.promptQuestions[i].hidden = true
-    }
-    this.promptSuccessMessage.hidden = false
-    this.promptSuccessMessage.focus()
-  }
-
-  Feedback.prototype.revealInitialPrompt = function () {
-    this.prompt.hidden = false
-    this.prompt.focus()
   }
 
   Modules.Feedback = Feedback
