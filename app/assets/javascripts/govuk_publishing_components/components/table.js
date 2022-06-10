8a7242aa00be6908a040a6286ad2b23c450fa90e
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2022-06-10 10:58:28 +0100

    Further updates code for filter section of table component - Adds hidden filter count for a11y - Improves tests - Updates content - Improves component JS - Adds option to add custom label for input - Improves filter accessibility

--- app/assets/javascripts/govuk_publishing_components/components/table.js
+++ app/assets/javascripts/govuk_publishing_components/components/table.js
@@ -2,38 +2,44 @@ window.GOVUK = window.GOVUK || {}
 window.GOVUK.Modules = window.GOVUK.Modules || {};
 
 (function (Modules) {
-  function TableFilter ($module) {
+  function Table ($module) {
     this.$module = $module
     this.searchInput = $module.querySelector('input[name="filter"]')
     this.tableRows = $module.querySelectorAll('.js-govuk-table__row')
     this.filter = $module.querySelector('.js-gem-c-table__filter')
+    this.filterCount = this.filter.querySelector('.js-filter-count')
     this.message = $module.querySelector('.js-gem-c-table__message')
     this.hiddenClass = 'govuk-!-display-none'
+    this.filterCountText = this.filterCount.getAttribute('data-count-text')
+    this.tableRowsContent = []
+
+    for (var i = 0; i < this.tableRows.length; i++) {
+      this.tableRowsContent.push(this.tableRows[i].textContent.toUpperCase())
+    }
   }
 
-  TableFilter.prototype.init = function () {
+  Table.prototype.init = function () {
     this.$module.updateRows = this.updateRows.bind(this)
     this.filter.classList.remove(this.hiddenClass)
     this.searchInput.addEventListener('input', this.$module.updateRows)
   }
 
   // Reads value of input and filters content
-  TableFilter.prototype.updateRows = function () {
+  Table.prototype.updateRows = function () {
     var value = this.searchInput.value
-    var hiddenClass = this.hiddenClass
     var hiddenRows = 0
     var length = this.tableRows.length
 
     for (var i = 0; i < length; i++) {
-      var row = this.tableRows[i]
-
-      row.classList.remove(hiddenClass)
-
-      if (!row.textContent.toUpperCase().includes(value.toUpperCase())) {
-        row.classList.add(hiddenClass)
+      if (this.tableRowsContent[i].includes(value.toUpperCase())) {
+        this.tableRows[i].classList.remove(this.hiddenClass)
+      } else {
+        this.tableRows[i].classList.add(this.hiddenClass)
         hiddenRows++
       }
     }
+
+    this.filterCount.textContent = (length - hiddenRows) + ' ' + this.filterCountText
 
     if (length === hiddenRows) {
       this.message.classList.remove(this.hiddenClass)
@@ -42,5 +48,5 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     }
   }
 
-  Modules.TableFilter = TableFilter
+  Modules.Table = Table
 })(window.GOVUK.Modules)
