e770ecfbd92e65f613a52f8dd30cd1f97159d121
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2022-06-15 09:11:11 +0100

    Initialise page view on page load
    - this will need to be modified before production to integrate with the cookie consent mechanism, for now this is for testing purposes on integration only

--- app/assets/javascripts/govuk_publishing_components/analytics-ga4.js
+++ app/assets/javascripts/govuk_publishing_components/analytics-ga4.js
@@ -1,1 +1,4 @@
+//= require ./analytics-ga4/gtm-page-views
 //= require ./analytics-ga4/gtm-click-tracking
+
+window.GOVUK.Gtm.sendPageView() // this will need integrating with cookie consent before production
