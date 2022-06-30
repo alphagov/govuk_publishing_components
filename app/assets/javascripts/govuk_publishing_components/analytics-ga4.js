46b50695f1f719f2fb6610f3e1f925d7a815b619
AshGDS <ashley.burnett@digital.cabinet-office.gov.uk>
2022-06-30 16:04:26 +0100

    Add comment about ga4 modules being imported in a certain order

--- app/assets/javascripts/govuk_publishing_components/analytics-ga4.js
+++ app/assets/javascripts/govuk_publishing_components/analytics-ga4.js
@@ -1,3 +1,4 @@
+// The following modules are imported in a specific order
 //= require ./analytics-ga4/pii-remover
 //= require ./analytics-ga4/gtm-page-views
 //= require ./analytics-ga4/gtm-click-tracking
