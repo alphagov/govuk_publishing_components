cadd93fb2b618978ae64502843e4b56ccd43e85f
James Chan <james.chan@digital.cabinet-office.gov.uk>
2022-08-09 21:21:12 +0100

    Update import

--- app/assets/javascripts/govuk_publishing_components/analytics-ga4.js
+++ app/assets/javascripts/govuk_publishing_components/analytics-ga4.js
@@ -2,6 +2,6 @@
 //= require ./analytics-ga4/gtm-schemas
 //= require ./analytics-ga4/pii-remover
 //= require ./analytics-ga4/gtm-page-views
-//= require ./analytics-ga4/gtm-click-tracking
+//= require ./analytics-ga4/gtm-event-click-tracker
 
 window.GOVUK.Gtm.sendPageView() // this will need integrating with cookie consent before production
