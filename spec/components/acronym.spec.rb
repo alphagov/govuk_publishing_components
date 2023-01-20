890dceb4b5ed739dbdcec2de191aac5bf66fb6e3
James Chan <james.chan@digital.cabinet-office.gov.uk>
2023-01-20 11:54:05 +0000

    Expand acronym component

--- spec/components/acronym.spec.rb
+++ spec/components/acronym.spec.rb
@@ -9,4 +9,10 @@ describe "Acronym", type: :view do
         test_data = {}
         assert_empty render_component(test_data)
     end
+
+    it "shows an acronym" do    
+        render_component(
+            acronym: "WWW"
+        )
+        assert_select '.gem-c-acronym', text: 'WWW'
 end
