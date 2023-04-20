9d89202accd3ed6212b20b6f18da122a8af59127
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2023-04-20 08:47:18 +0100

    WIP test the cookie consent api

--- app/assets/javascripts/govuk_publishing_components/components/cookie-banner.js
+++ app/assets/javascripts/govuk_publishing_components/components/cookie-banner.js
@@ -10,6 +10,25 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   }
 
   CookieBanner.prototype.init = function () {
+    var acceptAllCookies = JSON.stringify({
+      "essential": true,
+      "campaigns": false,
+      "settings": false,
+      "usage": false
+    })
+    window.Consent.setStatus(
+      acceptAllCookies,
+      (status) => {
+        console.log("Consent status successfully updated to", status)
+      }
+    )    
+    window.Consent.onStatusLoaded((status) => {
+      console.log("Consent Status:")
+      console.log(`- Essential cookies (${status.essential})`)
+      console.log(`- Campaign cookies (${status.campaigns})`)
+      console.log(`- Settings cookies (${status.settings})`)
+      console.log(`- Usage cookies (${status.usage})`)
+    })    
     this.$module.hideCookieMessage = this.hideCookieMessage.bind(this)
     this.$module.showConfirmationMessage = this.showConfirmationMessage.bind(this)
     this.$module.setCookieConsent = this.setCookieConsent.bind(this)
