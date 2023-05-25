9f446912da223cd96035f995bcd97c02aa4bd2fb
AshGDS <8880610+AshGDS@users.noreply.github.com>
2023-05-25 16:01:53 +0100

    Add tracking: previous and next page links

--- spec/components/previous_and_next_navigation_spec.rb
+++ spec/components/previous_and_next_navigation_spec.rb
@@ -68,6 +68,46 @@ describe "Previous and next navigation", type: :view do
     assert_select ".govuk-pagination__next .govuk-pagination__link .govuk-visually-hidden", false
   end
 
+  it "adds GA4 tracking when ga4_tracking is true" do
+    render_component(
+      previous_page: {
+        url: "previous-page",
+        title: "Previous page",
+      },
+      next_page: {
+        url: "next-page",
+        title: "Next page",
+      },
+      ga4_tracking: true,
+    )
+
+    assert_select ".govuk-pagination" do |pagination|
+      expect(pagination.attr("data-module").to_s).to eq "ga4-link-tracker"
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "content",
+      "text": "Previous page",
+      "section": "Previous",
+    }.to_json
+
+    assert_select ".govuk-pagination__prev a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "content",
+      "text": "Next page",
+      "section": "Next",
+    }.to_json
+
+    assert_select ".govuk-pagination__next a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+  end
+
   def assert_link(link)
     assert_select "a[href=\"#{link}\"]"
   end
