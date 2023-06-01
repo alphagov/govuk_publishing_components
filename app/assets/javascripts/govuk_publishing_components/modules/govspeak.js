0d9d59ed00a721114eda0192cae7e91bad795115
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-06-01 14:22:07 +0100

    thoughts about govspeak

--- app/assets/javascripts/govuk_publishing_components/modules/govspeak.js
+++ app/assets/javascripts/govuk_publishing_components/modules/govspeak.js
@@ -1,6 +1,3 @@
-// if we want to dispense entirely with the window object
-// i think we might have to use js-bundling? unless i can do
-
 class Govspeak {
   constructor($module) {
     this.$module = $module
