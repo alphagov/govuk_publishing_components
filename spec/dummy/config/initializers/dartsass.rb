4336653a2770a56366f0384c0ea8cf3dc4c71629
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2023-10-10 14:30:40 +0100

    Configure builds
    - Create `dartsass.builds` initializer and add all Sass files to be compiled. See https://github.com/rails/dartsass-rails#configuring-builds
    - Create .keep
    - Update manifest file to use /builds directory
    - Ignore /builds
    
    Co-Authored-By: Martin Jones <28779939+MartinJJones@users.noreply.github.com>

---
+++ spec/dummy/config/initializers/dartsass.rb
@@ -0,0 +1,8 @@
+APP_STYLESHEETS = {
+  "components/_app-component.scss" => "components/_app-component.css",
+  "views/_app-view.scss" => "views/_app-view.css",
+  "application.scss" => "application.css",
+}.freeze
+
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+Rails.application.config.dartsass.builds = all_stylesheets
