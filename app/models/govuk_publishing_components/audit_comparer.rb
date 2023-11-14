c4253cc49bdd81e2138ed8a0e1bf3b031401370e
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-11-14 10:01:43 +0000

    Audit now checks if component uses govuk-frontend
    If a component being used in an application is uses govuk-frontend
    ES6 JS, then a tag will be added to indicate this in the Component
    Audit view. This is useful to determine which components are using
    ES6 JS and need to have their JS be loaded in a different file.

--- app/models/govuk_publishing_components/audit_comparer.rb
+++ app/models/govuk_publishing_components/audit_comparer.rb
@@ -279,6 +279,7 @@ module GovukPublishingComponents
 
         results << {
           name: component_name,
+          govuk_frontend: component[:uses_govuk_frontend],
           count: locations.length,
           locations:,
         }
