e15997eaeb078c56a793d37c3c139a4e50ff009d
Patrick Cartlidge <patrickcartlidge@gmail.com>
2023-11-28 15:57:01 +0000

    Update govuk-frontend .js file names
    In the govuk-frontend v5, the .js of UMD files has been changed to use
    the "*.bundle.js" suffix. This means we have to update references to
    those UMD files in the JS of the components.

--- app/assets/javascripts/govuk_publishing_components/components/tabs.js
+++ app/assets/javascripts/govuk_publishing_components/components/tabs.js
@@ -1,5 +1,5 @@
 // This component relies on JavaScript from GOV.UK Frontend
-// = require govuk/components/tabs/tabs.js
+// = require govuk/components/tabs/tabs.bundle.js
 window.GOVUK = window.GOVUK || {}
 window.GOVUK.Modules = window.GOVUK.Modules || {}
 window.GOVUK.Modules.GovukTabs = window.GOVUKFrontend.Tabs
