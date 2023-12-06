530c2a77fca20214039878bbda7df565628d81d9
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2023-12-06 11:12:05 +0000

    Pass GA4 tracking attributes to components
    - removes the need for some of the JS around setting indexes on JS generated elements

--- app/assets/javascripts/analytics-ga4/ga4-finder-tracker.js
+++ app/assets/javascripts/analytics-ga4/ga4-finder-tracker.js
@@ -6,11 +6,6 @@
   GOVUK.analyticsGa4 = GOVUK.analyticsGa4 || {}
 
   GOVUK.analyticsGa4.Ga4FinderTracker = {
-
-    setFilterIndexes: function () {
-      window.GOVUK.triggerEvent(window, 'ga4-filter-indexes-added')
-    },
-
     // Called when the search results updates. Takes the event target, and a string containing the type of change and element type. Creates the GTM schema and pushes it.
     // changeEventMetadata is a string referring to the type of form change and the element type that triggered it. For example 'update-filter checkbox'.
     trackChangeEvent: function (eventTarget, changeEventMetadata) {
@@ -144,31 +139,6 @@
       } catch (e) {
         console.error('GA4 configuration error: ' + e.message, window.location)
       }
-    },
-
-    addFilterButtonTracking: function (button, section) {
-      button.setAttribute('data-ga4-expandable', '')
-      var ga4JSON = { event_name: 'select_content', type: 'finder', section: section }
-
-      try {
-        var index = button.closest('[data-ga4-index]') || undefined
-        if (index) {
-          var indexData = JSON.parse(index.getAttribute('data-ga4-index'))
-          // flatten the attributes in index into the main data
-          for (var prop in indexData) {
-            ga4JSON[prop] = indexData[prop]
-          }
-        } else {
-          console.warn('No data-ga4-index found on the following element or its parents:')
-          console.warn(button)
-        }
-      } catch (e) {
-        // if there's a problem with the index object, don't add the attribute
-        console.error('GA4 configuration error: ' + e.message, window.location)
-        return
-      }
-
-      button.setAttribute('data-ga4-event', JSON.stringify(ga4JSON))
     }
   }
 
