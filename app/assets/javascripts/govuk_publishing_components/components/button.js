c5faf5ad7f95cb144449aac3015f710c742f95a1
AshGDS <8880610+AshGDS@users.noreply.github.com>
2024-06-03 10:57:07 +0100

    Fix 'use strict' on button.js

--- app/assets/javascripts/govuk_publishing_components/components/button.js
+++ app/assets/javascripts/govuk_publishing_components/components/button.js
@@ -1,5 +1,8 @@
+ 'use strict'
 // This component relies on JavaScript from GOV.UK Frontend
+var Button = {}
 // = require govuk/components/button/button.bundle.js
 window.GOVUK = window.GOVUK || {}
 window.GOVUK.Modules = window.GOVUK.Modules || {}
+window.GOVUKFrontend = window.GOVUKFrontend || {}
 window.GOVUK.Modules.GovukButton = window.GOVUKFrontend.Button
