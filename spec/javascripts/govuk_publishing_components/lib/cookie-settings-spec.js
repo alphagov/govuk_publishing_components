57d0f01c6f467ce5da3ea3880f1bb475f8e8c3ef
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-06-19 17:11:49 +0100

    Remove UA from cookie settings code

--- spec/javascripts/govuk_publishing_components/lib/cookie-settings-spec.js
+++ spec/javascripts/govuk_publishing_components/lib/cookie-settings-spec.js
@@ -130,20 +130,6 @@ describe('cookieSettings', function () {
         expect(window.GOVUK.setCookie).toHaveBeenCalledWith('cookies_preferences_set', true, { days: 365 })
         expect(GOVUK.cookie('cookies_preferences_set')).toBeTruthy()
       })
-
-      it('fires a Google Analytics event', function () {
-        spyOn(GOVUK.analytics, 'trackEvent').and.callThrough()
-
-        new GOVUK.Modules.CookieSettings(element).init()
-
-        element.querySelector('#settings-on').checked = false
-        element.querySelector('#settings-off').checked = true
-
-        var button = element.querySelector('#submit-button')
-        button.click()
-
-        expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith('cookieSettings', 'Save changes', { label: 'settings-no usage-no campaigns-no ' })
-      })
     })
 
     describe('showConfirmationMessage', function () {
@@ -232,45 +218,6 @@ describe('cookieSettings', function () {
 
       expect(window.GOVUK.setConsentCookie).not.toHaveBeenCalled()
       expect(window.GOVUK.singleConsent.init).toHaveBeenCalled()
-    })
-  })
-
-  describe('when setting usage cookies to false', function () {
-    beforeEach(function () {
-      spyOn(GOVUK.analyticsGa4.core, 'getGemVersion').and.returnValue('aVersion')
-    })
-    it('stops analytics tracking', function () {
-      var analytics = new GOVUK.Analytics({
-        universalId: 'universal-id',
-        cookieDomain: '.www.gov.uk',
-        siteSpeedSampleRate: 100
-      })
-      window.dataLayer = []
-
-      // Expect the default return value when you send an analytics event
-      expect(analytics.trackEvent()).toEqual(undefined)
-      expect(analytics.trackShare()).toEqual(undefined)
-      expect(analytics.trackPageview()).toEqual(undefined)
-      expect(GOVUK.analyticsGa4.core.sendData({})).toEqual(undefined)
-
-      new GOVUK.Modules.CookieSettings(element).init()
-
-      element.querySelector('#usage-on').checked = false
-      element.querySelector('#usage-off').checked = true
-
-      var button = element.querySelector('#submit-button')
-      button.click()
-
-      expect(window.GOVUK.stopSendingAnalytics).toEqual(true)
-
-      // Expect a false return value now when you send an analytics event
-
-      expect(analytics.trackEvent()).toEqual(false)
-      expect(analytics.trackShare()).toEqual(false)
-      expect(analytics.trackPageview()).toEqual(false)
-      expect(window.GOVUK.analyticsGa4.core.sendData()).toEqual(false)
-
-      expect(window.GOVUK.LUX).toEqual({})
     })
   })
 })
