a4d8ba76db3fc44255c2e4ccf4ca6cb46e10c2d2
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-08-12 14:29:48 +0100

    Remove libsass fallback configuration
    - removes code added to detect if the parent application is using libsass instead of dart-sass and compile appropriately
    - changes originally added as part of https://github.com/alphagov/govuk_publishing_components/pull/3726

--- spec/features/manifest_spec.rb
+++ spec/features/manifest_spec.rb
@@ -2,36 +2,7 @@ require "rails_helper"
 
 describe "with `dartsass-rails`" do
   it "should include `govuk_publishing_components_manifest.js`" do
-    if Gem.loaded_specs.key?("dartsass-rails")
-      expect(Rails.application.config.assets.precompile).to include("govuk_publishing_components_manifest.js")
-      expect(Rails.application.config.assets.precompile).not_to include("govuk_publishing_components_sassc-rails_manifest.js")
-    end
-  end
-end
-
-describe "with `sassc-rails`" do
-  before "update `loaded_specs` to simulate using `sassc-rails`" do
-    Gem.loaded_specs.delete("dartsass-rails")
-    Gem.loaded_specs["sassc-rails"] = Gem::Specification.new do |s|
-      s.name = "sassc-rails"
-      s.version = Gem::Version.new("2.1")
-    end
-    Rails.application.config.assets.precompile = ["manifest.js"]
-    load Rails.root.join(GovukPublishingComponents::Config.gem_directory, "config", "initializers", "assets.rb")
-  end
-
-  it "should include `govuk_publishing_components_sassc-rails_manifest.js`" do
-    if Gem.loaded_specs.key?("sassc-rails")
-      expect(Rails.application.config.assets.precompile).to include("govuk_publishing_components_sassc-rails_manifest.js")
-      expect(Rails.application.config.assets.precompile).not_to include("govuk_publishing_components_manifest.js")
-    end
-  end
-
-  after "update `loaded_specs` to its initial state" do
-    Gem.loaded_specs.delete("sassc-rails")
-    Gem.loaded_specs["dartsass-rails"] = Gem::Specification.new do |s|
-      s.name = "dartsass-rails"
-      s.version = Gem::Version.new("0.5.0")
-    end
+    expect(Rails.application.config.assets.precompile).to include("govuk_publishing_components_manifest.js")
+    expect(Rails.application.config.assets.precompile).not_to include("govuk_publishing_components_sassc-rails_manifest.js")
   end
 end
