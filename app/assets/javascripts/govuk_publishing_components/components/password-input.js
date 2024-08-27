abece9f7c304e21b4550094e22eda63d0e6b151a
AshGDS <8880610+AshGDS@users.noreply.github.com>
2024-08-27 14:55:57 +0100

    Add password input component

---
+++ app/assets/javascripts/govuk_publishing_components/components/password-input.js
@@ -0,0 +1,5 @@
+// This component relies on JavaScript from GOV.UK Frontend
+// = require govuk/components/password-input/password-input.bundle.js
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {}
+window.GOVUK.Modules.GovukPasswordInput = window.GOVUKFrontend.PasswordInput
