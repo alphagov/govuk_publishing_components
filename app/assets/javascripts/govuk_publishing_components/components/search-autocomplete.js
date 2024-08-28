c84793d60d646e2265ecd5ac930ff867ac63fcf5
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-08-28 10:10:33 +0100

    temp remote

---
+++ app/assets/javascripts/govuk_publishing_components/components/search-autocomplete.js
@@ -0,0 +1,318 @@
+/* global fetch */
+
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {}
+window.GOVUK.Modules.GovukAutocomplete = window.GOVUKFrontend.Autocomplete;
+
+(function (Modules) {
+  'use strict'
+
+  class GemSearchAutocomplete {
+    constructor ($module) {
+      this.$module = $module
+
+      this.numberSuggestions = $module.getAttribute('data-display-number-suggestions') || 5
+      this.throttleDelayTime = $module.getAttribute('data-throttle-delay-time') || 0
+      this.submitOnSelect = $module.getAttribute('data-submit-on-select') || false
+      this.baseClass = $module.getAttribute('data-base-class') || 'gem-c-search-autocomplete'
+      this.source = $module.getAttribute('data-source')
+    }
+
+    init () {
+      this.onUpdate = this.handleUpdate
+      this.onSubmit = () => { }
+
+      this.expanded = false
+      this.loading = false
+
+      this.$module.insertAdjacentHTML('beforeend', '<ul class="gem-c-search-autocomplete__result-list govuk-body" role="listbox"></ul><div aria-atomic="true" aria-live="polite" role="status" class="govuk-visually-hidden">No results.</div>')
+
+      this.$resultList = this.$module.querySelector('ul')
+      this.$resultList.setAttribute('role', 'listbox')
+      this.$resultList.id = `${this.baseClass}-result-list-${this.$module.getAttribute('data-id-postfix')}`
+
+      this.$resultList.addEventListener('mousedown', this.handleResultMouseDown)
+      this.$resultList.addEventListener('click', this.handleResultClick.bind(this))
+
+      this.$input = this.$module.querySelector('input')
+      this.$input.setAttribute('role', 'combobox')
+      this.$input.setAttribute('autocomplete', 'off')
+      this.$input.setAttribute('aria-autocomplete', 'list')
+      this.$input.setAttribute('aria-haspopup', 'listbox')
+      this.$input.setAttribute('aria-expanded', 'false')
+      this.$input.setAttribute('aria-owns', this.$resultList.id)
+      this.$input.setAttribute('autocapitalize', 'off')
+      this.$input.setAttribute('spellcheck', 'false')
+
+      this.$input.addEventListener('input', this.handleInput.bind(this))
+      this.$input.addEventListener('keydown', this.handleKeyDown.bind(this))
+      this.$input.addEventListener('keyup', this.handleKeyUp.bind(this))
+      this.$input.addEventListener('focus', this.handleFocus.bind(this))
+      this.$input.addEventListener('blur', this.handleBlur.bind(this))
+
+      document.body.addEventListener('click', this.handleDocumentClick.bind(this))
+
+      this.createAssistiveHint()
+
+      this.$liveRegion = this.$module.querySelector('[role="status"]')
+
+      this.value = ''
+      this.searchCounter = 0
+      this.results = []
+      this.selectedIndex = -1
+      this.selectedResult = null
+      this.throttled = false
+
+      this.normaliseSource()
+    }
+
+    normaliseSource () {
+      if (Array.isArray(this.source.split(',').map(item => item.trim()))) {
+        this.search = () => {
+          return Promise.resolve(JSON.parse(this.source))
+        }
+      } else {
+        this.search = async (value) => {
+          const url = new URL(this.source)
+          url.searchParams.append('q', value)
+          const response = await fetch(url)
+          const results = await response.json()
+          return results
+        }
+      }
+    }
+
+    handleDocumentClick (event) {
+      if (this.$module.contains(event.target)) {
+        return
+      }
+      this.hideResults()
+    }
+
+    handleBlur () {
+      this.hideResults()
+    }
+
+    handleResultMouseDown (event) {
+      event.preventDefault()
+    }
+
+    handleInput (event) {
+      if (this.throttled) {
+        return
+      }
+
+      const value = event.target.value
+      this.updateResults(value)
+      this.value = value
+
+      if (this.throttleDelayTime) {
+        this.throttled = true
+        this.keypressTimeout = setTimeout(() => {
+          this.throttled = false
+        }, parseInt(this.throttleDelayTime))
+      }
+    }
+
+    handleKeyDown (event) {
+      const keyCodes = {
+        9: 'tab',
+        13: 'enter',
+        27: 'escape',
+        32: 'space',
+        38: 'up',
+        40: 'down'
+      }
+
+      switch (keyCodes[event.keyCode]) {
+        case 'up':
+        case 'down': {
+          const selectedIndex =
+            event.keyCode === 38 ? this.selectedIndex - 1 : this.selectedIndex + 1
+          event.preventDefault()
+          this.handleArrows(selectedIndex)
+          break
+        }
+        case 'tab': {
+          this.selectResult()
+          break
+        }
+        case 'enter':
+        case 'space': {
+          if (event.keyCode === 32) {
+            event.preventDefault()
+          }
+          this.selectedResult =
+            this.results[this.selectedIndex] || this.selectedResult
+          this.selectResult()
+
+          if (this.submitOnSelect && event.target.closest('form')) {
+            event.target.closest('form').submit()
+          }
+          break
+        }
+        case 'escape': {
+          this.hideResults()
+          this.setInputValue()
+          break
+        }
+      }
+    }
+
+    handleKeyUp (event) {
+      const activeEl = event.target.getAttribute('aria-activedescendant')
+      if (activeEl) {
+        event.target.value = document.getElementById(activeEl).innerText
+      }
+    }
+
+    handleResultClick (event) {
+      const target = event.target
+      const result = target.closest('[data-result-index]')
+      if (result) {
+        this.selectedIndex = parseInt(result.dataset.resultIndex, 10)
+        const selectedResult = this.results[this.selectedIndex]
+        this.selectResult()
+        this.onSubmit(selectedResult)
+      }
+
+      if (this.submitOnSelect && event.target.closest('form')) {
+        event.target.closest('form').submit()
+      }
+    }
+
+    handleFocus (event) {
+      const value = event.target.value
+      this.updateResults(value)
+      this.value = value
+    }
+
+    handleArrows (selectedIndex) {
+      const resultsCount = this.results.length
+      this.selectedIndex =
+        ((selectedIndex % resultsCount) + resultsCount) % resultsCount
+
+      this.handleUpdate(this.results, this.selectedIndex)
+    }
+
+    selectResult () {
+      const selectedResult = this.results[this.selectedIndex]
+      if (selectedResult) {
+        this.setInputValue(selectedResult)
+      }
+      this.hideResults()
+    }
+
+    updateResults (value) {
+      if (value && value.length) {
+        const currentSearch = ++this.searchCounter
+        this.search(value).then((results) => {
+          if (currentSearch !== this.searchCounter) {
+            return
+          }
+          this.results = results
+
+          if (typeof this.results === 'undefined' || this.results.length === 0) {
+            this.hideResults()
+            return
+          }
+
+          this.selectedIndex = this.autoSelect ? 0 : -1
+          this.handleUpdate(this.results, this.selectedIndex)
+          this.showResults()
+        })
+      }
+    }
+
+    showResults () {
+      this.setInputAttribute('aria-expanded', true)
+      this.handleShow()
+    }
+
+    hideResults () {
+      this.selectedIndex = -1
+      this.results = []
+      this.setInputAttribute('aria-expanded', false)
+      this.setInputAttribute('aria-activedescendant', '')
+      this.handleUpdate(this.results, this.selectedIndex)
+      this.handleHide()
+    }
+
+    updateStyle () {
+      this.$module.dataset.expanded = this.expanded
+      this.$module.dataset.loading = this.loading
+
+      this.$resultList.style.visibility = this.expanded ? 'visible' : 'hidden'
+      this.$resultList.style.pointerEvents = this.expanded ? 'auto' : 'none'
+    }
+
+    createAssistiveHint () {
+      const hintId = `${this.baseClass}-assistive-hint-${this.$module.getAttribute('data-id-postfix')}`
+      this.$module.insertAdjacentHTML('beforeend', `<span id="${hintId}" class="govuk-visually-hidden">When autocomplete results are available use up and down arrows to review and enter to select. Touch device users, explore by touch or with swipe gestures.</span>`)
+      this.$input.setAttribute('aria-describedby', `${hintId}`)
+    }
+
+    setInputAttribute (attribute, value) {
+      this.$input.setAttribute(attribute, value)
+    }
+
+    setInputValue (result) {
+      this.$input.value = result
+    }
+
+    createResultElement (index, selectedIndex) {
+      const resultElement = document.createElement('li')
+      resultElement.id = `${this.baseClass}-result-${index}`
+      resultElement.classList.add(`${this.baseClass}__result`)
+      resultElement.setAttribute('data-result-index', index)
+      resultElement.setAttribute('role', 'option')
+      if (index === selectedIndex) {
+        resultElement.setAttribute('aria-selected', 'true')
+      }
+
+      return resultElement
+    }
+
+    createResultHtml (result, inputVal) {
+      const searchIcon = '<span class="gem-c-search-autocomplete__result--search-icon"><svg width="20" height="20" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><circle cx="12.0161" cy="11.0161" r="8.51613" stroke="currentColor" stroke-width="3" /><line x1="17.8668" y1="17.3587" x2="26.4475" y2="25.9393" stroke="currentColor" stroke-width="3" /></svg></span>'
+      const index = result.toLowerCase().indexOf(inputVal.toLowerCase())
+      return `${searchIcon}<span class='govuk-!-font-weight-bold'>${result.substring(0, index)}</span>${result.substring(index, index + inputVal.length)}<span class='govuk-!-font-weight-bold'>${result.substring(index + inputVal.length, result.length)}<span><div class='gem-c-search-autocomplete__result--border'></div>`
+    }
+
+    handleShow () {
+      this.expanded = true
+      this.updateStyle()
+    }
+
+    handleHide () {
+      this.expanded = false
+      this.updateStyle()
+    }
+
+    handleUpdate (results, selectedIndex) {
+      if (results.length > this.numberSuggestions) {
+        results = results.slice(0, this.numberSuggestions)
+      }
+
+      this.$resultList.innerHTML = ''
+      results.forEach((result, index) => {
+        const resultListElement = this.createResultElement(index, selectedIndex)
+        resultListElement.insertAdjacentHTML('beforeend', this.createResultHtml(result, this.$input.value))
+        this.$resultList.insertAdjacentElement('beforeend', resultListElement)
+      })
+
+      this.$input.setAttribute(
+        'aria-activedescendant',
+        selectedIndex > -1 ? `${this.baseClass}-result-${selectedIndex}` : ''
+      )
+
+      this.updateStatus(results.length)
+    }
+
+    updateStatus (resultCount) {
+      this.$liveRegion.innerHTML = resultCount === 0 ? 'No results.' : `${resultCount} results available.`
+    }
+  }
+
+  Modules.GemSearchAutocomplete = GemSearchAutocomplete
+})(window.GOVUK.Modules)
---
+++ spec/javascripts/components/search-autocomplete-spec.js
@@ -0,0 +1,167 @@
+/* eslint-env jasmine */
+/* global GOVUK, KeyboardEvent */
+
+describe('Search autocomplete component', function () {
+  var $autocomplete
+  var $container
+  var $input
+  var $resultList
+  var $status
+
+  var idPostfix
+  var data = [
+    'prime minister',
+    'deputy prime minister',
+    'contact prime minister',
+    'email prime minister',
+    'last prime minister'
+  ]
+
+  var html =
+  `<div class="gem-c-search-autocomplete" data-throttle-delay-time="500" data-id-postfix="test" data-source="[&quot;prime minister&quot;,&quot;deputy prime minister&quot;,&quot;contact prime minister&quot;,&quot;email prime minister&quot;,&quot;last prime minister&quot;]">
+      <label for="input-1" class="gem-c-label govuk-label">Country</label>
+      <input class="gem-c-input govuk-input" name="country" type="text">
+  </div>`
+
+  function startAutocomplete () {
+    new GOVUK.Modules.GemSearchAutocomplete($autocomplete).init()
+  }
+
+  beforeEach(function () {
+    $container = document.createElement('div')
+    $container.innerHTML = html
+    document.body.appendChild($container)
+    $autocomplete = document.querySelector('.gem-c-search-autocomplete')
+
+    startAutocomplete()
+
+    jasmine.clock().install()
+
+    idPostfix = $autocomplete.getAttribute('data-id-postfix')
+    $input = $autocomplete.querySelector('input')
+    $resultList = $autocomplete.querySelector('.gem-c-search-autocomplete__result-list')
+  })
+
+  afterEach(function () {
+    jasmine.clock().uninstall()
+    document.body.removeChild($container)
+  })
+
+  describe('initial component state', function () {
+    it('is an editable field that is focusable', async function () {
+      await $input.focus()
+
+      expect($input.getAttribute('role')).toBe('combobox')
+      expect(document.activeElement).toEqual($input)
+      expect($input.getAttribute('aria-autocomplete')).toBe('list')
+      expect($input.getAttribute('autocomplete')).toBe('off')
+      expect($input.getAttribute('aria-haspopup')).toBe('listbox')
+      expect($input.getAttribute('aria-expanded')).toBe('false')
+    })
+
+    it('correct input attributes are set', async function () {
+      await $input.focus()
+
+      expect($input.getAttribute('autocapitalize')).toBe('off')
+      expect($input.getAttribute('spellcheck')).toBe('false')
+    })
+
+    it('user is informed of status and instructions', async function () {
+      await $input.focus()
+      $status = document.querySelector('[role="status"]')
+
+      expect($input.getAttribute('aria-expanded')).toBe('false')
+      expect($status.innerHTML).toBe('No results.')
+
+      expect($input.getAttribute('aria-describedby')).toBe(`gem-c-search-autocomplete-assistive-hint-${idPostfix}`)
+      expect(document.querySelectorAll(`#gem-c-search-autocomplete-assistive-hint-${idPostfix}`).length).toBe(1)
+    })
+
+    it('throttle delay time', async function () {
+      spyOn(window.GOVUK.Modules.GemSearchAutocomplete.prototype, 'updateResults').and.callThrough()
+
+      $input.value = 'r'
+      await $input.focus()
+      await $input.blur()
+      await $input.focus()
+
+      expect(window.GOVUK.Modules.GemSearchAutocomplete.prototype.updateResults.calls.count()).toBe(2)
+    })
+
+    it('form behaviour', function () {
+      // to be implemented
+    })
+
+    describe('user interaction', function () {
+      beforeEach(async function () {
+        $input.value = 'p'
+        $status = document.querySelector('[role="status"]')
+        await $input.focus()
+      })
+
+      describe('general behaviour', function () {
+        it('inform the user that content has been expanded', function () {
+          expect($autocomplete.getAttribute('data-expanded')).toBe('true')
+          expect($input.getAttribute('aria-expanded')).toBe('true')
+          expect($resultList.getAttribute('role')).toBe('listbox')
+          expect($status.getAttribute('aria-atomic')).toBe('true')
+          expect($status.getAttribute('aria-live')).toBe('polite')
+        })
+
+        it('inform the user when there are matches', function () {
+          expect($resultList.querySelectorAll('li').length).toBe(5)
+          expect($status.innerHTML).toBe('5 results available.')
+        })
+
+        it('allows user to select a suggestion and returns focus to input', function () {
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }))
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', keyCode: 13 }))
+
+          expect($input.value).toBe(data[0])
+          expect(document.activeElement).toEqual($input)
+        })
+      })
+
+      describe('keyboard behaviour', function () {
+        it('user can navigate the available matches using keyboard', function () {
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }))
+          expect($input.getAttribute('aria-activedescendant')).toBe('gem-c-search-autocomplete-result-0')
+
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp', keyCode: 38 }))
+          expect($input.getAttribute('aria-activedescendant')).toBe('gem-c-search-autocomplete-result-4')
+
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }))
+          expect($input.getAttribute('aria-activedescendant')).toBe('gem-c-search-autocomplete-result-0')
+        })
+
+        it('space key closes the menu, sets the query, focuses the input', () => {
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }))
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'Space', keyCode: 32 }))
+
+          expect($input.value).toBe(data[0])
+          expect(document.activeElement).toEqual($input)
+          expect($input.getAttribute('aria-expanded')).toBe('false')
+        })
+
+        it('esc key closes the menu, focuses the input', () => {
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }))
+          $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'Esc', keyCode: 27 }))
+
+          expect(document.activeElement).toEqual($input)
+          expect($input.getAttribute('aria-expanded')).toBe('false')
+        })
+      })
+
+      describe('mouse behaviour', function () {
+        it('user can select suggestion with mouse', async function () {
+          var $option1 = document.querySelector('.gem-c-search-autocomplete ul li:nth-child(1)')
+          $option1.click()
+
+          expect($input.value).toBe(data[0])
+          expect(document.activeElement).toEqual($input)
+          expect($input.getAttribute('aria-expanded')).toBe('false')
+        })
+      })
+    })
+  })
+})
