6d7831c486c68c18095349ccfd79a1853bf4a392
Christian Sutter <git@doublesignal.com>
2024-09-05 14:51:27 +0000

    Initialise AAC on init

--- app/assets/javascripts/govuk_publishing_components/components/search-with-autocomplete.js
+++ app/assets/javascripts/govuk_publishing_components/components/search-with-autocomplete.js
@@ -1,14 +1,31 @@
+/* global accessibleAutocomplete, fetch */
+//= require accessible-autocomplete/dist/accessible-autocomplete.min.js
 window.GOVUK = window.GOVUK || {};
 window.GOVUK.Modules = window.GOVUK.Modules || {};
 
 (function (Modules) {
   class SearchWithAutocomplete {
     constructor ($module) {
-      console.log('SearchWithAutocomplete:constructor')
+      this.$module = $module
+      this.$input = this.$module.querySelector('input')
+      this.$source = this.$module.getAttribute('data-source')
+      this.$sourceKey = this.$module.getAttribute('data-source-key')
     }
 
     init () {
-      console.log('SearchWithAutocomplete:init')
+      if (!this.$source || !this.$sourceKey) {
+        console.warn('search-with-autocomplete: No source or source-key provided; skipping init')
+        return
+      }
+
+      const configOptions = {
+        element: this.$module,
+        id: this.$input.id,
+        defaultValue: this.$input.value,
+        source: ['one', 'two', 'three'],
+      }
+
+      accessibleAutocomplete(configOptions)
     }
   }
 
