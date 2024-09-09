abe101a6e9e08057b46806c0a220830669f2ca18
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-09-09 12:33:37 +0100

    tidy up sass styles

--- app/assets/javascripts/govuk_publishing_components/components/search-with-autocomplete.js
+++ app/assets/javascripts/govuk_publishing_components/components/search-with-autocomplete.js
@@ -49,10 +49,10 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       const regex = new RegExp(`(${inputValue})`, 'gi')
       const matchHTML = result.replace(regex, '<span class="govuk-!-font-weight-regular js-result-match">$1</span>')
 
-      const html = `<span class="autocomplete__option--search-icon">
+      const html = `<span class="autocomplete__option-wrapper"><span class="autocomplete__option--search-icon">
       </span>
-      <span class="govuk-!-font-weight-bold">${matchHTML}</span>
-      <div class="autocomplete__option--border"></div>`
+      <span class="govuk-!-font-weight-bold autocomplete__suggestion-text">${matchHTML}</span>
+      <div class="autocomplete__option--border"></div></span>`
 
       return html
     }
