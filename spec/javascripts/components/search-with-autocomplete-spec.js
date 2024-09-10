09b0a3069b77c2f46ad9adb2b15bf5fe69416391
Christian Sutter <git@doublesignal.com>
2024-09-10 18:29:05 +0000

    Start tidying up tests before re-adding

--- spec/javascripts/components/search-with-autocomplete-spec.js
+++ spec/javascripts/components/search-with-autocomplete-spec.js
@@ -11,24 +11,42 @@ describe('Search with autocomplete component', () => {
     autocomplete = new GOVUK.Modules.SearchWithAutocomplete(fixture.querySelector('.gem-c-search-with-autocomplete'))
   }
 
-  const html =
-    '<div class="gem-c-search-with-autocomplete">' +
-      '<label for="autocomplete-id" class="govuk-label">Countries</label>' +
-      '<div class="gem-c-search__input-wrapper">' +
-        '<input name="autocomplete-name" id="autocomplete-id" class="autocomplete__input autocomplete__input--default gem-c-search__item gem-c-search__input js-class-toggle" list="autocomplete-list" type="text" value="test value">' +
-      '</div>' +
-    '</div>'
+  const html = `
+    <div
+      class="gem-c-search-with-autocomplete"
+      data-module="search-with-autocomplete"
+      data-source="https://www.example.org/api/autocomplete.json?foo=bar"
+      data-source-key="suggestions"
+    >
+      <div
+        class="gem-c-search gem-c-search--separate-label"
+        data-module="gem-toggle-input-class-on-focus"
+      >
+        <label for="search-main-7fdd251c" class="gem-c-search__label">Search on GOV.UK</label>
+        <div class="gem-c-search__item-wrapper">
+          <div class="gem-c-search__input-wrapper">
+            <input
+              enterkeyhint="search"
+              class="gem-c-search__item gem-c-search__input js-class-toggle" id="search-main-7b87262d"
+              name="q"
+              title="Search"
+              type="search"
+              value="i've been looking for freedom"
+            >
+          </div>
+
+          <div class="gem-c-search__item gem-c-search__submit-wrapper">
+            <button class="gem-c-search__submit" type="submit" enterkeyhint="search">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
 
   afterEach(() => {
     fixture.remove()
-  })
-
-  it('fails gracefully if there is no datalist', () => {
-    loadAutocompleteComponent(html)
-    spyOn(autocomplete, 'getResults').and.callThrough()
-    autocomplete.init()
-
-    expect(autocomplete.getResults).not.toHaveBeenCalled()
   })
 
   it('deletes the original input', () => {
@@ -44,9 +62,9 @@ describe('Search with autocomplete component', () => {
     autocomplete.init()
 
     const input = fixture.querySelector('input')
-    expect(input.getAttribute('name')).toEqual('autocomplete-name')
-    expect(input.getAttribute('id')).toEqual('autocomplete-id')
-    expect(input.getAttribute('type')).toEqual('text')
+    expect(input.getAttribute('name')).toEqual('q')
+    expect(input.getAttribute('id')).toEqual('search-main-7b87262d')
+    expect(input.getAttribute('type')).toEqual('search')
   })
 
   it('recreates the input with the same value', () => {
@@ -54,37 +72,6 @@ describe('Search with autocomplete component', () => {
     autocomplete.init()
 
     const input = fixture.querySelector('input')
-    expect(input.value).toEqual('test value')
-  })
-
-  describe('Results list', () => {
-    it('sets the suggestion text with match highlighted', () => {
-      loadAutocompleteComponent(html)
-      autocomplete.init()
-
-      fixture.querySelector('input').value = 'prime m'
-
-      const container = document.createElement('div')
-      container.insertAdjacentHTML('beforeend', autocomplete.constructSuggestionHTMLString('who is the prime minister'))
-
-      expect(container.querySelector('.js-result-match').innerText).toBe('prime m')
-    })
-
-    it('form is submitted when user selects a suggestion', async () => {
-      loadAutocompleteComponent(html)
-      autocomplete.init(['france', 'germany'])
-
-      spyOn(autocomplete, 'submitForm')
-      const $input = fixture.querySelector('input')
-      $input.value = 'f'
-      await $input.dispatchEvent(new Event('input'))
-
-      $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: 40 }))
-      $input.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', keyCode: 13 }))
-
-      document.querySelector('.autocomplete__menu li').click()
-
-      expect(autocomplete.submitForm).toHaveBeenCalled()
-    })
+    expect(input.value).toEqual("i've been looking for freedom")
   })
 })
