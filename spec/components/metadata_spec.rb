37adbb0fa0903769e15be56736e2d068f7763248
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2024-11-11 10:33:43 +0000

    Add heading option to metadata component

--- spec/components/metadata_spec.rb
+++ spec/components/metadata_spec.rb
@@ -237,6 +237,12 @@ describe "Metadata", type: :view do
     assert_select ".gem-c-metadata__definition:nth-of-type(2)[data-ga4-track-links-only]", false
   end
 
+  it "renders the component with a title" do
+    render_component(from: "<a href='/link'>Department</a>", title: "my title")
+
+    assert_select ".gem-c-metadata__title", text: "my title"
+  end
+
   def assert_truncation(length, limit)
     assert_select ".gem-c-metadata__toggle-items", count: 1
     assert_select ".gem-c-metadata__definition > a", count: limit
