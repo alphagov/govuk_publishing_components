4a654afdbdb663c43e2c2601e15169ea25cd6349
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2024-11-14 14:58:14 +0000

    No longer rendering add button

--- spec/components/add_another_spec.rb
+++ spec/components/add_another_spec.rb
@@ -29,21 +29,4 @@ describe "Add another", type: :view do
 
     assert_select "div.empty"
   end
-
-  it "renders the add button with the default text when the add button text option is not set" do
-    items = [sanitize("<div class=\"item1\">item1</div>"), sanitize("<div class=\"item2\">item2</div>")]
-    empty = ""
-    render_component(items:, empty:)
-
-    assert_select "button", text: "Add another"
-  end
-
-  it "renders the add button with the provided text when the add button text option is set" do
-    add_button_text = "Add one more"
-    items = [sanitize("<div class=\"item1\">item1</div>"), sanitize("<div class=\"item2\">item2</div>")]
-    empty = ""
-    render_component(items:, empty:, add_button_text:)
-
-    assert_select "button", text: add_button_text
-  end
 end
