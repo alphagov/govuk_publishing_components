886c53f7524f1baaf24c01415a7b56009c2c9f97
Kevin Dew <kevindew@me.com>
2024-12-09 14:36:17 +0000

    Disable Search Autocomplete based off env var presence
    Autocomplete uses a denylist to stop certain terms from serving
    suggestions. The process to update the denylist is very manual and
    developer heavy.
    
    It might be difficult to update the denylist over the holiday period. In
    that situation the decision might be taken to temporarily turn off
    autocomplete entirely.
    
    An environment variable is used to determine whether search with autocomplete
    has been disabled. The value of this variable will be applied
    globally across all applications that use site search.
    
    See: https://github.com/alphagov/govuk-helm-charts/pull/2832
    
    Co-authored-by: Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>

--- spec/components/layout_super_navigation_header_spec.rb
+++ spec/components/layout_super_navigation_header_spec.rb
@@ -163,11 +163,6 @@ describe "Super navigation header", type: :view do
     assert_select ".js-module-initialised[data-module=\"super-navigation-mega-menu\"]", false
   end
 
-  it "includes the search_with_autocomplete component with correct URL" do
-    render_component({})
-    assert_select ".gem-c-search-with-autocomplete[data-source-url='http://www.dev.gov.uk/api/search/autocomplete.json']"
-  end
-
   it "adds GA4 tracking" do
     render_component({})
 
@@ -179,5 +174,20 @@ describe "Super navigation header", type: :view do
     assert_select 'a[data-ga4-link=\'{"event_name":"navigation","type":"header menu bar","index_section":2,"index_link":1,"index_section_count":3,"index_total":6,"section":"Government activity"}\']'
     assert_select 'a[data-ga4-link=\'{"event_name":"navigation","type":"header menu bar","index_section":2,"index_link":6,"index_section_count":3,"index_total":6,"section":"Government activity"}\']'
     assert_select "form[data-module='ga4-search-tracker']"
+  end
+
+  describe "search autocomplete" do
+    it "includes the search_with_autocomplete component by default" do
+      render_component({})
+      assert_select ".gem-c-search-with-autocomplete[data-source-url='http://www.dev.gov.uk/api/search/autocomplete.json']"
+    end
+
+    it "allows the GOVUK_DISABLE_SEARCH_AUTOCOMPLETE env var presence to fallback to search without autocomplete" do
+      ClimateControl.modify GOVUK_DISABLE_SEARCH_AUTOCOMPLETE: "1" do
+        render_component({})
+        assert_select ".gem-c-search-autocomplete", false
+        assert_select ".gem-c-search"
+      end
+    end
   end
 end
