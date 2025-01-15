9fa2c222020df7f4832b29ed98ae983063d14f5b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-01-15 16:05:20 +0000

    Clean up component options
    - remove option to pass a link for the non-title text as not currently needed
    - rename options to just title and text, simpler
    - remove show banner option, use existence of title as basis for displaying banner
    - make banner appear in component guide
    - write some rspec tests

--- spec/components/global_bar_spec.rb
+++ spec/components/global_bar_spec.rb
@@ -4,4 +4,24 @@ describe "Global bar", type: :view do
   def component_name
     "global_bar"
   end
+
+  it "renders nothing without a title" do
+    assert_empty render_component({})
+  end
+
+  it "renders with a title" do
+    render_component(title: "Look here you")
+    assert_select ".gem-c-global-bar", text: "Look here you"
+  end
+
+  it "renders with a title and a link" do
+    render_component(title: "Look here you", title_href: "https://www.gov.uk")
+    assert_select ".gem-c-global-bar a.gem-c-global-bar-title", text: "Look here you"
+  end
+
+  it "renders with a title and text" do
+    render_component(title: "Look here you", text: "This is important")
+    assert_select ".gem-c-global-bar .gem-c-global-bar-title", text: "Look here you"
+    assert_select ".gem-c-global-bar .gem-c-global-bar-text", text: "This is important"
+  end
 end
