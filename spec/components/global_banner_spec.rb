0f322f6bb061be5cd3e4e072db6166a17721495a
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-01-16 09:16:17 +0000

    Clean up stylesheets
    - use correct BEM syntax
    - remove unused styles (checked and they don't seem to exist anywhere else in alphagov)

--- spec/components/global_banner_spec.rb
+++ spec/components/global_banner_spec.rb
@@ -16,12 +16,12 @@ describe "Global banner", type: :view do
 
   it "renders with a title and a link" do
     render_component(title: "Look here you", title_href: "https://www.gov.uk")
-    assert_select ".gem-c-global-banner a.gem-c-global-banner-title", text: "Look here you"
+    assert_select ".gem-c-global-banner a.gem-c-global-banner__title", text: "Look here you"
   end
 
   it "renders with a title and text" do
     render_component(title: "Look here you", text: "This is important")
-    assert_select ".gem-c-global-banner .gem-c-global-banner-title", text: "Look here you"
-    assert_select ".gem-c-global-banner .gem-c-global-banner-text", text: "This is important"
+    assert_select ".gem-c-global-banner .gem-c-global-banner__title", text: "Look here you"
+    assert_select ".gem-c-global-banner .gem-c-global-banner__text", text: "This is important"
   end
 end
