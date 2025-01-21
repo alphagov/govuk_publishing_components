dfd33396a1f3b781016986296c3a504c105f1c92
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-01-21 15:48:11 +0000

    Update character count with textarea changes
    - also improve tests, remove redundant data attributes test (component doesn't accept data attributes)

--- spec/components/character_count_spec.rb
+++ spec/components/character_count_spec.rb
@@ -9,38 +9,46 @@ describe "Character count", type: :view do
     assert_empty render_component({})
   end
 
-  it "renders character count with textarea" do
+  it "renders character count with minimal data passed" do
     render_component(
-      name: "character-count",
+      textarea: {
+        name: "more-details",
+      },
+      maxlength: "100",
+    )
+    assert_select ".govuk-character-count .govuk-textarea[name='more-details']"
+  end
+
+  it "renders character count with more options" do
+    render_component(
       textarea: {
         label: { text: "Can you provide more detail?" },
         name: "more-details",
       },
-      data: {
-        module: "character-count",
-      },
       maxlength: "100",
+      maxwords: "3",
+      threshold: "11",
     )
-
-    assert_select ".govuk-character-count .govuk-textarea"
-
+    assert_select ".gem-c-character-count[data-maxlength='100']"
+    assert_select ".gem-c-character-count[data-maxwords='3']"
+    assert_select ".gem-c-character-count[data-threshold='11']"
+    assert_select ".govuk-character-count .govuk-textarea[name='more-details']"
     assert_select ".govuk-character-count .govuk-label", text: "Can you provide more detail?"
   end
 
-  it "renders character count with data attributes" do
+  it "renders character count with the correct id for the textarea" do
     render_component(
-      name: "character-count",
       textarea: {
         label: { text: "Can you provide more detail?" },
         name: "more-details",
-      },
-      data: {
-        module: "character-count",
+        textarea_id: "textarea-element-id",
+        id: "textarea-component-id",
       },
       maxlength: "100",
+      id: "character-count-id",
     )
-
-    assert_select ".govuk-character-count[data-module='govuk-character-count']"
-    assert_select ".govuk-character-count[data-maxlength='100']"
+    assert_select "#textarea-component-id.gem-c-textarea"
+    assert_select "textarea#character-count-id"
+    assert_select "#textarea-element-id", false
   end
 end
