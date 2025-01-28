ab625b848f91dca64ae4699983dd5a8441c28da3
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-01-28 13:15:46 +0000

    Remove margin_top option from shared helper
    - should no longer be in use by any components
    - moves us towards a world where components only have bottom margin
    - also remove an example from the title component guide page that uses margin_top and is out of date

--- lib/govuk_publishing_components/presenters/shared_helper.rb
+++ lib/govuk_publishing_components/presenters/shared_helper.rb
@@ -1,11 +1,10 @@
 module GovukPublishingComponents
   module Presenters
     class SharedHelper
-      attr_reader :options, :margin_top, :margin_bottom, :heading_level, :classes
+      attr_reader :options, :margin_bottom, :heading_level, :classes
 
       def initialize(local_assigns)
         @options = local_assigns
-        @margin_top = @options[:margin_top] || nil
         @margin_bottom = @options[:margin_bottom] || 3
         @heading_level = @options[:heading_level] || 2
 
@@ -15,10 +14,6 @@ module GovukPublishingComponents
             raise(ArgumentError, "Passed classes must be prefixed with `js-`")
           end
         end
-      end
-
-      def get_margin_top
-        [*0..9].include?(@margin_top) ? "govuk-!-margin-top-#{margin_top}" : ""
       end
 
       def get_margin_bottom
