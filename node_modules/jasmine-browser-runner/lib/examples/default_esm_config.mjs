555f47b3cc383267643bc26f9bb95fc5ea06f79b
Mateusz Grotek <mateusz.grotek@digital.cabinet-office.gov.uk>
2025-01-29 16:16:42 +0000

    DO NOT MERGE

---
+++ node_modules/jasmine-browser-runner/lib/examples/default_esm_config.mjs
@@ -0,0 +1,34 @@
+export default {
+  srcDir: "src",
+  // srcFiles should usually be left empty when using ES modules, because you'll
+  // explicitly import sources from your specs.
+  srcFiles: [],
+  specDir: ".",
+  specFiles: [
+    "spec/**/*[sS]pec.?(m)js"
+  ],
+  helpers: [
+    "spec/helpers/**/*.?(m)js"
+  ],
+  esmFilenameExtension: ".mjs",
+  // Allows the use of top-level await in src/spec/helper files. This is off by
+  // default because it makes files load more slowly.
+  enableTopLevelAwait: false,
+  env: {
+    stopSpecOnExpectationFailure: false,
+    stopOnSpecFailure: false,
+    random: true
+  },
+
+  // For security, listen only to localhost. You can also specify a different
+  // hostname or IP address, or remove the property or set it to "*" to listen
+  // to all network interfaces.
+  listenAddress: "localhost",
+
+  // The hostname that the browser will use to connect to the server.
+  hostname: "localhost",
+
+  browser: {
+    name: "firefox"
+  }
+};
