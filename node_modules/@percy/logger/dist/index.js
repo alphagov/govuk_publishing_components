cbd0f88c3191f4b310d71607bedb5ee81be463f9
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-02-28 12:44:04 +0000

    Set up feature branch - temporary commit to delete later
    Include node modules so frontend applications can be run against
    this branch
    
    Add temporary FEATURE BRANCH CHANGELOG. So we reduce merge conflicts,
    as the CHANGELOG is frequently updated.
    
    This commit should be dropped when the branch is merged into main.

---
+++ node_modules/@percy/logger/dist/index.js
@@ -0,0 +1,35 @@
+import Logger from './logger.js';
+import TimeIt from './timing.js';
+export function logger(name) {
+  return new Logger().group(name);
+}
+Object.defineProperties(logger, {
+  stdout: {
+    get: () => Logger.stdout
+  },
+  stderr: {
+    get: () => Logger.stderr
+  },
+  constructor: {
+    get: () => Logger
+  },
+  instance: {
+    get: () => new Logger()
+  },
+  query: {
+    value: (...args) => logger.instance.query(...args)
+  },
+  format: {
+    value: (...args) => logger.instance.format(...args)
+  },
+  loglevel: {
+    value: (...args) => logger.instance.loglevel(...args)
+  },
+  timeit: {
+    get: () => new TimeIt(logger.instance.group('timer'))
+  },
+  measure: {
+    value: (...args) => logger.timeit.measure(...args)
+  }
+});
+export default logger;
