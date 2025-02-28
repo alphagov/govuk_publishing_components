be8c271edb0aa87dfac9ec9fad37ab51b80267d3
Martin Jones <28779939+MartinJJones@users.noreply.github.com>
2025-02-28 12:35:14 +0000

    Fix failing magnaCharta test in Safari
    - Updated the `cW` function to avoid truncating the calculated width, this matches the default behaviour in magnaCharta which does not restrict the number of decimal places
    - Use the [toBeCloseTo](https://jasmine.github.io/api/5.6/matchers#toBeCloseTo) matcher and set the number of decimal points to check to 4
    
    Fixes: https://github.com/alphagov/govuk_publishing_components/issues/2229

--- spec/javascripts/govuk_publishing_components/lib/govspeak/magna-charta-spec.js
+++ spec/javascripts/govuk_publishing_components/lib/govspeak/magna-charta-spec.js
@@ -14,14 +14,8 @@ describe('Magna charta', function () {
   // widths are 65/max * val (65 by default)
   var cW = function (max, val, padding) {
     padding = padding || 0
-    var result = ((65 / max) * val + padding).toString()
-    // truncate the result to only 4 digits after the decimal place
-    // e.g. 27.0833333333 becomes 27.0833, 27.1 and 27 remain the same
-    var split = result.split('.')
-    if (split.length > 1) {
-      result = split[0] + '.' + split[1].substring(0, 4)
-    }
-    return result + '%'
+    var width = ((65 / max) * val + padding)
+    return `${width}%`
   }
 
   var single =
@@ -282,12 +276,15 @@ describe('Magna charta', function () {
 
     it('the bar cells are given the right widths', function () {
       var cells = graph.find('.mc-bar-cell')
-      expect(cells.get(0).style.width).toEqual(cW(12, 5))
-      expect(cells.get(1).style.width).toEqual(cW(12, 6))
-      expect(cells.get(2).style.width).toEqual(cW(12, 6))
-      expect(cells.get(3).style.width).toEqual(cW(12, 2))
-      expect(cells.get(4).style.width).toEqual(cW(12, 3))
-      expect(cells.get(5).style.width).toEqual(cW(12, 9))
+
+      for (var cell of cells) {
+        var cellText = cell.textContent
+        var cellWidth = cell.style.width
+        var calculatedWidth = parseFloat(cW(12, cellText))
+
+        expect(cellWidth).toContain('%')
+        expect(parseFloat(cellWidth)).toBeCloseTo(calculatedWidth, 4)
+      }
     })
 
     it('the bar cells are given classes denoting their index', function () {
