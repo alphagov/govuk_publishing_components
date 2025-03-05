8dbf949f84d0e39e4d65822e2d9607707bf33899
AshGDS <8880610+AshGDS@users.noreply.github.com>
2025-03-05 09:52:08 +0000

    Rename 'back to top' to 'back to top link'
    For consistency reasons

--- spec/components/back_to_top_link_spec.rb
+++ spec/components/back_to_top_link_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Back To Top Link", type: :view do
   def component_name
-    "back_to_top"
+    "back_to_top_link"
   end
 
   it "fails to render a back to top link when no parameters given" do
@@ -26,18 +26,18 @@ describe "Back To Top Link", type: :view do
   it "renders a back to top link with default text" do
     render_component(href: "#contents")
 
-    assert_select ".gem-c-back-to-top[href='#contents']", text: "Contents"
+    assert_select ".gem-c-back-to-top-link[href='#contents']", text: "Contents"
   end
 
   it "renders a back to top link when a href is given" do
     render_component(href: "#contents")
 
-    assert_select ".gem-c-back-to-top[href='#contents']"
+    assert_select ".gem-c-back-to-top-link[href='#contents']"
   end
 
   it "renders a back to top link with custom text" do
     render_component(href: "#contents", text: "Back to top")
 
-    assert_select ".gem-c-back-to-top[href='#contents']", text: "Back to top"
+    assert_select ".gem-c-back-to-top-link[href='#contents']", text: "Back to top"
   end
 end
