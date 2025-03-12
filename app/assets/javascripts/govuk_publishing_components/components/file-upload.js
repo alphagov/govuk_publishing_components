299af72623018b90698a54362adb75aeb4979ca0
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-03-12 14:49:23 +0000

    Add file upload component javascript option

---
+++ app/assets/javascripts/govuk_publishing_components/components/file-upload.js
@@ -0,0 +1,5 @@
+// This component relies on JavaScript from GOV.UK Frontend
+// = require govuk/components/file-upload/file-upload.bundle.js
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {}
+window.GOVUK.Modules.GovukFileUpload = window.GOVUKFrontend.FileUpload
