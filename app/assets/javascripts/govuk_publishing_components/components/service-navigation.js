d65ae7876a166da27aff24bbee8884a245cffe8d
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-03-13 12:19:08 +0000

    TEMP - add service navigation component

---
+++ app/assets/javascripts/govuk_publishing_components/components/service-navigation.js
@@ -0,0 +1,5 @@
+// This component relies on JavaScript from GOV.UK Frontend
+// = require govuk/components/service-navigation/service-navigation.bundle.js
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {}
+window.GOVUK.Modules.GovukServiceNavigation = window.GOVUKFrontend.ServiceNavigation
