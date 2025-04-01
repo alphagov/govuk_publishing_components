0bd54b03e4f4d617ad5a3bda7fce880214971c6d
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-04-01 13:05:25 +0100

    Address PR comments

--- spec/components/service_navigation_spec.rb
+++ spec/components/service_navigation_spec.rb
@@ -29,7 +29,7 @@ describe "Service navigation", type: :view do
   it "renders with navigation items" do
     render_component({ navigation_items: })
 
-    assert_select(".gem-c-service-navigation")
+    assert_select("div.gem-c-service-navigation")
 
     assert_select(".govuk-service-navigation__item:nth-child(1) .govuk-service-navigation__link", text: "Navigation item 1")
     assert_select(".govuk-service-navigation__item:nth-child(2) .govuk-service-navigation__link", text: "Navigation item 2")
@@ -40,12 +40,24 @@ describe "Service navigation", type: :view do
     updated_navigation_items = navigation_items.map { |item| item[:text] == "Navigation item 2" ? item.merge(active: true) : item }
     render_component({ navigation_items: updated_navigation_items })
 
-    assert_select(".govuk-service-navigation__item:nth-child(2).govuk-service-navigation__item--active .govuk-service-navigation__link[aria-current]", text: "Navigation item 2")
+    assert_select(".govuk-service-navigation__item:nth-child(2).govuk-service-navigation__item--active
+      .govuk-service-navigation__link[aria-current]
+      strong.govuk-service-navigation__active-fallback", text: "Navigation item 2")
   end
 
   it "renders a service name when provided" do
     render_component({ navigation_items:, service_name: "My service name" })
 
-    assert_select(".govuk-service-navigation__service-name .govuk-service-navigation__link", text: "My service name")
+    assert_select("section.gem-c-service-navigation")
+
+    assert_select("span.govuk-service-navigation__service-name", text: "My service name")
+  end
+
+  it "renders a service name and service name url when provided" do
+    render_component({ navigation_items:, service_name: "My service name", service_name_url: "#" })
+
+    assert_select("section.gem-c-service-navigation")
+
+    assert_select("span.govuk-service-navigation__service-name .govuk-service-navigation__link", text: "My service name")
   end
 end
