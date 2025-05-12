12d90a0e248bb58bb73b6432ab7de8be0bb634c7
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-05-12 12:20:25 +0100

    Add option to increase spacing around the homepage logo
    Also update the homepage examples for 'super navigation header' and 'public layout' to reflect the purpose of the new option, which is to increase logo spacing to align it with other content on the page that also includes additional spacing.

--- spec/components/layout_super_navigation_header_spec.rb
+++ spec/components/layout_super_navigation_header_spec.rb
@@ -133,6 +133,14 @@ describe "Super navigation header", type: :view do
     assert_select ".gem-c-layout-super-navigation-header__navigation-item-link--blue-background"
   end
 
+  it "renders homepage variant" do
+    render_component({
+      homepage: true,
+    })
+
+    assert_select ".gem-c-layout-super-navigation-header__header-logo--homepage"
+  end
+
   it "renders large navbar variant" do
     render_component({
       large_navbar: true,
