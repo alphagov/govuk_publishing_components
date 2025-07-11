c868162153b240f685640a948703a313cbdf6a97
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-07-11 09:57:32 +0100

    Remove 'dark icon' options

--- spec/components/action_link_spec.rb
+++ spec/components/action_link_spec.rb
@@ -82,24 +82,6 @@ describe "Action link", type: :view do
     assert_select ".gem-c-action-link--simple-light"
   end
 
-  it "renders dark icon version" do
-    render_component(
-      text: "Get more info",
-      href: "/coronavirus",
-      dark_icon: true,
-    )
-    assert_select ".gem-c-action-link--dark-icon"
-  end
-
-  it "renders dark large icon version" do
-    render_component(
-      text: "Get more info",
-      href: "/coronavirus",
-      dark_large_icon: true,
-    )
-    assert_select ".gem-c-action-link--dark-large-icon"
-  end
-
   it "renders inverse version" do
     render_component(
       text: "Get more info",
