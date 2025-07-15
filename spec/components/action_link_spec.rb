f2e15ea1dec9c2315b32c2da5bba54a5cf50b9f1
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2025-07-15 16:00:09 +0100

    Remove other unused component options

--- spec/components/action_link_spec.rb
+++ spec/components/action_link_spec.rb
@@ -26,44 +26,6 @@ describe "Action link", type: :view do
     assert_select '.gem-c-action-link.govuk-\!-margin-bottom-6'
   end
 
-  it "renders non wrapping text if nowrap text is passed through" do
-    render_component(
-      text: "Get more info",
-      nowrap_text: "about COVID",
-      href: "/coronavirus",
-    )
-    assert_select ".gem-c-action-link .gem-c-action-link__nowrap-text", text: "about COVID"
-  end
-
-  it "renders subtext" do
-    render_component(
-      text: "Get more info",
-      href: "/coronavirus",
-      subtext: "because info is important",
-    )
-    assert_select ".gem-c-action-link.gem-c-action-link--with-subtext .gem-c-action-link__subtext", text: "because info is important"
-  end
-
-  it "shows subtext like on mobile" do
-    render_component(
-      text: "Get more info",
-      subtext: "because info is important",
-      mobile_subtext: true,
-    )
-    assert_select ".gem-c-action-link.gem-c-action-link--mobile-subtext"
-  end
-
-  it "can add a link to subtext" do
-    render_component(
-      text: "Get more info",
-      href: "/main-href",
-      subtext: "because info is important",
-      subtext_href: "/subtext-href",
-    )
-    assert_select ".gem-c-action-link .gem-c-action-link__link[href='/main-href']"
-    assert_select ".gem-c-action-link .gem-c-action-link__subtext .govuk-link[href='/subtext-href']"
-  end
-
   it "renders simple icon version" do
     render_component(
       text: "Get more info",
@@ -71,15 +33,6 @@ describe "Action link", type: :view do
       simple: true,
     )
     assert_select ".gem-c-action-link--simple"
-  end
-
-  it "renders simple light icon version" do
-    render_component(
-      text: "Get more info",
-      href: "/coronavirus",
-      simple_light: true,
-    )
-    assert_select ".gem-c-action-link--simple-light"
   end
 
   it "renders inverse version" do
