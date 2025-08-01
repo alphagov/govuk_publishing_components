253cfed9e43c04b8121c284c18935b98bb997a20
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2025-08-01 12:46:27 +0100

    Update preview page header and footer approach
    - Removed `hide-header-and-footer` class from the template and associated styles as these are not in use and did not have any impact on the rendering of the preview page
    - Updated the tests to ensure a header or footer element is not included on the preview page, the previous implementation would always pass

--- spec/component_guide/component_preview_spec.rb
+++ spec/component_guide/component_preview_spec.rb
@@ -4,9 +4,9 @@ describe "Component preview", :capybara do
   it "shows all component examples on a page without header or footer" do
     visit "/component-guide/test_component_with_params/preview"
     expect(page).to have_selector(".govuk-template--rebranded")
-    expect(page).to have_selector(".hide-header-and-footer")
-    expect(page).not_to have_selector("#global-header")
-    expect(page).not_to have_selector("#footer")
+
+    expect(page).not_to have_selector("body > header")
+    expect(page).not_to have_selector("body > footer")
 
     expect(page).to have_selector(".component-guide-preview-page .preview-title", text: "Default")
     expect(page).to have_selector(".component-guide-preview-page .test-component-with-params", text: "Some value")
@@ -18,9 +18,9 @@ describe "Component preview", :capybara do
   it "shows a specific example on a page without header or footer" do
     visit "/component-guide/test_component_with_params/another_example/preview"
     expect(page).to have_selector(".govuk-template--rebranded")
-    expect(page).to have_selector(".hide-header-and-footer")
-    expect(page).not_to have_selector("#global-header")
-    expect(page).not_to have_selector("#footer")
+
+    expect(page).not_to have_selector("body > header")
+    expect(page).not_to have_selector("body > footer")
 
     expect(page).to have_no_selector(".component-guide-preview-page .preview-title")
     expect(page).to have_selector(".component-guide-preview-page .test-component-with-params", text: "A different value")
