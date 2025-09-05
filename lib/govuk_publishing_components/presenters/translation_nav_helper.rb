fc10d1473a8fd93c2b8f7eef5aaab64f8dc0b9db
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-09-05 13:27:23 +0100

    Remove margin top from translation nav component
    - removes default margin top
    - removes option to override margin top to zero

--- lib/govuk_publishing_components/presenters/translation_nav_helper.rb
+++ lib/govuk_publishing_components/presenters/translation_nav_helper.rb
@@ -6,7 +6,6 @@ module GovukPublishingComponents
       def initialize(local_assigns)
         @translations = []
         @translations = local_assigns[:translations] if local_assigns[:translations]
-        @no_margin_top = local_assigns[:no_margin_top]
         @inverse = local_assigns[:inverse]
       end
 
@@ -17,16 +16,11 @@ module GovukPublishingComponents
       def classes
         classes = %w[gem-c-translation-nav]
         classes << inverse_class if @inverse
-        classes << margin_class if @no_margin_top
         classes.join(" ")
       end
 
       def inverse_class
         "gem-c-translation-nav--inverse"
-      end
-
-      def margin_class
-        "gem-c-translation-nav--no-margin-top"
       end
     end
   end
