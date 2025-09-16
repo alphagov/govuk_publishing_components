44cd40317e43b0d4092bdf39c5f9e86f1fba1e43
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-09-16 14:27:36 +0100

    Add basic application status page
    - adds route, controller and model for page giving basic information about frontend rendering apps
    - first checks for local content before doing fetch requests to github for information
    - caches results to prevent overuse
    - uses table display from component auditing

---
+++ app/models/govuk_publishing_components/applications_page.rb
@@ -0,0 +1,53 @@
+module GovukPublishingComponents
+  class ApplicationsPage
+    def initialize(application)
+      @application = application
+      @dir = get_directory
+      @exists = true if @dir
+      @gemfile = get_gemfile
+    end
+
+    def readable_name
+      @application.gsub("-", " ").capitalize
+    end
+
+    def gem_version
+      parse_file(@gemfile, /govuk_publishing_components \(([^)>=~ ]+)\)/)
+    end
+
+    def slimmer_version
+      parse_file(@gemfile, /slimmer \(([^)>=~ ]+)\)/)
+    end
+
+  private
+
+    def get_gemfile
+      @exists ? get_gemfile_local : get_gemfile_remote
+    end
+
+    def get_gemfile_local
+      lockfile = "#{@dir}/Gemfile.lock"
+      return unless File.file?(lockfile)
+
+      File.read(lockfile)
+    end
+
+    def get_gemfile_remote
+      url = "https://raw.githubusercontent.com/alphagov/#{@application}/main/Gemfile.lock"
+      Net::HTTP.get(URI.parse(url))
+    end
+
+    def parse_file(src, regex)
+      return unless src
+
+      matches = src.match(regex)
+      matches[1] if matches
+    end
+
+    def get_directory
+      host_dir = File.expand_path("..")
+      app_dir = "#{host_dir}/#{@application}"
+      @directory = app_dir if Dir.exist?(app_dir)
+    end
+  end
+end
