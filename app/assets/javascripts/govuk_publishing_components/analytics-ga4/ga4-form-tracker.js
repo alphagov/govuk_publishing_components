c9da7cf95c5db04fa1946525ffc59b8b055cc666
Patrick Cartlidge <patrickcartlidge@gmail.com>
2025-09-24 10:17:20 +0100

    `Ga4FormTracker` report single selected option
    If form is set to report the number of selected options within a select,
    in the case in which only one option has been selected the `text` of the
    only selected option will be reported instead of the count (which will
    be `1`).

--- app/assets/javascripts/govuk_publishing_components/analytics-ga4/ga4-form-tracker.js
+++ app/assets/javascripts/govuk_publishing_components/analytics-ga4/ga4-form-tracker.js
@@ -135,7 +135,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         }
       } else if (inputNodename === 'SELECT' && elem.querySelectorAll('option:checked')) {
         var selectedOptions = Array.from(elem.querySelectorAll('option:checked')).map(function (element) { return element.text })
-        input.answer = this.useSelectCount ? selectedOptions.length : selectedOptions.join(',')
+        input.answer = this.useSelectCount && selectedOptions.length > 1 ? selectedOptions.length : selectedOptions.join(',')
       } else if (isTextField && elem.value) {
         if (this.includeTextInputValues || elem.hasAttribute('data-ga4-form-include-input')) {
           if (this.useTextCount && !isDateField) {
