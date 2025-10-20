1fcd3aa68481341822cccfab073715ed4eb2d045
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2025-10-20 09:28:14 +0100

    Remove individual loading of stylesheets from component_guide

--- lib/govuk_publishing_components/config.rb
+++ lib/govuk_publishing_components/config.rb
@@ -35,10 +35,14 @@ module GovukPublishingComponents
       Gem::Specification.find_by_name("govuk_publishing_components").gem_dir
     end
 
+    def self.component_guide_stylesheet
+      { "#{gem_directory}/app/assets/stylesheets/component_guide/application.scss" => "component_guide/application.css" }
+    end
+
     def self.all_stylesheets
       stylesheets = Dir.glob("#{gem_directory}/app/assets/stylesheets/govuk_publishing_components/components/*.scss")
 
-      application_stylesheet_hash = { "#{gem_directory}/app/assets/stylesheets/component_guide/application.scss" => "component_guide/application.css" }
+      application_stylesheet_hash = component_guide_stylesheet
 
       stylesheets.each_with_object(application_stylesheet_hash) do |path, hsh|
         hsh[path] = "govuk_publishing_components/components/#{File.basename(path, '.*')}.css"
