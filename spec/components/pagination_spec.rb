uk-gov-mirror/alphagov.govuk_publishing_components

Add pagination to `Pagination`
`Pagination` can now accept `items` as a parameter,
which will result in links to individual pages being
rendered. This is the same functionality as the
GOV.UK Design System component and accepts the same
parameters.

The tests have been updated to reflect this change.

spec/components/pagination_spec.rb
@@ -34,6 +34,64 @@ describe "Pagination", type: :view do
     assert_link("next-page")
   end
 
+  it "renders pagination container and page links if items specified" do
+    render_component(
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination--block", false
+    assert_select ".govuk-pagination__item", text: "1"
+    assert_select ".govuk-pagination__link[aria-label='Page 1']", text: "1"
+    assert_link("page-1")
+  end
+
+  it "renders page link as current page" do
+    render_component(
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+          current: true,
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination--block", false
+    assert_select ".govuk-pagination__item", text: "1"
+    assert_select ".govuk-pagination__item--current", text: "1"
+    assert_select ".govuk-pagination__link[aria-current='page']", text: "1"
+  end
+
+  it "renders ellipsis" do
+    render_component(
+      items: [
+        {
+          ellipsis: true,
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination--block", false
+    assert_select ".govuk-pagination__item--ellipses", text: "⋯"
+  end
+
+  it "raises error if number not defined for item that is not ellipsis" do
+    expect {
+      render_component(
+        items: [
+          {
+            href: "page-1",
+          },
+        ],
+      )
+    }.to raise_error(ActionView::Template::Error, "Number required for item 0")
+  end
+
   it "makes distinction between the navigation text and label text of the links when a label is present" do
     render_component(
       previous_page: {
@@ -107,6 +165,62 @@ describe "Pagination", type: :view do
     end
   end
 
+  it "includes GA4 tracking for pagination" do
+    render_component(
+      previous_page: {
+        url: "previous-page",
+        title: "Previous page",
+      },
+      next_page: {
+        url: "next-page",
+        title: "Next page",
+      },
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination" do |pagination|
+      expect(pagination.attr("data-module").to_s).to eq "ga4-link-tracker"
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "pagination",
+      "text": "Previous page",
+      "section": "Previous",
+    }.to_json
+
+    assert_select ".govuk-pagination__prev a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "pagination",
+      "text": "Next page",
+      "section": "Next",
+    }.to_json
+
+    assert_select ".govuk-pagination__next a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "pagination",
+      "text": "1",
+      "section": "Pagination list",
+    }.to_json
+
+    assert_select ".govuk-pagination__item a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+  end
+
   it "allows GA4 tracking to be disabled" do
     render_component(
       disable_ga4: true,
@@ -118,11 +232,18 @@ describe "Pagination", type: :view do
         url: "next-page",
         title: "Next page",
       },
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+        },
+      ],
     )
 
     assert_select ".govuk-pagination[data-module='ga4-link-tracker']", false
     assert_select ".govuk-pagination__prev a[data-ga4-link]", false
     assert_select ".govuk-pagination__next a[data-ga4-link]", false
+    assert_select ".govuk-pagination__item a[data-ga4-link]", false
   end
 
   def assert_link(link)

alphagov/govuk_publishing_components

spec/components/pagination_spec.rb
@@ -34,6 +34,64 @@ describe "Pagination", type: :view do
     assert_link("next-page")
   end
 
+  it "renders pagination container and page links if items specified" do
+    render_component(
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination--block", false
+    assert_select ".govuk-pagination__item", text: "1"
+    assert_select ".govuk-pagination__link[aria-label='Page 1']", text: "1"
+    assert_link("page-1")
+  end
+
+  it "renders page link as current page" do
+    render_component(
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+          current: true,
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination--block", false
+    assert_select ".govuk-pagination__item", text: "1"
+    assert_select ".govuk-pagination__item--current", text: "1"
+    assert_select ".govuk-pagination__link[aria-current='page']", text: "1"
+  end
+
+  it "renders ellipsis" do
+    render_component(
+      items: [
+        {
+          ellipsis: true,
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination--block", false
+    assert_select ".govuk-pagination__item--ellipsis", text: "⋯"
+  end
+
+  it "raises error if number not defined for item that is not ellipsis" do
+    expect {
+      render_component(
+        items: [
+          {
+            href: "page-1",
+          },
+        ],
+      )
+    }.to raise_error(ActionView::Template::Error, "Number required for item 0")
+  end
+
   it "makes distinction between the navigation text and label text of the links when a label is present" do
     render_component(
       previous_page: {
@@ -107,6 +165,62 @@ describe "Pagination", type: :view do
     end
   end
 
+  it "includes GA4 tracking for pagination" do
+    render_component(
+      previous_page: {
+        url: "previous-page",
+        title: "Previous page",
+      },
+      next_page: {
+        url: "next-page",
+        title: "Next page",
+      },
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+        },
+      ],
+    )
+
+    assert_select ".govuk-pagination" do |pagination|
+      expect(pagination.attr("data-module").to_s).to eq "ga4-link-tracker"
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "pagination",
+      "text": "Previous page",
+      "section": "Previous",
+    }.to_json
+
+    assert_select ".govuk-pagination__prev a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "pagination",
+      "text": "Next page",
+      "section": "Next",
+    }.to_json
+
+    assert_select ".govuk-pagination__next a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+
+    expected_ga4_json = {
+      "event_name": "navigation",
+      "type": "pagination",
+      "text": "1",
+      "section": "Pagination list",
+    }.to_json
+
+    assert_select ".govuk-pagination__item a" do |pagination_link|
+      expect(pagination_link.attr("data-ga4-link").to_s).to eq expected_ga4_json
+    end
+  end
+
   it "allows GA4 tracking to be disabled" do
     render_component(
       disable_ga4: true,
@@ -118,11 +232,18 @@ describe "Pagination", type: :view do
         url: "next-page",
         title: "Next page",
       },
+      items: [
+        {
+          number: "1",
+          href: "page-1",
+        },
+      ],
     )
 
     assert_select ".govuk-pagination[data-module='ga4-link-tracker']", false
     assert_select ".govuk-pagination__prev a[data-ga4-link]", false
     assert_select ".govuk-pagination__next a[data-ga4-link]", false
+    assert_select ".govuk-pagination__item a[data-ga4-link]", false
   end
 
   def assert_link(link)