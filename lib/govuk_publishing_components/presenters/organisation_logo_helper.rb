71f0ec8ca8d9da6580a078d3383d11395d4661e6
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-12-23 15:59:45 +0000

    Refactor organisation logo helper
    - remove attr reader values as not used anywhere
    - add checking for what is passed and raise an error unless minimum required to render the component is passed
    - set image src and alt to nil instead of false, as image alt was previous rendered as `alt="false"` instead of not being present
    - use instance variables for name, url, image etc. as previously relied on attr reader
    - rewrite logo container class to use word array and be more readable

--- lib/govuk_publishing_components/presenters/organisation_logo_helper.rb
+++ lib/govuk_publishing_components/presenters/organisation_logo_helper.rb
@@ -4,40 +4,44 @@ module GovukPublishingComponents
       include ActionView::Helpers
       include ActionView::Context
 
-      attr_reader :name, :url, :crest, :image, :logo_image_src, :logo_image_alt, :hide_underline
-
       def initialize(local_assigns)
+        raise(ArgumentError, "Component requires an organisation including a name") unless minimum_for_component?(local_assigns)
+
         @name = local_assigns[:organisation][:name]
         @url = local_assigns[:organisation][:url]
         @crest = local_assigns[:organisation][:crest]
         @image = local_assigns[:organisation][:image] || false
         if @image
-          @logo_image_src = local_assigns[:organisation][:image][:url] || false
-          @logo_image_alt = local_assigns[:organisation][:image][:alt_text] || false
+          @logo_image_src = local_assigns[:organisation][:image][:url] || nil
+          @logo_image_alt = local_assigns[:organisation][:image][:alt_text] || nil
         end
         @hide_underline = local_assigns[:hide_underline] || false
       end
 
       def logo_content
-        if image
-          image_tag(logo_image_src, alt: logo_image_alt, class: "gem-c-organisation-logo__image")
+        if @image
+          image_tag(@logo_image_src, alt: @logo_image_alt, class: "gem-c-organisation-logo__image")
         else
-          content_tag("span", name, class: "gem-c-organisation-logo__name")
+          content_tag("span", @name, class: "gem-c-organisation-logo__name")
         end
       end
 
       def logo_container_class
-        logo_class = "gem-c-organisation-logo__container"
-        logo_class = "#{logo_class} gem-c-organisation-logo__link" if url
-        logo_class = "#{logo_class} gem-c-organisation-logo__link-hide-underline" if hide_underline
-        logo_class = "#{logo_class} gem-c-organisation-logo__crest gem-c-organisation-logo__crest--#{crest}" if crest_has_visual_identity?
-        logo_class
+        logo_class = %w[gem-c-organisation-logo__container]
+        logo_class << "gem-c-organisation-logo__link" if @url
+        logo_class << "gem-c-organisation-logo__link-hide-underline" if @hide_underline
+        logo_class << "gem-c-organisation-logo__crest gem-c-organisation-logo__crest--#{@crest}" if crest_has_visual_identity?
+        logo_class.join(" ")
       end
 
     private
 
+      def minimum_for_component?(options)
+        true unless !options[:organisation] || options[:organisation] && !options[:organisation][:name]
+      end
+
       def crest_has_visual_identity?
-        crest.present? && !%w[no-identity custom].include?(crest)
+        @crest.present? && !%w[no-identity custom].include?(@crest)
       end
     end
   end
