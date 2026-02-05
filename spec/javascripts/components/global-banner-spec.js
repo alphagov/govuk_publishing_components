76f83b25f18b39341e014afc795a07c036995ee0
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2026-02-05 19:16:03 +0000

    Remove jQuery from global banner spec

--- spec/javascripts/components/global-banner-spec.js
+++ spec/javascripts/components/global-banner-spec.js
@@ -1,4 +1,4 @@
-/* eslint-env jasmine, jquery */
+/* eslint-env jasmine */
 /* global GOVUK */
 describe('Global banner module', function () {
   'use strict'
@@ -11,22 +11,24 @@ describe('Global banner module', function () {
   }
 
   beforeEach(function () {
+    element = document.createElement('div')
+    element.id = 'global-banner'
+    element.dataset.module = 'global-banner'
+    element.dataset.bannerVersion = 5
+    element.innerHTML = '<a href="/register-to-vote" class="govuk-link js-call-to-action">Register to Vote</a>'
+
     window.GOVUK.setConsentCookie({ settings: true })
     document.cookie = 'global_banner_seen=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
   })
 
   afterEach(function () {
     window.GOVUK.setConsentCookie({ settings: null })
-    $('#global-banner').remove()
+    element.remove()
   })
 
   describe('global banner default', function () {
     beforeEach(function () {
-      element = $(
-        '<div id="global-banner" data-module="global-banner" data-banner-version="5">' +
-          '<a href="/register-to-vote" class="govuk-link js-call-to-action">Register to Vote</a>' +
-        '</div>'
-      )
+      document.body.appendChild(element)
 
       document.cookie = 'global_banner_seen=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
     })
@@ -34,7 +36,7 @@ describe('Global banner module', function () {
     it('sets basic global_banner_seen cookie if not already set', function () {
       expect(GOVUK.getCookie('global_banner_seen')).toBeNull()
 
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(0)
@@ -44,7 +46,7 @@ describe('Global banner module', function () {
     it('sets basic global_banner_seen cookie if existing one is malformed', function () {
       GOVUK.setCookie('global_banner_seen', 1)
 
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(0)
@@ -54,19 +56,16 @@ describe('Global banner module', function () {
 
   describe('global banner interactions', function () {
     beforeEach(function () {
-      element = $(
-        '<div id="global-banner" data-module="global-banner" data-banner-version="6">' +
-          '<a href="/register-to-vote" class="govuk-link js-call-to-action">Register to Vote</a>' +
-        '</div>'
-      )
+      element.dataset.bannerVersion = 6
 
-      $(document.body).append(element)
+      document.body.appendChild(element)
+
       document.cookie = 'global_banner_seen=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
     })
 
     it('increments view count', function () {
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 1, version: 6 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(2)
@@ -76,19 +75,19 @@ describe('Global banner module', function () {
 
   describe('always on', function () {
     beforeEach(function () {
+      element.dataset.bannerVersion = 6
+
+      document.body.appendChild(element)
+
       document.cookie = 'global_banner_seen=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
     })
 
     it('does not increment view count when on', function () {
-      element = $(
-        '<div id="global-banner" data-module="global-banner" data-banner-version="11" data-global-banner-permanent="true">' +
-          '<a href="/register-to-vote" class="govuk-link js-call-to-action">Register to Vote</a>' +
-        '</div>'
-      )
-      $(document.body).append(element)
+      element.dataset.bannerVersion = 11
+      element.dataset.globalBannerPermanent = 'true'
 
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 2, version: 11 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(2)
@@ -96,15 +95,11 @@ describe('Global banner module', function () {
     })
 
     it('continues to increment view count when off', function () {
-      element = $(
-        '<div id="global-banner" data-module="global-banner" data-banner-version="11" data-global-banner-permanent="false">' +
-          '<a href="/register-to-vote" class="govuk-link js-call-to-action">Register to Vote</a>' +
-        '</div>'
-      )
-      $(document.body).append(element)
+      element.dataset.bannerVersion = 11
+      element.dataset.globalBannerPermanent = 'false'
 
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 2, version: 11 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(3)
@@ -114,11 +109,11 @@ describe('Global banner module', function () {
 
   describe('on initialise for cookies', function () {
     function expectGlobalBannerToShow () {
-      expect($('#global-banner').hasClass('gem-c-global-banner--visible')).toBe(true)
+      expect(document.querySelector('#global-banner')).toHaveClass('gem-c-global-banner--visible')
     }
 
     function expectGa4AttributeToExist () {
-      expect($('#global-banner').attr('data-ga4-global-banner')).toBe('')
+      expect(document.querySelector('#global-banner').getAttribute('data-ga4-global-banner')).toBe('')
     }
 
     function deleteAllCookies () {
@@ -133,19 +128,14 @@ describe('Global banner module', function () {
     }
 
     beforeEach(function () {
-      element = $(
-        '<div id="global-banner" data-module="global-banner" data-banner-version="5">' +
-          '<a href="/register-to-vote" class="govuk-link js-call-to-action">Register to Vote</a>' +
-        '</div>'
-      )
-      $('html').append(element)
-      $('html').removeClass('show-global-banner')
+      document.body.appendChild(element)
+
       deleteAllCookies()
       window.GOVUK.setConsentCookie({ settings: true })
     })
 
     it('sets global_banner_seen cookie', function () {
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(0)
@@ -156,7 +146,7 @@ describe('Global banner module', function () {
 
     it('sets cookie to default value if current cookie is old (prior to versioning mechanism)', function () {
       GOVUK.setCookie('global_banner_seen', 1)
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(0)
@@ -168,7 +158,7 @@ describe('Global banner module', function () {
 
     it('resets cookie if version number is out of date, if count below 3', function () {
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 1, version: 1 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(0)
@@ -179,7 +169,7 @@ describe('Global banner module', function () {
 
     it('resets cookie if version number is out of date, if count above 3', function () {
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 10, version: 1 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(0)
@@ -190,7 +180,7 @@ describe('Global banner module', function () {
 
     it('makes banner visible if view count is less than 3', function () {
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 1, version: 5 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expectGlobalBannerToShow()
@@ -199,15 +189,15 @@ describe('Global banner module', function () {
 
     it('keeps the banner hidden if view count is 3 or more', function () {
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 3, version: 5 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
-      expect($('#global-banner').hasClass('gem-c-global-banner--visible')).toBe(false)
+      expect(document.querySelector('#global-banner')).not.toHaveClass('gem-c-global-banner--visible')
     })
 
     it('will not set the count higher than 3', function () {
       GOVUK.setCookie('global_banner_seen', JSON.stringify({ count: 3, version: 5 }))
-      globalBanner = new GOVUK.Modules.GlobalBanner(element[0])
+      globalBanner = new GOVUK.Modules.GlobalBanner(element)
       globalBanner.init()
 
       expect(parseCookie(GOVUK.getCookie('global_banner_seen')).count).toBe(3)
