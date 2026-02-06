8329ff7e4fad90ef13a697301295a166820c0653
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2026-02-06 10:29:57 +0000

    Add integration aggregate cookie

--- app/assets/javascripts/govuk_publishing_components/lib/cookie-functions.js
+++ app/assets/javascripts/govuk_publishing_components/lib/cookie-functions.js
@@ -31,7 +31,8 @@
     TLSversion: 'usage',
     _ga_VBLT2V3FZR: 'usage', // gtag cookie used to persist the session state, integration
     _ga_P1DGM6TVYF: 'usage', // staging
-    _ga_S5RQ7FTGVR: 'usage' // production
+    _ga_S5RQ7FTGVR: 'usage', // production
+    _ga_57Z1E1V4D6: 'usage' // integration aggregate
   }
 
   /*
