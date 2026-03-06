c923dc2ca5327ddbda8012a1e84f34dcae300cd4
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2026-03-06 16:56:13 +0000

    Load `gtm.js` when usage or aggregate consent is true

--- app/assets/javascripts/govuk_publishing_components/analytics-ga4/init-ga4.js
+++ app/assets/javascripts/govuk_publishing_components/analytics-ga4/init-ga4.js
@@ -5,7 +5,7 @@ var initFunction = function () {
   window.removeEventListener('cookie-consent', window.GOVUK.analyticsGa4.init)
   var consentCookie = window.GOVUK.getConsentCookie()
 
-  if (consentCookie && consentCookie.usage) {
+  if (consentCookie && (consentCookie.usage || consentCookie.aggregate)) {
     window.GOVUK.analyticsGa4.vars.internalDomains = []
     window.GOVUK.analyticsGa4.vars.internalDomains.push(window.GOVUK.analyticsGa4.core.trackFunctions.getHostname())
     window.GOVUK.analyticsGa4.core.trackFunctions.appendDomainsWithoutWWW(window.GOVUK.analyticsGa4.vars.internalDomains)
