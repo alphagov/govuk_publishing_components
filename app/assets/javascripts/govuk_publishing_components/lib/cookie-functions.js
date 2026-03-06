0dddb71f6cf9e8a36bc2f1177d33b871316543da
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2026-03-06 16:38:52 +0000

    Add support for aggregate cookie in cookie-functions.js
    - ensure 'aggregate' is set to false when approving all cookie types as 'usage' take priority
    - update cookie function tests to check aggregate is included and set to true by default
    - update cookie banner test to set aggregate cookie option to false
    - add 'aggregate' cookie for each environment

--- app/assets/javascripts/govuk_publishing_components/lib/cookie-functions.js
+++ app/assets/javascripts/govuk_publishing_components/lib/cookie-functions.js
@@ -8,7 +8,8 @@
     essential: true,
     settings: false,
     usage: false,
-    campaigns: false
+    campaigns: false,
+    aggregate: true
   }
 
   var COOKIE_CATEGORIES = {
@@ -30,7 +31,10 @@
     TLSversion: 'usage',
     _ga_VBLT2V3FZR: 'usage', // gtag cookie used to persist the session state, integration
     _ga_P1DGM6TVYF: 'usage', // staging
-    _ga_S5RQ7FTGVR: 'usage' // production
+    _ga_S5RQ7FTGVR: 'usage', // production
+    _ga_E926VS6V96: 'aggregate', // integration aggregate
+    _ga_KQ77TQ2YXL: 'aggregate', // staging aggregate
+    _ga_57Z1E1V4D6: 'aggregate' // production aggregate
   }
 
   /*
@@ -73,7 +77,8 @@
       essential: true,
       settings: true,
       usage: true,
-      campaigns: true
+      campaigns: true,
+      aggregate: false
     }
 
     window.GOVUK.setCookie('cookies_policy', JSON.stringify(approvedConsent), { days: 365 })
