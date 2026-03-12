28919e14f9ec83bc4ffa5739c26dfea4830fa09e
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2026-03-12 08:30:02 +0000

    Update init-ga4.js
    - Set the default consent cookie if it isn't already present, as `aggregate: true` is included in the updated default consent cookie, this ensures analytics loads as expected for visitors who do not already have the `cookies_policy` cookie set.
    - Start the ga4-link-tracker module when aggregate is true, this ensures the `ga4-specialist-link-tracker` module works as expected as this relies on the ga4-link-tracker-module
    - Update tests in `init-ga4-spec.js` to include this additional functionality

--- spec/javascripts/govuk_publishing_components/analytics-ga4/init-ga4.spec.js
+++ spec/javascripts/govuk_publishing_components/analytics-ga4/init-ga4.spec.js
@@ -2,6 +2,10 @@
 
 describe('Initialising GA4', function () {
   var GOVUK = window.GOVUK
+  // default cookie consent to use as basis of comparison
+  var DEFAULT_COOKIE_CONSENT = '{"essential":true,"settings":false,"usage":false,"campaigns":false,"aggregate":true}'
+  // all consent cookie to use as a basis of comparison
+  var ALL_COOKIE_CONSENT = '{"essential":true,"settings":true,"usage":true,"campaigns":true,"aggregate":false}'
 
   afterEach(function () {
     window.dataLayer = []
@@ -21,9 +25,41 @@ describe('Initialising GA4', function () {
       GOVUK.analyticsGa4.analyticsModules.Test.init = function () { test.functionThatMightBeCalled() }
     })
 
-    it('calls analytics modules successfully', function () {
+    afterEach(function () {
+      GOVUK.deleteCookie('cookies_policy')
+    })
+
+    it('sets a default `cookie_policy` consent cookie if not previously set', function () {
+      GOVUK.cookie('cookies_policy', null)
+      spyOn(window.GOVUK, 'setDefaultConsentCookie').and.callThrough()
+
+      GOVUK.analyticsGa4.init()
+
+      expect(window.GOVUK.setDefaultConsentCookie).toHaveBeenCalled()
+      expect(GOVUK.getCookie('cookies_policy')).toEqual(DEFAULT_COOKIE_CONSENT)
+    })
+
+    it('does not update the `cookie_policy` cookie to the default values if previously set', function () {
+      GOVUK.cookie('cookies_policy', ALL_COOKIE_CONSENT)
+      spyOn(window.GOVUK, 'setDefaultConsentCookie').and.callThrough()
+
+      GOVUK.analyticsGa4.init()
+
+      expect(window.GOVUK.setDefaultConsentCookie).not.toHaveBeenCalled()
+      expect(GOVUK.getCookie('cookies_policy')).toEqual(ALL_COOKIE_CONSENT)
+    })
+
+    it('calls analytics modules that run on page load successfully when usage consent is true', function () {
       spyOn(GOVUK.analyticsGa4.analyticsModules.Test, 'init').and.callThrough()
       GOVUK.setCookie('cookies_policy', '{"essential":true,"settings":true,"usage":true,"campaigns":true}')
+      GOVUK.analyticsGa4.init()
+
+      expect(test.functionThatMightBeCalled).toHaveBeenCalled()
+    })
+
+    it('calls analytics modules that run on page load successfully when aggregate consent is true', function () {
+      spyOn(GOVUK.analyticsGa4.analyticsModules.Test, 'init').and.callThrough()
+      GOVUK.setCookie('cookies_policy', '{"essential":true,"settings":true,"usage":false,"campaigns":true,"aggregate":true}')
       GOVUK.analyticsGa4.init()
 
       expect(test.functionThatMightBeCalled).toHaveBeenCalled()
