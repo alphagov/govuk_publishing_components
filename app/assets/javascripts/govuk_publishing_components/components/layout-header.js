2b73a2e65edb3642eb3aad76f22f09eec4f93c2c
Jon Kirwan <jon.kirwan@digital.cabinet-office.gov.uk>
2026-03-19 13:25:38 +0000

    Remove removed header bundle script
    Note this breaks the layout header navigation

--- app/assets/javascripts/govuk_publishing_components/components/layout-header.js
+++ app/assets/javascripts/govuk_publishing_components/components/layout-header.js
@@ -1,5 +1,4 @@
 // This component relies on JavaScript from GOV.UK Frontend
-// = require govuk/components/header/header.bundle.js
 window.GOVUK = window.GOVUK || {}
 window.GOVUK.Modules = window.GOVUK.Modules || {}
 window.GOVUK.Modules.GovukHeader = window.GOVUKFrontend.Header
