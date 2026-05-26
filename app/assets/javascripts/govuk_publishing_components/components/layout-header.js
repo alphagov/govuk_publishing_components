6573f6f1d0edcd6b6da07f66cec0dcb854b760a9
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2026-05-26 17:17:54 +0100

    DELETE THIS COMMIT BEFORE MERGING INTO V6 FEATURE BRANCH Hotpotch of temp fixes, just to get gem app running locally with govuk-frontend 6.0.0.0 so we can see what's failing
    Was seeing link_tree errors for the rebrand folder references which according to the v6 release notes we have to address. Not sure this is the correct fix I've used here, need to double check on that.
    
    Was seeing errors for missing header.bundle.js. It looks like the govuk-frontend header.js got removed from v6 (https://github.com/alphagov/govuk-frontend/pull/6423) which we rely on here: https://github.com/alphagov/govuk_publishing_components/blob/97500154f4d1b0898c7bbedc351214bf8a178f19/app/assets/javascripts/govuk_publishing_components/components/layout-header.js#L2 So that will need to be addressed, for now I've just comment out the entire assignment which is obviously not ideal.
    
    Also took out the sass adjust and darken sass functions that together with the deprecated colours weren't giving out warnings but actually stopped the sass compilation. As discussed with Jon, I've not touched the functional colours upgrade, that might address the issues seen here.

--- app/assets/javascripts/govuk_publishing_components/components/layout-header.js
+++ app/assets/javascripts/govuk_publishing_components/components/layout-header.js
@@ -1,3 +1,2 @@
 // This component relies on JavaScript from GOV.UK Frontend
-// = require govuk/components/header/header.bundle.js
-window.GOVUK.Modules.GovukHeader = window.GOVUKFrontend.Header
+// window.GOVUK.Modules.GovukHeader = window.GOVUKFrontend.Header
