e028954e77bb10b6242efc7b17df6d24f8f089b9
Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2026-06-16 13:28:12 +0100

    Implement the outgoing side of link decoration
    This iterates over each link on a page when ga4 is initialised and appends
    a query string to all non gov.uk government links (eg. example.gov.uk) that
    corresponds to the users cookie consent choice. This is done whether they
    accept or decline cookies and a boolean is added to the query string
    accordingly. This will stop the cookie banner from appearing when a user
    comes from a government site and they've made a cookie choice there

--- app/assets/javascripts/govuk_publishing_components/analytics-ga4/init-ga4.js
+++ app/assets/javascripts/govuk_publishing_components/analytics-ga4/init-ga4.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (typeof window.GOVUK.analyticsGa4.init === 'function') {
+    window.GOVUK.analyticsGa4.init()
+  }
+})
+
 var initFunction = function () {
   window.removeEventListener('cookie-consent', window.GOVUK.analyticsGa4.init)
 
@@ -19,35 +25,63 @@ var initFunction = function () {
     }
   }
 
-  var consentCookie = window.GOVUK.getConsentCookie()
-  if (consentCookie && consentCookie.usage) {
-    window.GOVUK.analyticsGa4.vars.internalDomains = []
-    window.GOVUK.analyticsGa4.vars.internalDomains.push(window.GOVUK.analyticsGa4.core.trackFunctions.getHostname())
-    window.GOVUK.analyticsGa4.core.trackFunctions.appendDomainsWithoutWWW(window.GOVUK.analyticsGa4.vars.internalDomains)
-    window.GOVUK.analyticsGa4.core.load()
+  window.GOVUK.analyticsGa4.decorateLinks = function (consent) {
+    const links = document.querySelectorAll('a[href]')
+    const allowedDomains = [
+      'end-to-end-journeys-545890405086.europe-west2.run.app',
+      'x-domain-prototype-2-545890405086.europe-west2.run.app',
+      'x-domain-prototype-3-545890405086.europe-west2.run.app'
+    ]
 
-    if (!window.GOVUK.analyticsGa4.analyticsModulesStarted) {
-      // Initialise analytics modules that start on page load
-      // https://github.com/alphagov/govuk_publishing_components/blob/main/docs/analytics-ga4/analytics.md#code-structure
-      var analyticsModules = window.GOVUK.analyticsGa4.analyticsModules
-      for (var property in analyticsModules) {
-        var module = analyticsModules[property]
-        if (typeof module.init === 'function') {
-          try {
-            module.init()
-          } catch (e) {
-            // if there's a problem with the module, catch the error to allow other modules to start
-            console.warn('Error starting analytics module ' + property + ': ' + e.message, window.location)
-          }
+    links.forEach(link => {
+      try {
+        const url = new URL(link.href, window.location.origin)
+
+        if (allowedDomains.includes(url.hostname)) {
+          url.searchParams.set('cookies[analytics]', consent)
+          link.href = url.toString()
         }
+      } catch (e) {
       }
-      window.GOVUK.analyticsGa4.analyticsModulesStarted = true
-    }
-  } else {
-    window.addEventListener('cookie-consent', window.GOVUK.analyticsGa4.init)
+    })
   }
 
   window.GOVUK.analyticsGa4.checkCookieConsentLinkDecoration(window.location)
+
+  var consentCookie = window.GOVUK.getConsentCookie()
+
+  if (consentCookie) {
+    var consentValue = consentCookie.usage ? 'yes' : 'no'
+    window.GOVUK.analyticsGa4.decorateLinks(consentValue)
+
+    if (consentCookie.usage) {
+      window.GOVUK.analyticsGa4.vars.internalDomains = []
+      window.GOVUK.analyticsGa4.vars.internalDomains.push(window.GOVUK.analyticsGa4.core.trackFunctions.getHostname())
+      window.GOVUK.analyticsGa4.core.trackFunctions.appendDomainsWithoutWWW(window.GOVUK.analyticsGa4.vars.internalDomains)
+      window.GOVUK.analyticsGa4.core.load()
+
+      if (!window.GOVUK.analyticsGa4.analyticsModulesStarted) {
+        // Initialise analytics modules that start on page load
+        // https://github.com/alphagov/govuk_publishing_components/blob/main/docs/analytics-ga4/analytics.md#code-structure
+        var analyticsModules = window.GOVUK.analyticsGa4.analyticsModules
+        for (var property in analyticsModules) {
+          var module = analyticsModules[property]
+          if (typeof module.init === 'function') {
+            try {
+              module.init()
+            } catch (e) {
+              // if there's a problem with the module, catch the error to allow other modules to start
+              console.warn('Error starting analytics module ' + property + ': ' + e.message, window.location)
+            }
+          }
+        }
+        window.GOVUK.analyticsGa4.analyticsModulesStarted = true
+      }
+    }
+  }
+  window.addEventListener('cookie-consent', window.GOVUK.analyticsGa4.init)
 }
 
 window.GOVUK.analyticsGa4.init = initFunction
+
+window.GOVUK.analyticsGa4.init()
