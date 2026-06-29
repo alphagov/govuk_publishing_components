e0b7309549357f23e99528db2e5565390528f8b7
Luke Booker <BookerL@MAC-Dev-MR21K7K97F.local>
2026-06-29 11:04:28 +0100

    Remove navigation functionality from the layout header component

--- spec/components/layout_header_spec.rb
+++ spec/components/layout_header_spec.rb
@@ -142,65 +142,18 @@ describe "Layout header", type: :view do
   it "has the default logo link when no logo_link is specified" do
     render_component({})
 
-    assert_select ".govuk-header__link--homepage[href='/']"
+    assert_select ".govuk-header__homepage-link[href='/']"
   end
 
   it "has a custom logo link when specified" do
     render_component({ logo_link: "https://example.com" })
 
-    assert_select ".govuk-header__link--homepage[href='https://example.com']"
+    assert_select ".govuk-header__homepage-link[href='https://example.com']"
   end
 
-  it "renders the header with deprecated navigation items" do
-    navigation_items = [
-      { text: "Foo", href: "/foo", active: true },
-      { text: "Bar", href: "/bar" },
-      { text: "Hello", href: "/hello", show_only_in_collapsed_menu: true },
-    ]
+  it "renders the signout link when requested" do
+    render_component(signout_link: true)
 
-    render_component(environment: "staging", navigation_items:, deprecated_navigation: true)
-
-    assert_select ".govuk-header__navigation .govuk-header__menu-button", text: "Menu"
-    assert_select ".govuk-header__navigation-item.govuk-header__navigation-item--active", text: "Foo"
-    assert_select ".govuk-header__navigation-item", text: "Bar"
-    assert_select ".govuk-header__navigation-item.govuk-header__navigation-item--collapsed-menu-only", text: "Hello"
-    assert_select ".gem-c-header__nav[aria-label='Top level']"
-    assert_select ".govuk-header__menu-button[hidden]"
-  end
-
-  it "renders the deprecated header navigation items with custom aria-label when navigation_aria_label is specified" do
-    navigation_items = [
-      { text: "Foo", href: "/foo", active: true },
-      { text: "Bar", href: "/bar" },
-      { text: "Hello", href: "/hello", show_only_in_collapsed_menu: true },
-    ]
-
-    render_component(environment: "staging", navigation_items:, navigation_aria_label: "My fancy label", deprecated_navigation: true)
-
-    assert_select ".gem-c-header__nav[aria-label='My fancy label']"
-  end
-
-  it "renders the deprecated navigation links with data attributes when specified" do
-    navigation_items = [
-      {
-        text: "Foo",
-        href: "/foo",
-        data: {
-          hello: "world",
-        },
-      },
-      {
-        text: "Bar",
-        href: "/bar",
-        data: {
-          more_than_one_word: "test",
-        },
-      },
-    ]
-
-    render_component(navigation_items:, deprecated_navigation: true)
-
-    assert_select ".gem-c-header__nav .govuk-header__link[data-hello='world']", text: "Foo"
-    assert_select ".gem-c-header__nav .govuk-header__link[data-more-than-one-word='test']", text: "Bar"
+    assert_select ".gem-c-layout-header__signout-link[href='/auth/gds/sign_out']", text: "Sign out"
   end
 end
