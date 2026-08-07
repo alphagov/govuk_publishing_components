02ef1548deb5e7bde618044a8e51861d84124332
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2026-08-07 14:59:42 +0100

    Update tests for updated component

--- spec/components/datetime_fields_spec.rb
+++ spec/components/datetime_fields_spec.rb
@@ -6,13 +6,10 @@ describe "Date input", type: :view do
   end
 
   it "renders the basic component" do
-    render_component({
-      prefix: "prefix",
-      field_name: "field_name",
-    })
+    render_component({})
 
     assert_select ".gem-c-datetime-fields"
-    assert_select ".govuk-fieldset__legend", text: "Date (required)"
+    assert_select ".govuk-fieldset__legend", text: "Date"
     assert_select ".govuk-input[name='day']"
     assert_select ".govuk-input[name='month']"
     assert_select ".govuk-input[name='year']"
@@ -21,24 +18,36 @@ describe "Date input", type: :view do
     assert_select ".govuk-label", text: "Minute"
   end
 
+  it "renders the correct names for the inputs" do
+    render_component({
+      year: { name: "year_field_name" },
+      month: { name: "month_field_name" },
+      day: { name: "day_field_name" },
+      hour: { name: "hour_field_name" },
+      minute: { name: "minute_field_name" },
+    })
+
+    assert_select ".govuk-input[name='year_field_name']"
+    assert_select ".govuk-input[name='month_field_name']"
+    assert_select ".govuk-input[name='day_field_name']"
+    assert_select ".govuk-select[name='hour_field_name']"
+    assert_select ".govuk-select[name='minute_field_name']"
+  end
+
   it "allows heading level and size" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       heading_level: 1,
       heading_size: "l",
     })
 
-    assert_select "legend.govuk-fieldset__legend--l", text: "Date (required)"
-    assert_select "h1.govuk-fieldset__heading", text: "Date (required)"
+    assert_select "legend.govuk-fieldset__legend--l", text: "Date"
+    assert_select "h1.govuk-fieldset__heading", text: "Date"
     assert_select "legend.govuk-fieldset__legend--l", text: "Time"
     assert_select "h1.govuk-fieldset__heading", text: "Time"
   end
 
   it "allows custom text for the date and time headings" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       date_heading: "The date",
       time_heading: "The time",
     })
@@ -49,20 +58,16 @@ describe "Date input", type: :view do
 
   it "shows hint text" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       date_hint: "For example, 01 August 2022",
       time_hint: "For example, 09:30 or 19:30",
     })
 
-    assert_select "[data-ga4-form-section='Date (required)'] .govuk-hint", text: "For example, 01 August 2022"
+    assert_select "[data-ga4-form-section='Date'] .govuk-hint", text: "For example, 01 August 2022"
     assert_select "[data-ga4-form-section='Time'] .govuk-hint", text: "For example, 09:30 or 19:30"
   end
 
   it "accepts an id" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       id: "kevin",
     })
 
@@ -70,39 +75,43 @@ describe "Date input", type: :view do
     assert_select ".govuk-date-input[id='kevin_date']"
   end
 
-  it "renders custom date fields" do
+  it "renders custom field ids" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       year: {
         id: "year_id",
-        name: "year_name",
-        width: 4,
-        value: "2024",
       },
       month: {
         id: "month_id",
-        name: "month_name",
-        width: 3,
-        value: "1",
       },
       day: {
         id: "day_id",
-        name: "day_name",
-        width: 2,
-        value: "14",
+      },
+      hour: {
+        id: "hour_id",
+      },
+      minute: {
+        id: "minute_id",
       },
     })
 
-    assert_select ".govuk-input.govuk-input--width-4[id='year_id'][name='year_name'][value='2024']"
-    assert_select ".govuk-input.govuk-input--width-3[id='month_id'][name='month_name'][value='1']"
-    assert_select ".govuk-input.govuk-input--width-2[id='day_id'][name='day_name'][value='14']"
+    assert_select ".govuk-input[id='year_id']"
+    assert_select ".govuk-input[id='month_id']"
+    assert_select ".govuk-input[id='day_id']"
+    assert_select ".govuk-select[id='hour_id']"
+    assert_select ".govuk-select[id='minute_id']"
   end
 
-  it "accepts values and ids for hour and minute elements" do
+  it "renders field values" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
+      year: {
+        value: 2024,
+      },
+      month: {
+        value: 1,
+      },
+      day: {
+        value: 14,
+      },
       hour: {
         value: 2,
         id: "my-hour-id",
@@ -113,14 +122,15 @@ describe "Date input", type: :view do
       },
     })
 
+    assert_select ".govuk-input[value='2024']"
+    assert_select ".govuk-input[value='1']"
+    assert_select ".govuk-input[value='14']"
     assert_select "select[id='my-hour-id'] option[value='02'][selected='selected']"
     assert_select "select[id='my-minute-id'] option[value='03'][selected='selected']"
   end
 
   it "renders error fields for the whole component" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       error_items: [
         {
           text: "Descriptive error 1",
@@ -137,8 +147,6 @@ describe "Date input", type: :view do
 
   it "renders separate error fields for the date and time sections" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       error_items_date: [
         {
           text: "Descriptive date error 1",
@@ -157,14 +165,12 @@ describe "Date input", type: :view do
       ],
     })
 
-    assert_select "[data-ga4-form-section='Date (required)'] .govuk-error-message", text: "Error: Descriptive date error 1Descriptive date error 2"
+    assert_select "[data-ga4-form-section='Date'] .govuk-error-message", text: "Error: Descriptive date error 1Descriptive date error 2"
     assert_select "[data-ga4-form-section='Time'] .govuk-error-message", text: "Error: Descriptive time error 1Descriptive time error 2"
   end
 
   it "accepts data attributes" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       data_attributes: {
         module: "not-a-real-module",
         something_else: "i-just-thought-of",
@@ -176,8 +182,6 @@ describe "Date input", type: :view do
 
   it "with date only option" do
     render_component({
-      prefix: "prefix",
-      field_name: "field_name",
       date_only: true,
     })
 
