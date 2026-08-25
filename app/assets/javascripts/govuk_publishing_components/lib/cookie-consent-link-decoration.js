9179a7d496d2d531ec924b285518ebcb62568825
Jon Hallam <jonathan.hallam@dsit.gov.uk>
2026-08-25 16:05:50 +0100

    Implement the outgoing side of link decoration
    This iterates over each link on a page when ga4 is initialised and appends
    a query string to urls that are in the allowedDomains object that
    corresponds to the users cookie consent choice. This is done whether they
    accept or decline cookies and a boolean is added to the query string
    accordingly. This will stop the cookie banner from appearing when a user
    comes from a government site and they've made a cookie choice there

--- app/assets/javascripts/govuk_publishing_components/lib/cookie-consent-link-decoration.js
+++ app/assets/javascripts/govuk_publishing_components/lib/cookie-consent-link-decoration.js
@@ -5,6 +5,7 @@
 
   CookieConsentLinkDecoration.prototype.init = function () {
     this.checkCookieConsentLinkDecoration(window.location)
+    this.decorateLinks()
   }
 
   CookieConsentLinkDecoration.prototype.checkCookieConsentLinkDecoration = function (location) {
@@ -22,6 +23,31 @@
         window.GOVUK.cookie('cookies_preferences_set', 'true')
       }
     }
+  }
+
+  CookieConsentLinkDecoration.prototype.decorateLinks = function () {
+    var consentCookie = window.GOVUK.getConsentCookie ? window.GOVUK.getConsentCookie() : null
+    var consentCount = Object.values(consentCookie || {}).filter(val => val === true).length
+    var consentValue = consentCount === 4 ? 'yes' : 'no'
+    var links = document.querySelectorAll("[href^='https']")
+    var allowedDomains = [
+      'end-to-end-journeys-545890405086.europe-west2.run.app',
+      'x-domain-prototype-2-545890405086.europe-west2.run.app',
+      'x-domain-prototype-3-545890405086.europe-west2.run.app'
+    ]
+
+    links.forEach((link) => {
+      try {
+        var url = new URL(link.href, window.location.origin)
+
+        if (allowedDomains.includes(url.hostname)) {
+          url.searchParams.set('cookies[analytics]', consentValue)
+          link.href = url.toString()
+        }
+      } catch (e) {
+        console.error("Couldn't decorate link - " + link.href)
+      }
+    })
   }
 
   Modules.CookieConsentLinkDecoration = CookieConsentLinkDecoration
